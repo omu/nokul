d8294c60fe767ae35fd8a35e5259baef70106dca
M. Serhat Dündar <msdundars@gmail.com>
2018-03-05 01:39:33 +0300

    more yoksis experiments

---
+++ app/services/yoksis/v2/meb_mezun_sorgulama.rb
@@ -0,0 +1,38 @@
+module Services
+  module Yoksis
+    module V2
+      # client = Services::Yoksis::V2::MebMezunSorgulama.new
+      class MebMezunSorgulama
+        def initialize
+          @client = Savon.client(
+            wsdl: 'https://servisler.yok.gov.tr/ws/mebmezunsorgulav2?WSDL',
+            convert_request_keys_to: :camelcase,
+            logger: Rails.logger,
+            basic_auth: [ENV['YOKSIS_USER'], ENV['YOKSIS_PASSWORD']],
+            soap_version: 2
+          )
+        end
+
+        def mezuniyet_verilerini_getir(queried_id_number)
+          message = {
+            'TCKIMLIKNO' => queried_id_number,
+            'ServicePassWord' => ENV['YOKSIS_USER']
+          }
+
+          @client.call(__method__, message: message)
+                 .body[:mezuniyet_verilerini_getir_response]
+        end
+
+        def mezuniyet_verilerini_getir_detay
+          message = {
+            'TCKIMLIKNO' => queried_id_number,
+            'ServicePassWord' => ENV['YOKSIS_USER']
+          }
+
+          @client.call(__method__, message: message)
+                 .body[:mezuniyet_verilerini_getir_detay_response]
+        end
+      end
+    end
+  end
+end
---
+++ app/services/yoksis/v4/ogrenci-bilgi-toplama.rb
@@ -0,0 +1,23 @@
+module Services
+  module Yoksis
+    module V4
+      # client = Services::Yoksis::V4::OgrenciBilgiToplama.new
+      class OgrenciBilgiToplama
+        def initialize
+          wsdl = if Rails.env = "production"
+                  "https://servisler.yok.gov.tr/ws/ogrencibilgitoplamav4?WSDL"
+                 else
+                  "https://servisler.yok.gov.tr/ws/ogrencibilgitoplamav4Test?WSDL"
+                 end
+
+          @client = Savon.client(
+            wsdl: wsdl,
+            convert_request_keys_to: :camelcase,
+            logger: Rails.logger,
+            basic_auth: [ENV['YOKSIS_USER'], ENV['YOKSIS_PASSWORD']]
+          )
+        end
+      end
+    end
+  end
+end
