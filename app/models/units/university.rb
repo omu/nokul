b455a8ffcbabd5233112464dbc705978dc42fde8
ecmelkytz <ecmel.kytz@gmail.com>
2018-03-26 14:41:26 +0300

    Define instance methods for university class

--- app/models/units/university.rb
+++ app/models/units/university.rb
@@ -1,2 +1,10 @@
 class University < Unit
+  # Define these functions: faculties, institutes, vocationalschools, academies,
+  # departments, sciencedisciplines, artdisciplines, disciplines,
+  # interdisciplinarydisciplines, disciplines, rectorships, researchcenters
+  (types - %w[University]).each do |type|
+    define_method(type.downcase.pluralize) do
+      descendants.where(type: type)
+    end
+  end
 end
