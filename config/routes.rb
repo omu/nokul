ffa430e4b28c7606467e072b9448a7f2977dc87f
M. Serhat Dündar <msdundars@gmail.com>
2018-04-18 15:14:13 +0300

    Upgrade Rails to 5.2.0 :tada:

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'foobar#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
