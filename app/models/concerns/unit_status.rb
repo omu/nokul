1d6dab32b5a7ce31ea17a6f18922a0da0eee3fd6
M. Serhat Dündar <msdundars@gmail.com>
2018-04-20 02:30:57 +0300

    Minor changes

--- app/models/concerns/unit_status.rb
+++ app/models/concerns/unit_status.rb
@@ -2,6 +2,6 @@ module UnitStatus
   extend ActiveSupport::Concern
 
   included do
-    enum status: %i[passive active partially_passive closed archived unknown]
+    enum status: { passive: 0, active: 1, partially_passive: 3, closed: 4, archived: 5, unknown: 6 }
   end
 end
