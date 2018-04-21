789519858b1110cee2d1a3ae7f465519785f6e9a
M. Serhat Dündar <msdundars@gmail.com>
2018-04-22 00:41:29 +0300

    Use GEM object for version comparison

--- test/config/application_config_test.rb
+++ test/config/application_config_test.rb
@@ -5,12 +5,12 @@ require 'test_helper'
 # tests config/application.rb configuration to prevent the app from unexpected config changes!
 
 class ApplicationConfigTest < ActiveSupport::TestCase
-  test 'Configuration defaults for should match with Rails version' do
+  test 'Configuration defaults should match with Rails version' do
     assert_equal Rails.application.config.loaded_config_version, Rails.version.to_f
   end
 
   test 'Compare Gemfile Rails version with configured Rails version' do
-    assert_equal Gem.loaded_specs['rails'].version.to_s.to_f, Rails.application.config.loaded_config_version
+    assert_equal Gem.loaded_specs['rails'].version, Gem::Version.new(Rails.application.config.loaded_config_version)
   end
 
   test 'Time zone should match with Istanbul' do
