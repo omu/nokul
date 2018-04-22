3ab85924054c5a3de6745a0bcfa322df8e703bb0
M. Serhat Dündar <msdundars@gmail.com>
2018-04-22 04:47:50 +0300

    Fix auto-load problems

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# If you need complete loading for some models, define them as methods and list below:
+Unit.generate_scopes
