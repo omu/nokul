ca765864755f34885d26be82a7a15ee53e095637
ecmelkytz <ecmel.kytz@gmail.com>
2018-04-30 14:58:39 +0300

    Create AcademicTerm model, fixture and unit test

---
+++ app/models/calendars/academic_term.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class AcademicTerm < ApplicationRecord
+  # validations
+  validates :year, :term, presence: true
+  validates :year, uniqueness: { scope: :term }
+
+  # enums
+  enum term: { fall: 0, spring: 1, summer: 2 }
+
+  def self.years
+    (1975..Time.now.year).map { |year| "#{year}-#{year + 1}" }.reverse
+  end
+end
