d8b7e5e24d26af1b2993cb85e32063b182fedcdd
msdundar <msdundars@gmail.com>
2018-05-04 16:14:26 +0300

    Fix Rake error

--- db/migrate/20180503161718_create_staff_academic_titles.rb
+++ db/migrate/20180503161718_create_staff_academic_titles.rb
@@ -1,6 +1,6 @@
 class CreateStaffAcademicTitles < ActiveRecord::Migration[5.2]
   def change
-    create_table :staff_academician_titles do |t|
+    create_table :staff_academic_titles do |t|
       t.string :name
       t.integer :code
     end
