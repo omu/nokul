069c6d348a487e818af37a17c389cd247aba4267
msdundar <msdundars@gmail.com>
2018-05-15 23:28:18 +0300

    minor changes

--- db/migrate/20180426071040_create_calendar_title_types.rb
+++ db/migrate/20180426071040_create_calendar_title_types.rb
@@ -4,7 +4,6 @@ class CreateCalendarTitleTypes < ActiveRecord::Migration[5.2]
       t.references :type, foreign_key: { to_table: :calendar_types }
       t.references :title, foreign_key: { to_table: :calendar_titles }
       t.integer :status, default: 0
-
       t.timestamps
     end
     add_index :calendar_title_types, [:type_id, :title_id], unique: true, name: 'index_of_calendar_title_types'
