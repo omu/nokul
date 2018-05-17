4c618a896d40e6433b97648d1156b2c5d32a7adc
msdundar <msdundars@gmail.com>
2018-05-17 22:11:51 +0300

    add missing helper tests

---
+++ test/helpers/academic_term_helper_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class AcademicTermHelperTest < ActionView::TestCase
+  test 'years should return ranged years as Array' do
+    assert years.is_a?(Array)
+  end
+
+  test 'years should return a list of ranged years from 1975 to today' do
+    assert_equal years.size, DateTime.now.year-1975+1
+  end
+
+  test 'full_name should return a full name for academic term' do
+    assert_equal full_name(academic_terms(:fall_2017_2018)), '2017-2018 / fall'
+  end
+end
