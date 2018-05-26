2c0bdc01a263344881b3b0e0a47e601a11f8de60
M. Serhat Dündar <msdundars@gmail.com>
2018-05-26 03:03:42 +0300

    Completed validations for address

---
+++ app/validators/address_validator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AddressValidator < ActiveModel::Validator
+  def validate(record)
+    # TODO: i18n
+    record.errors[:base] << 'En fazla 5 adet adres ekleyebilirsiniz.' if record.user.addresses.size > 5
+    record.errors[:base] << 'En fazla 1 ikamet adresiniz olabilir.' if record.user.addresses.formal.present?
+  end
+end
