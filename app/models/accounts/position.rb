ec4393d04433f686956cc314f287481df2521320
M. Serhat Dündar <msdundars@gmail.com>
2018-05-26 03:04:50 +0300

    Users can not have duplicate positions for the same duty

--- app/models/accounts/position.rb
+++ app/models/accounts/position.rb
@@ -4,4 +4,7 @@ class Position < ApplicationRecord
   # relations
   belongs_to :duty
   belongs_to :administrative_function
+
+  # validations
+  validates :duty, uniqueness: { scope: %i[administrative_function] }
 end
--- test/models/accounts/position_test.rb
+++ test/models/accounts/position_test.rb
@@ -12,4 +12,11 @@ class PositionTest < ActiveSupport::TestCase
       assert positions(:baum_dean).send(property)
     end
   end
+
+  # validations: uniqueness
+  test 'a user can not have duplicate positions in a department' do
+    fake = positions(:baum_dean).dup
+    assert_not fake.valid?
+    assert_not_empty fake.errors[:duty]
+  end
 end
