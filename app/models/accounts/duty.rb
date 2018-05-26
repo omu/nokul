ec485a6c1282781c16602027aa4cf2277cc9c439
M. Serhat Dündar <msdundars@gmail.com>
2018-05-26 04:29:59 +0300

    Add custom validations to user related models

--- app/models/accounts/duty.rb
+++ app/models/accounts/duty.rb
@@ -11,4 +11,10 @@ class Duty < ApplicationRecord
   validates :temporary, presence: true
   validates :start_date, presence: true
   validates :unit_id, uniqueness: { scope: %i[employee] }
+  validates_with DutyValidator
+
+  # scopes
+  scope :temporary, -> { where(temporary: true) }
+  scope :tenure, -> { where(temporary: false) }
+  scope :active, -> { where('end_date < ? OR end_date IS NULL', Time.zone.today) }
 end
--- db/migrate/20180507052504_create_employees.rb
+++ db/migrate/20180507052504_create_employees.rb
@@ -3,7 +3,7 @@
 class CreateEmployees < ActiveRecord::Migration[5.2]
   def change
     create_table :employees do |t|
-      t.boolean :status, default: true, null: false
+      t.boolean :active, default: true, null: false
       t.references :title, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
