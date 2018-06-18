ed8eb74bb09bace32fbb79a3f159a3337e1bfb1c
M. Serhat Dündar <msdundars@gmail.com>
2018-06-18 03:43:28 +0300

    JS manifests for guest, coreui and panel

---
+++ app/assets/javascripts/coreui.js
@@ -0,0 +1,15 @@
+// Manifest including all assets of CoreUI
+
+// core-components
+//= require jquery/dist/jquery.min
+//= require popper.js/dist/umd/popper.min
+//= require bootstrap/dist/js/bootstrap.min
+
+// core-ui
+//= require @coreui/coreui/dist/js/coreui.min
+//= require @coreui/coreui/dist/js/coreui-utilities.min
+
+// extensions
+//= require pace-progress/pace.min.js
+//= require perfect-scrollbar/dist/perfect-scrollbar.min
+//= require select2/dist/js/select2.min
