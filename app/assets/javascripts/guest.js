c71db2466c3d5421d9abe3150ff65b9aba52490c
M. Serhat Dündar <msdundars@gmail.com>
2018-06-18 03:43:28 +0300

    JS manifests for guest, coreui and panel

---
+++ app/assets/javascripts/guest.js
@@ -0,0 +1,5 @@
+// Manifest file for guest template. Only include assets that you need.
+
+//= require jquery/dist/jquery.min
+//= require toastr/build/toastr.min
+//= require shared/toastr_config
---
+++ app/assets/javascripts/shared/toastr_config.js
@@ -0,0 +1,3 @@
+toastr.options.closeButton = true;
+toastr.options.showMethod = 'slideDown';
+toastr.options.progressBar = true;
