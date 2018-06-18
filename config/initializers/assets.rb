b4f9bd3f74dc71b8cf09d008c85524738be40f4e
msdundar <msdundars@gmail.com>
2018-06-18 16:59:38 +0300

    .

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w[guest.scss guest.js]
+Rails.application.config.assets.precompile << %r{simple-line-icons/fonts/[\w-]+\.(?:eot|svg|ttf|woff2?)$}
+
