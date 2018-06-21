e623b5e8ecd224a27a04fc0dc29bf56b14bfb161
M. Serhat Dündar <msdundars@gmail.com>
2018-06-22 02:06:17 +0300

    Refactor Identity controller and views

--- app/controllers/account/identities_controller.rb
+++ app/controllers/account/identities_controller.rb
@@ -6,14 +6,20 @@ module Account
     before_action :check_formality, only: %i[edit update destroy]
 
     def index
+      breadcrumb t('.card_header'), identities_path
       @identities = current_user.identities
     end
 
     def new
+      breadcrumb t('.index.card_header'), identities_path, match: :exact
+      breadcrumb t('.form_title'), identities_path
       @identity = current_user.identities.informal.new
     end
 
-    def edit; end
+    def edit
+      breadcrumb t('.index.card_header'), identities_path, match: :exact
+      breadcrumb t('.form_title'), identities_path
+    end
 
     def create
       @identity = current_user.identities.informal.new(identity_params)
