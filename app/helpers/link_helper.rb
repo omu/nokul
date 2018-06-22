8995e02194a678e7efeae25c8c1427c11312d938
M. Serhat Dündar <msdundars@gmail.com>
2018-06-22 12:43:37 +0300

    Use default parameters in link_helper

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -1,53 +1,53 @@
 # frozen_string_literal: true
 
 module LinkHelper
-  def link_to_back(path = nil, text: t('action_group.back'))
+  def link_to_back(path = nil, text = t('action_group.back'))
     link_to(
       fa_icon('arrow-left', text: text),
       path,
-      class: 'btn btn-dark btn-sm mb-1'
+      class: 'btn btn-dark btn-sm'
     )
   end
 
-  def link_to_destroy(path = nil, text: t('action_group.destroy'))
+  def link_to_destroy(path = nil, text = t('action_group.destroy'))
     link_to(
       fa_icon('trash', text: text),
       path,
       method: :delete,
       data: { confirm: t('are_you_sure') },
-      class: 'btn btn-outline-danger btn-sm mb-1'
+      class: 'btn btn-outline-danger btn-sm'
     )
   end
 
-  def link_to_edit(path = nil, text: t('action_group.edit'))
+  def link_to_edit(path = nil, text = t('action_group.edit'))
     link_to(
       fa_icon('pencil', text: text),
       path,
-      class: 'btn btn-outline-success btn-sm mb-1'
+      class: 'btn btn-outline-success btn-sm'
     )
   end
 
-  def link_to_new(path = nil, text: t('action_group.add'))
+  def link_to_new(path = nil, text = t('action_group.add'))
     link_to(
       fa_icon('plus', text: text),
       path,
-      class: 'btn btn-outline-primary btn-sm mb-1'
+      class: 'btn btn-outline-primary btn-sm'
     )
   end
 
-  def link_to_show(path = nil, text: t('action_group.show'))
+  def link_to_show(path = nil, text = t('action_group.show'))
     link_to(
       fa_icon('eye', text: text),
       path,
-      class: 'btn btn-outline-info btn-sm mb-1'
+      class: 'btn btn-outline-info btn-sm'
     )
   end
 
-  def link_to_update(path = nil, text: t('action_group.update'))
+  def link_to_update(path = nil, text = t('action_group.update'))
     link_to(
       fa_icon('pencil-square-o', text: text),
       path,
-      class: 'btn btn-outline-info btn-sm mb-1'
+      class: 'btn btn-outline-info btn-sm'
     )
   end
 end
--- test/helpers/link_helper_test.rb
+++ test/helpers/link_helper_test.rb
@@ -7,50 +7,49 @@ class LinkHelperTest < ActionView::TestCase
 
   test '#link_to_back' do
     link = <<-HTML.squish
-      <a class="btn btn-dark btn-sm mb-1"
-         href="#"><i class="fa fa-arrow-left"></i> Back</a>
+      <a class="btn btn-dark btn-sm" href="#"><i class="fa fa-arrow-left"></i> Back</a>
     HTML
-    assert_equal link_to_back('#', text: 'Back'), link
+    assert_equal link_to_back('#', 'Back'), link
   end
 
   test '#link_to_destroy' do
     link = <<-HTML.squish
       <a data-confirm="#{t('are_you_sure')}"
-         class="btn btn-outline-danger btn-sm mb-1"
+         class="btn btn-outline-danger btn-sm"
          rel="nofollow" data-method="delete" href="#"><i class="fa fa-trash"></i> Test Destroy</a>
     HTML
-    assert_equal link_to_destroy('#', text: 'Test Destroy'), link
+    assert_equal link_to_destroy('#', 'Test Destroy'), link
   end
 
   test '#link_to_edit' do
     link = <<-HTML.squish
-      <a class="btn btn-outline-success btn-sm mb-1"
+      <a class="btn btn-outline-success btn-sm"
          href="#"><i class="fa fa-pencil"></i> Test Edit</a>
     HTML
-    assert_equal link_to_edit('#', text: 'Test Edit'), link
+    assert_equal link_to_edit('#', 'Test Edit'), link
   end
 
   test '#link_to_new' do
     link = <<-HTML.squish
-      <a class="btn btn-outline-primary btn-sm mb-1"
+      <a class="btn btn-outline-primary btn-sm"
          href="#"><i class="fa fa-plus"></i> Test New</a>
     HTML
-    assert_equal link_to_new('#', text: 'Test New'), link
+    assert_equal link_to_new('#', 'Test New'), link
   end
 
   test '#link_to_show' do
     link = <<-HTML.squish
-      <a class="btn btn-outline-info btn-sm mb-1"
+      <a class="btn btn-outline-info btn-sm"
          href="#"><i class="fa fa-eye"></i> Test Show</a>
     HTML
-    assert_equal link_to_show('#', text: 'Test Show'), link
+    assert_equal link_to_show('#', 'Test Show'), link
   end
 
   test '#link_to_update' do
     link = <<-HTML.squish
-      <a class="btn btn-outline-info btn-sm mb-1"
+      <a class="btn btn-outline-info btn-sm"
         href="#"><i class="fa fa-pencil-square-o"></i> Test Update</a>
     HTML
-    assert_equal link_to_update('#', text: 'Test Update'), link
+    assert_equal link_to_update('#', 'Test Update'), link
   end
 end
