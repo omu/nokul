d0198551f7da84388e63b726e8a54df5b780c372
msdundar <msdundars@gmail.com>
2018-06-24 23:42:27 +0300

    Add beta environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
-end
+if Rails.env.beta?
+  Sidekiq.configure_server do |config|
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_URL'] }
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDIS_URL'] }
+  end
 end
