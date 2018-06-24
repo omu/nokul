19e9e89969d70132343e9678f7cb9072f80b7a70
msdundar <msdundars@gmail.com>
2018-06-24 20:21:32 +0300

    Fix rubocop offenses

--- test/errors/id_number_error_test.rb
+++ test/errors/id_number_error_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class IdNumberErrorTest < ActiveSupport::TestCase
-  test "ID number throws an error message when raised" do
+  test 'ID number throws an error message when raised' do
     assert_equal IdNumberError.new.message, 'Geçersiz T.C. Kimlik Numarası'
     I18n.locale = 'en'
     assert_equal IdNumberError.new.message, 'Invalid ID Number'
