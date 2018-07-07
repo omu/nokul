d4eb287b0c1670d24cf50185295fc0a7403c34bf
msdundar <msdundars@gmail.com>
2018-07-07 20:16:18 +0300

    Remove show from references. Pluralize reference. Reference -> references.

--- config/routes.rb
+++ config/routes.rb
@@ -50,20 +50,20 @@ Rails.application.routes.draw do
     end
   end
 
-  scope module: :reference do
-    resources :student_disability_types
-    resources :student_drop_out_types
-    resources :student_education_levels
-    resources :student_entrance_point_types
-    resources :student_entrance_types
-    resources :student_grades
-    resources :student_grading_systems
-    resources :student_punishment_types
-    resources :student_studentship_statuses
-    resources :unit_instruction_languages
-    resources :unit_instruction_types
-    resources :unit_statuses
-    resources :unit_types
-    resources :university_types
+  scope module: :references do
+    resources :student_disability_types, except: :show
+    resources :student_drop_out_types, except: :show
+    resources :student_education_levels, except: :show
+    resources :student_entrance_point_types, except: :show
+    resources :student_entrance_types, except: :show
+    resources :student_grades, except: :show
+    resources :student_grading_systems, except: :show
+    resources :student_punishment_types, except: :show
+    resources :student_studentship_statuses, except: :show
+    resources :unit_instruction_languages, except: :show
+    resources :unit_instruction_types, except: :show
+    resources :unit_statuses, except: :show
+    resources :unit_types, except: :show
+    resources :university_types, except: :show
   end
 end
