ec969e88683ef9d6aa4af7fd9d45ad859d4344e2
msdundar <msdundars@gmail.com>
2018-07-10 01:46:24 +0300

    Common concern for reference controllers. Closes #89.

--- app/controllers/references/student_drop_out_types_controller.rb
+++ app/controllers/references/student_drop_out_types_controller.rb
@@ -2,48 +2,11 @@
 
 module References
   class StudentDropOutTypesController < ApplicationController
-    include Pagy::Backend
-
-    before_action :set_student_drop_out_type, only: %i[edit update destroy]
-
-    def index
-      @pagy, @student_drop_out_types = pagy(StudentDropOutType.all)
-    end
-
-    def new
-      @student_drop_out_type = StudentDropOutType.new
-    end
-
-    def edit; end
-
-    def create
-      @student_drop_out_type = StudentDropOutType.new(student_drop_out_type_params)
-      @student_drop_out_type.save ? redirect_with('success') : render(:new)
-    end
-
-    def update
-      if @student_drop_out_type.update(student_drop_out_type_params)
-        redirect_to(@student_drop_out_type, notice: t('.success'))
-      else
-        render(:edit)
-      end
-    end
-
-    def destroy
-      @student_drop_out_type.destroy ? redirect_with('success') : redirect_with('warning')
-    end
+    include ReferenceResource
 
     private
 
-    def redirect_with(message)
-      redirect_to(student_drop_out_types_path, notice: t(".#{message}"))
-    end
-
-    def set_student_drop_out_type
-      @student_drop_out_type = StudentDropOutType.find(params[:id])
-    end
-
-    def student_drop_out_type_params
+    def secure_params
       params.require(:student_drop_out_type).permit(:name, :code)
     end
   end
--- app/controllers/references/student_grading_systems_controller.rb
+++ app/controllers/references/student_grading_systems_controller.rb
@@ -2,48 +2,11 @@
 
 module References
   class StudentGradingSystemsController < ApplicationController
-    include Pagy::Backend
-
-    before_action :set_student_grading_system, only: %i[edit update destroy]
-
-    def index
-      @pagy, @student_grading_systems = pagy(StudentGradingSystem.all)
-    end
-
-    def new
-      @student_grading_system = StudentGradingSystem.new
-    end
-
-    def edit; end
-
-    def create
-      @student_grading_system = StudentGradingSystem.new(student_grading_system_params)
-      @student_grading_system.save ? redirect_with('success') : render(:new)
-    end
-
-    def update
-      if @student_grading_system.update(student_grading_system_params)
-        redirect_to(@student_grading_system, notice: t('.success'))
-      else
-        render(:edit)
-      end
-    end
-
-    def destroy
-      @student_grading_system.destroy ? redirect_with('success') : redirect_with('warning')
-    end
+    include ReferenceResource
 
     private
 
-    def redirect_with(message)
-      redirect_to(student_grading_systems_path, notice: t(".#{message}"))
-    end
-
-    def set_student_grading_system
-      @student_grading_system = StudentGradingSystem.find(params[:id])
-    end
-
-    def student_grading_system_params
+    def secure_params
       params.require(:student_grading_system).permit(:name, :code)
     end
   end
