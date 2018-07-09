330d67c71fcbeb11ecd784974c33c577264d8400
M. Serhat Dündar <msdundars@gmail.com>
2018-07-09 03:31:18 +0300

    GSuite SMTP ayarlarını yap
    Closes #79.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,17 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :domain         => 'mail.google.com',
+    :port           => 587,
+    :user_name      => Rails.application.credentials.gmail[:username],
+    :password       => Rails.application.credentials.gmail[:password],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
