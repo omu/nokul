4fc9f6fdb73d6560d58f9020801d60d597193a58
M. Serhat Dündar <msdundars@gmail.com>
2018-07-09 03:29:22 +0300

    Use encrypted secrets instead of environment variables
    Closes #78.

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -4,7 +4,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = Rails.application.credentials.rollbar[:access_token]
 
   # Here we'll disable in 'test':
   config.enabled = false if Rails.env.test? || Rails.env.development?
