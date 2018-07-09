c23c10a8e4a9f7ab6978bf54b99da8b7e46dd935
M. Serhat Dündar <msdundars@gmail.com>
2018-07-09 04:20:26 +0300

    Add permalinks to employee
    Closes #68 and closes #69.

--- app/models/accounts/employee.rb
+++ app/models/accounts/employee.rb
@@ -21,6 +21,16 @@ class Employee < ApplicationRecord
   # scopes
   scope :active, -> { where(active: true) }
   scope :passive, -> { where(active: false) }
+
+  # permalinks
+  extend FriendlyId
+  friendly_id :username_slug, use: :slugged
+
+  def username_slug
+    username, domain = user.email.split('@')
+    username if domain.eql?('omu.edu.tr')
+  end
+
   # custom methods
   def academic?
     title.branch.eql?('ÖE')
