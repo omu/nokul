90eff5039b7a53e0bfaca4a27f7f98349c11f351
M. Serhat Dündar <msdundars@gmail.com>
2018-07-09 04:26:20 +0300

    WIP: Certifications
    Initial YOKSIS experiments. Work in progress.

---
+++ app/services/yoksis/v1/ozgecmis.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module Services
+  module Yoksis
+    module V1
+      # client = Services::Yoksis::V1::Ozgecmis.new
+      class Ozgecmis
+        def initialize
+          @username = Rails.application.credentials.yoksis[:user]
+          @password = Rails.application.credentials.yoksis[:password]
+
+          @client = Savon.client(
+            wsdl: 'http://servisler.yok.gov.tr/ws/ozgecmisv1?WSDL',
+            convert_request_keys_to: :camelcase,
+            basic_auth: [@username, @password]
+          )
+        end
+
+        def get_arastirma_sertifka_bilgisi_v1(id_number)
+          message = {
+            'parametre' => {
+              'P_KULLANICI_ID' => @username,
+              'P_SIFRE' => @password,
+              'P_TC_KIMLIK_NO' => id_number
+            }
+          }
+
+          @client.call(__method__, message: message).body["#{__method__}_response".to_sym]
+        end
+
+        alias certifications get_arastirma_sertifka_bilgisi_v1
+      end
+    end
+  end
+end
