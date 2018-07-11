88c4c9aefbcd34270eceb2cafa38936f4ccbe8c9
Sinan Sahin <snansahin1@gmail.com>
2018-07-11 19:22:42 +0300

    fix test failuers

--- app/models/locations/country.rb
+++ app/models/locations/country.rb
@@ -24,7 +24,6 @@ class Country < ApplicationRecord
   validates :alpha_3_code, presence: true, uniqueness: true
   validates :numeric_code, presence: true, uniqueness: true
   validates :mernis_code, numericality: { only_integer: true }, uniqueness: true, allow_blank: true
-  validates :yoksis_code, numericality: { only_integer: true }, uniqueness: true, allow_blank: true
 
   # callbacks
   before_save do
