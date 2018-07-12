451c99bc70d3cdbfc24d753fe47925f811fcdb69
msdundar <msdundars@gmail.com>
2018-07-12 16:05:24 +0300

    Update dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -379,7 +379,7 @@ ActiveRecord::Schema.define(version: 2018_07_11_121944) do
     t.string "preferred_language", default: "tr"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "password_changed_at", default: -> { "now()" }, null: false
+    t.datetime "password_changed_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["id_number"], name: "index_users_on_id_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
