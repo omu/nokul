31c65b486a6702e6d6e1c0148f5e215ff554bac5
isubas <irfan.subas@uzem.omu.edu.tr>
2018-07-17 10:50:33 +0300

    Fix #125

--- app/services/kps/omu/adres.rb
+++ app/services/kps/omu/adres.rb
@@ -15,24 +15,23 @@ module Services
             :adres_sorgula, message: message
           ).body[:adres_sorgula_response][:return][:sorgula_result][:sorgu_sonucu][:kimlik_noile_kisi_adres_bilgileri]
 
-          if response[:hata_bilgisi].present?
-            raise IdNumberError i
-          else
-            yerlesim_yeri = response[:yerlesim_yeri_adresi]
-            address_root = if yerlesim_yeri[:il_ilce_merkez_adresi].present?
-                             yerlesim_yeri[:il_ilce_merkez_adresi]
-                           elsif yerlesim_yeri[:koy_adresi].present?
-                             yerlesim_yeri[:koy_adresi]
-                           elsif yerlesim_yeri[:yurt_disi_adresi].present?
-                             yerlesim_yeri[:yurt_disi_adresi]
-                           end
-            # return a hash, ready to use for building an Address.
-            address_information = {
-              full_address: yerlesim_yeri[:acik_adres],
-              district_id: address_root[:ilce_kodu].to_i
-            }
-            address_information
-          end
+          raise IdNumberError if response[:hata_bilgisi].present?
+
+          yerlesim_yeri = response[:yerlesim_yeri_adresi]
+          address_root = if yerlesim_yeri[:il_ilce_merkez_adresi].present?
+                            yerlesim_yeri[:il_ilce_merkez_adresi]
+                          elsif yerlesim_yeri[:koy_adresi].present?
+                            yerlesim_yeri[:koy_adresi]
+                          elsif yerlesim_yeri[:yurt_disi_adresi].present?
+                            yerlesim_yeri[:yurt_disi_adresi]
+                          end
+          
+          # return a hash, ready to use for building an Address.
+          address_information = {
+            full_address: yerlesim_yeri[:acik_adres],
+            district_id: address_root[:ilce_kodu].to_i
+          }
+          address_information
         end
       end
     end
