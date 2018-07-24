4e84defc9246418ce6ca91b139391782a50c9789
Sinan Sahin <snansahin1@gmail.com>
2018-07-24 17:50:41 +0300

    Add: Import from MERNIS

--- app/controllers/account/addresses_controller.rb
+++ app/controllers/account/addresses_controller.rb
@@ -37,6 +37,15 @@ module Account
       end
     end
 
+    def import_from_mernis
+      if current_user.addresses.formal.blank?
+        KpsAddressCreateJob.perform_later(current_user)
+        redirect_with('will_update')
+      else
+        redirect_with('wait')
+      end
+    end
+
     private
 
     def check_formality
