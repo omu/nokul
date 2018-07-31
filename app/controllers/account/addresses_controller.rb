821c01e11728535882f5b163741a66a783410c72
msdundar <msdundars@gmail.com>
2018-08-01 02:26:31 +0300

    Tidy up identities_controller. Prefer guard clause

--- app/controllers/account/addresses_controller.rb
+++ app/controllers/account/addresses_controller.rb
@@ -4,7 +4,6 @@ module Account
   class AddressesController < ApplicationController
     before_action :set_address, only: %i[edit update destroy]
     before_action :check_formality, only: %i[edit update destroy]
-    before_action :set_formal_address, only: %i[save_from_mernis]
     before_action :set_elapsed_time, only: %i[save_from_mernis]
 
     def index
@@ -31,34 +30,30 @@ module Account
     end
 
     def save_from_mernis
-      if @elapsed_time.present? && @elapsed_time < 7
-        redirect_with('wait')
-      else
-        KpsAddressSaveJob.perform_later(current_user)
-        redirect_with('will_update')
-      end
+      KpsAddressSaveJob.perform_later(current_user)
+      redirect_with('will_update')
     end
 
     private
 
-    def set_formal_address
-      @address = current_user.addresses.formal.presence ? current_user.addresses.formal : nil
-    end
-
-    def set_elapsed_time
-      @elapsed_time = (Time.zone.now - @address.first.updated_at) / 1.day if @address.present?
+    def set_address
+      @address = current_user.addresses.find(params[:id])
     end
 
     def check_formality
       redirect_with('warning') if @address.formal?
     end
 
-    def redirect_with(message)
-      redirect_to(addresses_path, notice: t(".#{message}"))
+    def set_elapsed_time
+      formal_address = current_user.addresses.formal
+      return unless formal_address.present?
+
+      elapsed_time = (Time.zone.now - formal_address.first.updated_at) / 1.day
+      redirect_with('wait') if elapsed_time.present? && elapsed_time < 7
     end
 
-    def set_address
-      @address = current_user.addresses.find(params[:id])
+    def redirect_with(message)
+      redirect_to(addresses_path, notice: t(".#{message}"))
     end
 
     def address_params
--- app/controllers/account/identities_controller.rb
+++ app/controllers/account/identities_controller.rb
@@ -4,7 +4,6 @@ module Account
   class IdentitiesController < ApplicationController
     before_action :set_identity, only: %i[edit update destroy]
     before_action :check_formality, only: %i[edit update destroy]
-    before_action :set_formal_identities, only: %i[save_from_mernis]
     before_action :set_elapsed_time, only: %i[save_from_mernis]
 
     def index
@@ -31,34 +30,30 @@ module Account
     end
 
     def save_from_mernis
-      if @elapsed_time.present? && @elapsed_time < 7
-        redirect_with('wait')
-      else
-        KpsIdentitySaveJob.perform_later(current_user)
-        redirect_with('will_update')
-      end
+      KpsIdentitySaveJob.perform_later(current_user)
+      redirect_with('will_update')
     end
 
     private
 
-    def set_formal_identities
-      @identities = current_user.identities.formal.presence ? current_user.identities.formal : nil
-    end
-
-    def set_elapsed_time
-      @elapsed_time = (Time.zone.now - @identities.first.updated_at) / 1.day if @identities.present?
+    def set_identity
+      @identity = current_user.identities.find(params[:id])
     end
 
     def check_formality
       redirect_with('warning') if @identity.formal?
     end
 
-    def redirect_with(message)
-      redirect_to(identities_path, notice: t(".#{message}"))
+    def set_elapsed_time
+      formal_identity = current_user.identities.formal
+      return unless formal_identity.present?
+
+      elapsed_time = (Time.zone.now - formal_identity.first.updated_at) / 1.day
+      redirect_with('wait') if elapsed_time.present? && elapsed_time < 7
     end
 
-    def set_identity
-      @identity = current_user.identities.find(params[:id])
+    def redirect_with(message)
+      redirect_to(identities_path, notice: t(".#{message}"))
     end
 
     def identity_params
