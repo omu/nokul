e5cf599fb6c9e348b04d2110e3e2c08523f9a181
msdundar <msdundars@gmail.com>
2018-08-01 03:11:01 +0300

    Move common behavior to concern

---
+++ app/controllers/concerns/last_update_from_mernis.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module LastUpdateFromMernis
+  extend ActiveSupport::Concern
+
+  def elapsed_time(resource)
+    elapsed_time = (Time.zone.now - resource.first.updated_at) / 1.day
+    redirect_with('wait') if elapsed_time.present? && elapsed_time < 7
+  end
+end
