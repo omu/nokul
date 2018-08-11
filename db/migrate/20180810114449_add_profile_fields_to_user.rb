cffc20c15855d09c426d7a6512086ba8ccb9780b
msdundar <msdundars@gmail.com>
2018-08-11 03:04:52 +0300

    Use a single profile column

--- db/migrate/20180810114449_add_profile_fields_to_user.rb
+++ db/migrate/20180810114449_add_profile_fields_to_user.rb
@@ -1,6 +1,5 @@
 class AddProfileFieldsToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :profile, :jsonb
-    add_column :users, :preferences, :jsonb
+    add_column :users, :profile_preferences, :jsonb
   end
 end
