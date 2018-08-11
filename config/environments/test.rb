e716680bf9dac67fdf65f3b28830892d336adae9
M. Serhat Dündar <msdundars@gmail.com>
2018-08-11 23:04:49 +0300

    .

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -32,6 +32,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system in a temporary directory
   config.active_storage.service = :test
 
+  # Use inline job processing to make things happen immediately
+  config.active_job.queue_adapter = :inline
+
   config.action_mailer.perform_caching = false
 
   # Tell Action Mailer not to deliver emails to the real world.
