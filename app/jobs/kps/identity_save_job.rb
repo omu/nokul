9b7f6b7ebcc9e0476a07e83035b6d5826b14b3aa
M. Serhat Dündar <msdundars@gmail.com>
2018-09-04 00:53:42 +0300

    Reorganize KPS jobs under a module

---
+++ app/jobs/kps/identity_save_job.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Kps
+  class IdentitySaveJob < ApplicationJob
+    queue_as :high
+
+    # slow operation
+    def perform(user, student_id = nil)
+      @student_id = student_id
+      @response = Services::Kps::Omu::Kimlik.new.sorgula(user.id_number.to_i)
+    end
+
+    # callbacks
+    after_perform do |job|
+      response = @response.merge(student_id: @student_id)
+      formal_address = job.arguments.first.identities.formal
+      formal_address.present? ? formal_address.update(response) : formal_address.create(response)
+    end
+  end
+end
--- test/jobs/kps_identity_job_test.rb
+++ test/jobs/kps_identity_job_test.rb
@@ -3,29 +3,33 @@
 require 'test_helper'
 
 class KpsIdentityJobTest < ActiveJob::TestCase
-  test 'can enqueue KpsIdentitySaveJob' do
+  test 'can enqueue Kps::IdentitySaveJob' do
     assert_enqueued_jobs 0
-    KpsIdentitySaveJob.perform_later(users(:serhat))
+    Kps::IdentitySaveJob.perform_later(users(:serhat))
     assert_enqueued_jobs 1
   end
 
-  test 'can initialize KPS queries as KpsIdentitySaveJob for given user and identities' do
+  test 'can initialize KPS queries as Kps::IdentitySaveJob for given user and identities' do
     client = Minitest::Mock.new
     def client.sorgula
       true
     end
 
     Services::Kps::Omu::Kimlik.stub :new, client do
-      assert KpsIdentitySaveJob.perform_later({}) # dummy hash
+      assert Kps::IdentitySaveJob.perform_later({}) # dummy hash
     end
   end
 
-  test 'can perform enqueued jobs for KpsIdentitySaveJob' do
+  test 'can perform enqueued jobs for Kps::IdentitySaveJob' do
     skip 'this block on CircleCI since it needs IP permissions to run.' if ENV['CI']
     assert_performed_jobs 0
     perform_enqueued_jobs do
-      KpsIdentitySaveJob.perform_later(users(:serhat))
+      Kps::IdentitySaveJob.perform_later(users(:serhat))
     end
     assert_performed_jobs 1
+  end
+
+  test 'KPS::Identity jobs runs in the high priority queue' do
+    assert_equal Kps::IdentitySaveJob.new.queue_name, 'high'
   end
 end
