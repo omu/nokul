2958cfca04f6768f391d404a1f0194838c4fcf89
isubas <irfan.subas@uzem.omu.edu.tr>
2018-09-18 20:53:21 +0300

    Müfredat için testleri oluştur

---
+++ test/controllers/course_management/curriculums_controller_test.rb
@@ -0,0 +1,91 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Curriculums
+  class CurriculumsControllerTest < ActionDispatch::IntegrationTest
+    setup do
+      sign_in users(:serhat)
+      @curriculum = curriculums(:one)
+      @unit = units(:omu)
+    end
+
+    test 'should get index' do
+      get index_path
+      assert_response :success
+      assert_select '#add-button', translate('.index.add_new_curriculum')
+      assert_select '#collapseSmartSearchLink', t('smart_search')
+      assert_select '#collapseDetailedSearchLink', t('detailed_search')
+    end
+
+    test 'should get show' do
+      get curriculum_path(@curriculum)
+      assert_response :success
+    end
+
+    test 'should get new' do
+      get new_curriculum_path
+      assert_response :success
+    end
+
+    test 'should create curriculum' do
+      assert_difference('Curriculum.count') do
+        post index_path, params: {
+          curriculum: {
+            name: 'Test Create Curriculum', unit_id: @unit.id, number_of_semesters: 4, status: :active
+          }
+        }
+      end
+
+      curriculum = Curriculum.last
+
+      assert_equal 'Test Create Curriculum', curriculum.name
+      assert_equal 4, curriculum.number_of_semesters
+      assert curriculum.active?
+      assert_redirected_to index_path
+      assert_equal translate('.create.success'), flash[:notice]
+    end
+
+    test 'should get edit' do
+      get edit_curriculum_path(@curriculum)
+      assert_response :success
+      assert_select '.card-header strong', translate('.edit.form_title')
+    end
+
+    test 'should update curriculum' do
+      curriculum = Curriculum.last
+      patch curriculum_path(curriculum), params: {
+        curriculum: {
+          name: 'Test Update Curriculum', number_of_semesters: 4, status: :passive
+        }
+      }
+
+      curriculum.reload
+
+      assert_equal 'Test Update Curriculum', curriculum.name
+      assert_equal 4, curriculum.number_of_semesters
+      assert curriculum.passive?
+      assert_redirected_to index_path
+      assert_equal translate('.update.success'), flash[:notice]
+    end
+
+    test 'should destroy curriculum' do
+      assert_difference('Curriculum.count', -1) do
+        delete curriculum_path(Curriculum.last)
+      end
+
+      assert_redirected_to index_path
+      assert_equal translate('.destroy.success'), flash[:notice]
+    end
+
+    private
+
+    def index_path
+      curriculums_path
+    end
+
+    def translate(key)
+      t("course_management.curriculums#{key}")
+    end
+  end
+end
---
+++ test/models/course_management/curriculum_test.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class CurriculumTest < ActiveSupport::TestCase
+  setup do
+    @curriculum = curriculums(:one)
+  end
+
+  # relations
+  test 'curriculum can communicate with unit' do
+    assert @curriculum.unit
+  end
+
+  # validations: presence
+  %i[
+    name
+    number_of_semesters
+    status
+    unit
+  ].each do |property|
+    test "presence validations for #{property} of a curriculum" do
+      @curriculum.send("#{property}=", nil)
+      assert_not @curriculum.valid?
+      assert_not_empty @curriculum.errors[property]
+    end
+  end
+
+  # validations: uniqueness
+  test 'uniqueness validations for name of a curriculum' do
+    fake = @curriculum.dup
+    assert_not fake.valid?
+    assert_not_empty fake.errors[:name]
+
+    fake.unit_id = units(:cbu).id
+    assert fake.valid?
+    assert_empty fake.errors[:name]
+  end
+
+  # validations: numericality
+  test 'numericality validations for number_of_semesters of a curriculum' do
+    @curriculum.number_of_semesters = 0
+    assert_not @curriculum.valid?
+    assert_not_empty @curriculum.errors[:number_of_semesters]
+  end
+
+  # enums
+  {
+    status: { passive: 0, active: 1 }
+  }.each do |property, hash|
+    hash.each do |key, value|
+      test "have a #{key} value of #{property} enum" do
+        enums = Curriculum.defined_enums.with_indifferent_access
+        assert_equal enums.dig(property, key), value
+      end
+    end
+  end
+end
