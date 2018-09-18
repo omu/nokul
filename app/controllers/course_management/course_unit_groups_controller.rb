10b4be97c80d00ebbba4629834ac3872944b8ba3
ecmelkytz <ecmel.kytz@gmail.com>
2018-09-18 21:40:16 +0300

    Add course_unit_group link to sidebar

---
+++ app/controllers/course_management/course_unit_groups_controller.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module CourseManagement
+  class CourseUnitGroupsController < ApplicationController
+    before_action :set_course_unit_group, only: %i[edit update destroy]
+
+    def index
+      @course_unit_groups = pagy_by_search(CourseUnitGroup.all)
+    end
+
+    def new
+      @course_unit_group = CourseUnitGroup.new
+    end
+
+    def create
+      @course_unit_group = CourseUnitGroup.new(course_unit_group_params)
+      @course_unit_group.save ? redirect_with('success') : render(:new)
+    end
+
+    def edit; end
+
+    def update
+      @course_unit_group.update(course_unit_group_params) ? redirect_with('success') : render(:edit)
+    end
+
+    def destroy
+      @course_unit_group.destroy ? redirect_with('success') : redirect_with('warning')
+    end
+
+    private
+
+    def redirect_with(message)
+      redirect_to(course_unit_groups_path, notice: t(".#{message}"))
+    end
+
+    def set_course_unit_group
+      @course_unit_group = CourseUnitGroup.find(params[:id])
+    end
+
+    def course_unit_group_params
+      params.require(:course_unit_group).permit(:name)
+    end
+  end
+end
