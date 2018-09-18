1048be4bbbe1042a56d5cccafe414bf6157d165d
isubas <irfan.subas@uzem.omu.edu.tr>
2018-09-18 20:52:17 +0300

    Müfredat için controller ve views oluştur

---
+++ app/controllers/course_management/curriculums_controller.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module CourseManagement
+  class CurriculumsController < ApplicationController
+    before_action :set_curriculum, only: %i[show edit update destroy]
+
+    def index
+      @pagy, @curriculums = pagy(
+        Curriculum.all.dynamic_search(search_params(Curriculum))
+      )
+    end
+
+    def show; end
+
+    def new
+      @curriculum = Curriculum.new
+    end
+
+    def edit; end
+
+    def create
+      @curriculum = Curriculum.new(curriculum_params)
+      @curriculum.save ? redirect_with('success') : render(:new)
+    end
+
+    def update
+      @curriculum.update(curriculum_params) ? redirect_with('success') : render(:edit)
+    end
+
+    def destroy
+      message = @curriculum.destroy ? 'success' : 'error'
+      redirect_with(message)
+    end
+
+    private
+
+    def redirect_with(message)
+      redirect_to curriculums_path, flash: { notice: t(".#{message}") }
+    end
+
+    def set_curriculum
+      @curriculum = Curriculum.find(params[:id])
+    end
+
+    def curriculum_params
+      params.require(:curriculum).permit(:name, :unit_id, :number_of_semesters, :status)
+    end
+  end
+end
