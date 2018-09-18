da8f8a4b33b1a51ae4edabdca40f84fc67ba9fe8
isubas <irfan.subas@uzem.omu.edu.tr>
2018-09-18 20:48:42 +0300

    Müfredat tanımı için model oluştur

---
+++ app/models/course_management/curriculum.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class Curriculum < ApplicationRecord
+  # search
+  include PgSearch
+  include DynamicSearch
+
+  pg_search_scope(
+    :search,
+    against: %i[name],
+    using: { tsearch: { prefix: true } }
+  )
+
+  # dynamic_search
+  search_keys :unit_id, :status
+
+  # relations
+  belongs_to :unit
+
+  # validations
+  validates :name, presence: true, uniqueness: { scope: :unit_id }
+  validates :number_of_semesters, numericality: { greater_than: 0 }
+  validates :status, presence: true
+
+  # enumerations
+  enum status: { passive: 0, active: 1 }
+end
