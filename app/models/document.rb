005a0467d255672fdf8cadaa52ecf15d4d2ce03b
Dilara Koca <adilarakoca@gmail.com>
2018-09-19 16:18:05 +0300

    Registration Document modeli ekle

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Document < ApplicationRecord
+  # relations
+  has_many :registration_documents, dependent: :destroy
+
   # validations
   validates :name, presence: true, uniqueness: true
 end
