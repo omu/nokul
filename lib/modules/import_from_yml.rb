fffafd9000e0dc9c931688eba6dd76d5efea6fc2
Recai Oktaş <roktas@gmail.com>
2018-09-21 12:18:21 +0300

    Prefer module instead of class for stateless code

--- lib/modules/import_from_yml.rb
+++ lib/modules/import_from_yml.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ImportFromYml
+module ImportFromYml
   def self.parse(klass)
     file = YAML.load_file(Rails.root.join('db', 'static_data', "#{klass.tableize}.yml"))
     progress_bar = ProgressBar.spawn(klass.pluralize.to_s, file.count)
