d292534eaa801255d40c9302419337ee078019b2
isubas <irfan.subas@uzem.omu.edu.tr>
2018-09-21 15:21:34 +0300

    Fixes rubocop offense

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,7 +3,7 @@
 class UnitsController < ApplicationController
   include PagyBackendWithHelpers
   before_action :set_unit, only: %i[edit update destroy show courses programs]
-  
+
   def index
     units = Unit.includes(
       :unit_status, :unit_instruction_language, :unit_instruction_type, :unit_type, district: [:city]
