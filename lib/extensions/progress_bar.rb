d4e2f0756ca5a6a2219658818605d53fd72cd915
msdundar <msdundars@gmail.com>
2018-09-22 03:31:36 +0300

    Enable blackhole output in ruby-progressbar

--- lib/extensions/progress_bar.rb
+++ lib/extensions/progress_bar.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+require 'ruby-progressbar/outputs/null'
+
 class ProgressBar
-  def self.spawn(title, total)
-    ProgressBar.create(title: title, total: total, format: '%t %B %c/%C %a')
+  def self.spawn(title, total, output = $stdout)
+    ProgressBar.create(title: title, total: total, format: '%t %B %c/%C %a', output: output)
   end
 end
