3375eca5ecec746c2565fadbdcad8330cb948ad7
msdundar <msdundars@gmail.com>
2018-09-24 05:08:37 +0300

    Add tests for high school types

--- test/models/yoksis_reference/high_school_type.rb
+++ test/models/yoksis_reference/high_school_type.rb
@@ -6,8 +6,12 @@ class HighSchoolTypeTest < ActiveSupport::TestCase
   include ReferenceCallbacksTest
   include ReferenceValidationsTest
 
+  setup do
+    @object = high_schools_types(:aksam_lisesi)
+  end
+
   # relations
   test 'high_school_type can communicate with prospective students' do
-    assert high_schools(:aksam_lisesi).prospective_students
+    assert high_schools_types(:aksam_lisesi).prospective_students
   end
 end
