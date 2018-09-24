21c2ba88a3e95e2f263fe21230d53ce041e1ffe9
msdundar <msdundars@gmail.com>
2018-09-24 16:53:50 +0300

    Remove leftover routes

--- config/routes.rb
+++ config/routes.rb
@@ -81,23 +81,6 @@ Rails.application.routes.draw do
   get '/profiles/:id', to: 'public_profile#show'
   get '/profiles/:id/vcard',  to: 'public_profile#vcard', as: :profile_vcard
 
-  scope module: :yoksis_references do
-    resources :student_disability_types, except: :show
-    resources :student_drop_out_types, except: :show
-    resources :student_education_levels, except: :show
-    resources :student_entrance_point_types, except: :show
-    resources :student_entrance_types, except: :show
-    resources :student_grades, except: :show
-    resources :student_grading_systems, except: :show
-    resources :student_punishment_types, except: :show
-    resources :student_studentship_statuses, except: :show
-    resources :unit_instruction_languages, except: :show
-    resources :unit_instruction_types, except: :show
-    resources :unit_statuses, except: :show
-    resources :unit_types, except: :show
-    resources :university_types, except: :show
-  end
-
   scope module: :studies do
     get '/studies', to: 'dashboard#index'
     get '/studies/articles', to: 'articles#index'
