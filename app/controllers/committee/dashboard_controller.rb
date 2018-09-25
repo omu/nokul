0b118c09874e52b05e3a456d0c5057f11b3991da
ecmelkytz <ecmel.kytz@gmail.com>
2018-09-25 15:58:40 +0300

    Add agenda file show link to dashboard

--- app/controllers/committee/dashboard_controller.rb
+++ app/controllers/committee/dashboard_controller.rb
@@ -13,7 +13,7 @@ module Committee
     def show
       @committee = Unit.committees.find(params[:id])
       @agendas = pagy_by_search(
-        @committee.agendas.includes(:agenda_type)
+        @committee.agendas.includes(:agenda_type, agenda_file_attachment: :blob)
       )
     end
   end
