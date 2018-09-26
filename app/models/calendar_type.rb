278c6cec54a3c21430c863e2c09bf64991928a44
M. Serhat Dündar <msdundars@gmail.com>
2018-09-26 08:40:15 +0300

    Add missing relation between calendar_type and academic_calendar
    Fixes #374

--- app/models/calendar_type.rb
+++ app/models/calendar_type.rb
@@ -4,6 +4,7 @@ class CalendarType < ApplicationRecord
   # relations
   has_many :calendar_title_types, foreign_key: :type_id, inverse_of: :type, dependent: :destroy
   has_many :titles, through: :calendar_title_types
+  has_many :academic_calendars, dependent: :destroy
   accepts_nested_attributes_for :calendar_title_types, reject_if: :all_blank, allow_destroy: true
 
   # validations
