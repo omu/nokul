973a767c4c4657971a5722d13af0dde5ba375f2a
Emre KURT <emre.kurt@bil.omu.edu.tr>
2018-09-26 19:35:57 +0300

    Referanslar için dashboard oluştur

--- config/routes/references.rb
+++ config/routes/references.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 scope module: :references do
+  get 'references', to: 'home#index'
   resources :countries do
     resources :cities, except: [:index] do
       resources :districts, except: %i[show index] do
