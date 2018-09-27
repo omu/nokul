f830f525093855e4cbd7f9e516cc887077cfb98d
M. Serhat Dündar <msdundars@gmail.com>
2018-09-28 00:56:29 +0300

    Configure capybara in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,11 +14,16 @@ Codacy::Reporter.start if ENV['CI']
 require 'minitest/autorun'
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'capybara/rails'
+require 'capybara/minitest'
 
 module ActiveSupport
   class TestCase
     include AbstractController::Translation
     include Devise::Test::IntegrationHelpers
+    include Capybara::DSL
+    include Capybara::Minitest::Assertions
+
     fixtures :all
 
     def controller_name
@@ -29,6 +34,14 @@ module ActiveSupport
       ActiveStorage::Blob.create_after_upload!(
         io: file_fixture(filename).open, filename: filename, content_type: content_type, metadata: metadata
       )
+    end
+
+    Capybara.default_driver = :selenium_chrome_headless
+    Capybara.server = :puma, { Silent: true }
+
+    def teardown
+      Capybara.reset_sessions!
+      Capybara.use_default_driver
     end
   end
 end
