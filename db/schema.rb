ae59e91cb98863ff7ec9bf277152e5b58135fb57
M. Serhat Dündar <msdundars@gmail.com>
2018-09-27 04:17:53 +0300

    Create controller actions for student registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_26_182120) do
+ActiveRecord::Schema.define(version: 2018_09_26_213938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -470,6 +470,7 @@ ActiveRecord::Schema.define(version: 2018_09_26_182120) do
     t.bigint "unit_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "permanently_registered", default: false, null: false
     t.index ["unit_id"], name: "index_students_on_unit_id"
     t.index ["user_id"], name: "index_students_on_user_id"
   end
