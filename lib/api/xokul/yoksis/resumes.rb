16dfe8047ee9ec5a4e682ab4752f7688175fdb09
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-10-02 17:58:12 +0300

    Add all actions under Resumes endpoint

---
+++ lib/api/xokul/yoksis/resumes.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+module Xokul
+  module Yoksis
+    module Resumes
+      module_function
+
+      def academic_duties(id_number:)
+        connection.get "/yoksis/resumes/#{__callee__}", params: { id_number: id_number }
+      end
+
+      def authors(author_id:)
+        connection.get '/yoksis/resumes/authors', params: { author_id: author_id }
+      end
+
+      def citations(id_number:, year:)
+        connection.get '/yoksis/resumes/citations', params: { id_number: id_number, year: year }
+      end
+
+      class << self
+        alias academic_links                  academic_duties
+        alias administrative_duties           academic_duties
+        alias articles                        academic_duties
+        alias artistic_activities             academic_duties
+        alias awards                          academic_duties
+        alias books                           academic_duties
+        alias certifications                  academic_duties
+        alias designs                         academic_duties
+        alias editorships                     academic_duties
+        alias education_informations          academic_duties
+        alias fields                          academic_duties
+        alias foreign_languages               academic_duties
+        alias lectures                        academic_duties
+        alias memberships                     academic_duties
+        alias other_experiences               academic_duties
+        alias papers                          academic_duties
+        alias patents                         academic_duties
+        alias projects                        academic_duties
+        alias refereeing                      academic_duties
+        alias thesis_advisors                 academic_duties
+        alias incentive_applications          citations
+        alias incentive_activity_declarations citations
+      end
+
+      def connection
+        Connection.instance
+      end
+
+      private_class_method :connection
+    end
+  end
+end
