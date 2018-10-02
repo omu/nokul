604188d991de600f472f3e267f1e1cd4cfc1d1d2
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-10-02 17:55:23 +0300

    Reorganize api folder

--- lib/api/xokul/yoksis/references.rb
+++ lib/api/xokul/yoksis/references.rb
@@ -2,20 +2,6 @@
 
 module Xokul
   module Yoksis
-    module Graduates
-      module_function
-
-      def informations(id_number:)
-        connection.get '/yoksis/graduates/informations', params: { id_number: id_number }
-      end
-
-      def connection
-        Connection.instance
-      end
-
-      private_class_method :connection
-    end
-
     module References
       module_function
 
