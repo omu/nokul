c64628861355b51efd8c0a90bb66b473007f095a
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-10-02 17:59:01 +0300

    Add all actions under Students endpoint

---
+++ lib/api/xokul/yoksis/students.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Xokul
+  module Yoksis
+    module Students
+      module_function
+
+      def informations(id_number:)
+        connection.get '/yoksis/students/informations', params: { id_number: id_number }
+      end
+
+      def connection
+        Connection.instance
+      end
+
+      private_class_method :connection
+    end
+  end
+end
