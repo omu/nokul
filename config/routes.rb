dcd8c3d612344eaebf75b18c24e54e7d2f38c5fb
ecmelkytz <ecmel.kytz@gmail.com>
2018-10-03 10:50:37 +0300

    Tidy up meetings

--- config/routes.rb
+++ config/routes.rb
@@ -68,7 +68,7 @@ Rails.application.routes.draw do
   resources :committees, only: :index, controller: 'committee/dashboard' do
     scope module: :committee do
       resources :agendas, except: :show
-      resources :committee_meetings, except: :show, as: :meeting
+      resources :committee_meetings, except: :show, as: :meetings, path: :meetings
     end
   end
 end
