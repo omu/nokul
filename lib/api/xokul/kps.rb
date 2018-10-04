2d5cfff04100f0dfb3a3f1caa6cb81d8045525ec
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-10-04 17:06:48 +0300

    Use Connection instance directly

--- lib/api/xokul/kps.rb
+++ lib/api/xokul/kps.rb
@@ -5,17 +5,11 @@ module Xokul
     module_function
 
     def identity(id_number:)
-      connection.get '/kps/queries/identities', params: { id_number: id_number }
+      Connection.instance.get '/kps/queries/identities', params: { id_number: id_number }
     end
 
     def address(id_number:)
-      connection.get '/kps/queries/addresses',  params: { id_number: id_number }
+      Connection.instance.get '/kps/queries/addresses',  params: { id_number: id_number }
     end
-
-    def connection
-      Connection.instance
-    end
-
-    private_class_method :connection
   end
 end
--- lib/api/xokul/yoksis/students.rb
+++ lib/api/xokul/yoksis/students.rb
@@ -6,14 +6,8 @@ module Xokul
       module_function
 
       def informations(id_number:)
-        connection.get '/yoksis/students/informations', params: { id_number: id_number }
+        Connection.instance.get '/yoksis/students/informations', params: { id_number: id_number }
       end
-
-      def connection
-        Connection.instance
-      end
-
-      private_class_method :connection
     end
   end
 end
