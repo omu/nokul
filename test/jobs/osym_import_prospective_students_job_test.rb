7cf2b480b476c4486df1a46adab6e106e6893f72
Dilara Koca <adilarakoca@gmail.com>
2018-10-04 13:27:30 +0300

    Test coverage için tekrar düzenle

--- test/jobs/osym_import_prospective_students_job_test.rb
+++ test/jobs/osym_import_prospective_students_job_test.rb
@@ -10,11 +10,18 @@ class OsymImportProspectiveStudentsJobTest < ActiveJob::TestCase
   end
 
   test 'can perform enqueued jobs for Osym::ImportProspectiveStudentsJob' do
+    client = Minitest::Mock.new
+    def client.program_name(*)
+      { universite: { ad: 'Üniversite' }, birim: { ad: 'Birim' } }
+    end
+
     assert_performed_jobs 0
 
     assert_difference('ProspectiveStudent.count', 3) do
       perform_enqueued_jobs do
-        Osym::ImportProspectiveStudentsJob.perform_later('test/fixtures/files/prospective_students.csv.enc')
+        Yoksis::V4::UniversiteBirimler.stub :new, client do
+          Osym::ImportProspectiveStudentsJob.perform_later('test/fixtures/files/prospective_students.csv.enc')
+        end
       end
 
       assert_performed_jobs 1
