b549365f3e3694bf47e2b067045d0407e37b9bdf
msdundar <msdundars@gmail.com>
2018-10-05 00:11:51 +0300

    Remove outputs/null from progressbar

--- lib/support/ext/progress_bar.rb
+++ lib/support/ext/progress_bar.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'ruby-progressbar/outputs/null'
-
 class ProgressBar
   def self.spawn(title, total, output = $stdout)
     ProgressBar.create(title: title, total: total, format: '%t %B %c/%C %a', output: output) if Rails.env.development?
