32063498ab15cccdbd3647312f16a0d6c37c4782
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-10-04 21:57:16 +0300

    Add Staff endpoint for Yoksis service

---
+++ lib/api/xokul/yoksis/staff.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Xokul
+  module Yoksis
+    module Staff
+      module_function
+
+      def academicians(id_number:)
+        Connection.instance.get '/yoksis/staff/academicians', params: { id_number: id_number }
+      end
+
+      def nationalities
+        Connection.instance.get '/yoksis/staff/nationalities'
+      end
+
+      def pages(page:)
+        Connection.instance.get '/yoksis/staff/pages', params: { page: page }
+      end
+    end
+  end
+end
