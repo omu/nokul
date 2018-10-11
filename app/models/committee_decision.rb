f24301cd0e5d4f69b1729b1ef17aba9df1e22553
ecmelkytz <ecmel.kytz@gmail.com>
2018-10-11 13:54:26 +0300

    Generate automatic of decision number

--- app/models/committee_decision.rb
+++ app/models/committee_decision.rb
@@ -7,4 +7,16 @@ class CommitteeDecision < ApplicationRecord
   # validations
   validates :description, presence: true
   validates :decision_no, presence: true, uniqueness: true
+  validates :year, presence: true
+
+  # callbacks
+  before_validation { self.year = meeting_agenda_year }
+  before_validation { self.decision_no = "#{year}/#{count_of_decisions_by_year(year) + 1}" }
+
+  # delegates
+  delegate :meeting_no, :meeting_date, :year, :unit, to: :meeting_agenda, prefix: true
+
+  def count_of_decisions_by_year(year)
+    meeting_agenda_unit.decisions.where(year: year).count
+  end
 end
