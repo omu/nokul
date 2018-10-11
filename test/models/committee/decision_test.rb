6b7cdc440b049c77c7f9d630962472b89df6901a
ecmelkytz <ecmel.kytz@gmail.com>
2018-10-11 13:54:46 +0300

    Refactor decision test

--- test/models/committee/decision_test.rb
+++ test/models/committee/decision_test.rb
@@ -15,7 +15,6 @@ class DecisionTest < ActiveSupport::TestCase
   # validations: presence
   %i[
     description
-    decision_no
   ].each do |property|
     test "presence validations for #{property} of a decision" do
       @decision.send("#{property}=", nil)
@@ -24,9 +23,27 @@ class DecisionTest < ActiveSupport::TestCase
     end
   end
 
-  # validations: uniqueness
-  test 'uniqueness validations for decision_no of a desicion' do
-    fake_decision = @decision.dup
-    assert_not fake_decision.valid?
+  # delegates
+  %i[
+    meeting_agenda_meeting_no
+    meeting_agenda_meeting_date
+    meeting_agenda_year
+    meeting_agenda_unit
+  ].each do |property|
+    test "a decision reach committee_meeting's #{property} parameter" do
+      assert @decision.send(property)
+    end
+  end
+
+  # callbacks
+  test 'before initialize callback must run for year and decision_no attribute' do
+    decision = CommitteeDecision.create(description: 'Test Karar', meeting_agenda: meeting_agendas(:one))
+    assert_equal 2018, decision.year
+    assert_equal '2018/4', decision.decision_no
+  end
+
+  # custom
+  test 'count_of_decisions_by_year return decision count by year' do
+    assert_equal 3, @decision.count_of_decisions_by_year(2018)
   end
 end
