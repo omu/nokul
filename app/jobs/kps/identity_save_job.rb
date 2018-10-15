6308c035cb809d7fbd40edad21ddbd28cff4389e
Dilara Koca <adilarakoca@gmail.com>
2018-10-15 14:35:44 +0300

    Kps entegrasyonunu xokul'a taşı

--- app/jobs/kps/identity_save_job.rb
+++ app/jobs/kps/identity_save_job.rb
@@ -7,15 +7,15 @@ module Kps
     # slow operation
     def perform(user, student_id = nil)
       @student_id = student_id
-      @response = Kps::Omu::Kimlik.new.sorgula(user.id_number.to_i)
+      @response = Xokul::Kps::Identity.new(user.id_number.to_i)
     end
 
     # callbacks
     after_perform do |job|
-      response = @response.merge(student_id: @student_id, type: 'formal')
+      model_data = @response.model_data.merge(student_id: @student_id, type: 'formal')
       user = job.arguments.first
       formal_identity = user.identities.user_identity
-      formal_identity.present? ? formal_identity.update(response) : user.identities.create(response)
+      formal_identity.present? ? formal_identity.update(model_data) : user.identities.create(model_data)
     end
   end
 end
