51ade866ee98210dda0059661c7faf85e0f61f5e
Dilara Koca <adilarakoca@gmail.com>
2018-10-19 12:00:33 +0300

    Yoksis jobs değişen keyler için düzenleme yap

--- app/jobs/yoksis/articles_create_job.rb
+++ app/jobs/yoksis/articles_create_job.rb
@@ -22,19 +22,18 @@ module Yoksis
           index: article[:index_id],
           title: article[:name],
           authors: article[:authors].join(', '),
-          number_of_authors: article[:number_of_author],
           country: article[:country_id],
           journal: article[:journal_name],
           language_of_publication: article[:publication_language_name],
           access_type: article[:access_type_id],
           keyword: article[:keywords],
           special_issue: article[:special_issue_id],
-          # sponsored_by: article[:sponsor],
           status: article[:activity_id],
           type: article[:type_id],
           **article.slice(
-            :city, :month, :year, :volume, :issue, :first_page, :last_page, :doi, :issn, :access_link,
-            :discipline, :special_issue_name, :author_id, :last_update, :incentive_point
+            :city, :month, :year, :volume, :issue, :first_page, :last_page,
+            :doi, :issn, :access_link, :discipline, :special_issue_name, :author_id,
+            :last_update, :incentive_point, :sponsored_by, :number_of_authors
           )
         )
       end
