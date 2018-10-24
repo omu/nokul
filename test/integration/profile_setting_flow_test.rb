7c99df36d2540adf3b0fc2cf0aefe432fb78a784
Sinan Sahin <snansahin1@gmail.com>
2018-10-24 16:30:58 +0300

    Ziyaret adresini güncelle

--- test/integration/profile_setting_flow_test.rb
+++ test/integration/profile_setting_flow_test.rb
@@ -6,8 +6,9 @@ class ProfileSettingFlowTest < ActionDispatch::IntegrationTest
   SUPPORTED_SCREEN_RESOLUTIONS.each do |resolution|
     test "can profile setting with correct credentials with a #{resolution} screen" do
       page.driver.browser.manage.window.resize_to(resolution[0], resolution[1])
-      login_as(users(:serhat), scope: :user, run_callbacks: false)
-      visit('/profile')
+      @user = User.find_by(id_number: users(:serhat).id_number)
+      login_as(@user, scope: :user, run_callbacks: false)
+      visit(user_profile_path(@user))
       attach_file('user[avatar]', File.absolute_path('test/fixtures/files/valid_jpg_picture.jpg'))
       fill_in('user[phone_number]', with: '362 312 1919')
       fill_in('user[extension_number]', with: '1234')
