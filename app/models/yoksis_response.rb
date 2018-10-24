ae26314ce0deb2eff72811df0014791ba9f53010
Dilara Koca <adilarakoca@gmail.com>
2018-10-24 12:36:31 +0300

    YoksisResponse modelini kaldır

--- app/models/yoksis_response.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class YoksisResponse < ApplicationRecord
-  # validations
-  validates :name, presence: true
-  validates :endpoint, presence: true
-  validates :action, presence: true
-  validates :sha1, presence: true, uniqueness: true, length: { is: 40 }
-end
