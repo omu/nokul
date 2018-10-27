f79fc96fec7ff928b3a777333a9b56e3a8d35bce
Recai Oktaş <roktas@gmail.com>
2018-10-27 12:56:10 +0300

    Refactor Tenant convention

--- lib/support/tenant/convention.rb
+++ lib/support/tenant/convention.rb
@@ -3,6 +3,32 @@
 module Tenant
   module_function
 
+  def active
+    ENV['RAILS_TENANT'] || 'omu'
+  end
+
+  def root
+    Pathname.new Rails.root.join('tenant')
+  end
+
+  def path(relative_path)
+    root.join(active, relative_path).to_s
+  end
+
+  def config_file
+    File.join(Path.config, 'config.yml')
+  end
+
+  def configuration
+    YAML.load_file(config_file).fetch(Rails.env, {}).to_deep_ostruct
+  end
+
+  def load_rules
+    [*Path.common_rules, *rules].each do |dir|
+      Dir[File.join(dir, '**', '*.rb')].each { |rule| require rule }
+    end
+  end
+
   module Path
     module_function
 
@@ -15,43 +41,5 @@ module Tenant
         Tenant.root.join('common', subdir, *paths).to_s
       end
     end
-  end
-
-  def active
-    ENV['RAILS_TENANT'] || 'omu'
-  end
-
-  def config_file
-    File.join(Path.config, 'config.yml')
-  end
-
-  def configuration
-    YAML.load_file(config_file).fetch(Rails.env, {}).to_deep_ostruct
-  end
-
-  def root
-    Pathname.new Rails.root.join('tenant')
-  end
-
-  def file(relative_path)
-    root.join(active, relative_path).to_s
-  end
-
-  def rules
-    File.join(Path.app, 'rules')
-  end
-
-  def common_rules
-    File.join(Path.common_app, 'rules')
-  end
-
-  # def load_rules
-  #   [*Path.common_rules, *rules].each do |dir|
-  #     Dir[File.join(dir, '**', '*.rb')].each { |rule| require rule }
-  #   end
-  # end
-
-  def init
-    # load_rules
   end
 end
