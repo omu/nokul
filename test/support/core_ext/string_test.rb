65d99b6ee23497ea067afe40a9c8afcb899b2792
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-11-03 13:57:11 +0300

    Add unit test for titleize_tr

--- test/support/core_ext/string_test.rb
+++ test/support/core_ext/string_test.rb
@@ -3,9 +3,17 @@
 require 'test_helper'
 
 class StringTest < ActiveSupport::TestCase
-  test 'capitalize_all method can capitalize words in Turkish' do
-    word = 'ışık ılık süt iç'
-    assert_equal word.capitalize_all, 'Işık Ilık Süt İç'
+  test 'titleize_tr method can titleize sentences in Turkish' do
+    sentences = {
+      'ışık ılık süt iç' => 'Işık Ilık Süt İç',
+      'İşik ILIK sÜt (iç)' => 'İşik Ilık Süt (İç)',
+      'foo "bar"' => 'Foo "Bar"',
+      'Işık süt ve BALI karıŞtıRarak /İÇ' => 'Işık Süt ve Balı Karıştırarak /İç'
+    }
+
+    sentences.each do |senctence, expected|
+      assert_equal senctence.titleize_tr, expected
+    end
   end
 
   test 'upcase_tr method can upcase words in Turkish' do
