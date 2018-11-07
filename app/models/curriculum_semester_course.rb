f2807291bf72103816df856f51a388116ae30ab7
isubas <irfan.subas@uzem.omu.edu.tr>
2018-11-07 17:04:43 +0300

    Müfredat derslerinde ufak iyileştirmeler

--- app/models/curriculum_semester_course.rb
+++ app/models/curriculum_semester_course.rb
@@ -5,6 +5,9 @@ class CurriculumSemesterCourse < ApplicationRecord
   belongs_to :course
   belongs_to :curriculum_semester
 
+  # validations
+  validates :ects, presence: true, numericality: { greater_than: 0 }
+
   # delegates
   delegate :code, :credit, :name, to: :course
 end
