673297d10b904d33b5c1c75fc01a0bdfd0309155
M. Serhat Dündar <msdundars@gmail.com>
2018-11-13 19:45:42 +0300

    Add string limit to committee_decisions
    Fixes #529

--- db/migrate/20181019105705_rename_number_of_semesters_to_curriculum.rb
+++ db/migrate/20181019105705_rename_number_of_semesters_to_curriculum.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameNumberOfSemestersToCurriculum < ActiveRecord::Migration[5.2]
   def up
     rename_column :curriculums, :number_of_semesters, :semesters_count
