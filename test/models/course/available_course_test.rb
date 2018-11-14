fbc8bf33001ec00aa16a4fb8d1b588377cfcdc89
ecmelkytz <ecmel.kytz@gmail.com>
2018-11-14 22:48:17 +0300

    Açılan dersler müfredat ve akademik döneme göre tekil olsun

--- test/models/course/available_course_test.rb
+++ test/models/course/available_course_test.rb
@@ -14,4 +14,16 @@ class AvailableCourseTest < ActiveSupport::TestCase
       assert available_courses(:ati_fall_2017_2018).send(property)
     end
   end
+
+  # validations: uniqueness
+  test 'uniqueness validations for course of a academic term and curriculum' do
+    fake = available_courses(:ati_fall_2017_2018).dup
+    assert_not fake.valid?
+    assert_not_empty fake.errors[:course]
+    fake.academic_term = academic_terms(:spring_2017_2018)
+    assert fake.valid?
+    fake.academic_term = academic_terms(:fall_2017_2018)
+    fake.curriculum = curriculums(:two)
+    assert fake.valid?
+  end
 end
