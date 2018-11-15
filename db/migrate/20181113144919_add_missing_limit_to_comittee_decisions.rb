21a21e1679ec68b299d8aaef6858a9648e206838
isubas <irfan.subas@uzem.omu.edu.tr>
2018-11-15 17:26:51 +0300

    Rollback hatasını düzelt

--- db/migrate/20181113144919_add_missing_limit_to_comittee_decisions.rb
+++ db/migrate/20181113144919_add_missing_limit_to_comittee_decisions.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class AddMissingLimitToComitteeDecisions < ActiveRecord::Migration[5.2]
-  def change
+  def up
     change_column :committee_decisions, :decision_no, :string, limit: 255
+  end
+
+  def down
+    change_column :committee_decisions, :decision_no, :string
   end
 end
