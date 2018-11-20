ed90f74e4041a95fe729e5186cd2d66686235e91
isubas <irfan.subas@uzem.omu.edu.tr>
2018-11-20 17:26:36 +0300

    Fix migration file

--- db/migrate/20181113144919_add_missing_limit_to_comittee_decisions.rb
+++ db/migrate/20181113144919_add_missing_limit_to_comittee_decisions.rb
@@ -4,7 +4,7 @@ class AddMissingLimitToComitteeDecisions < ActiveRecord::Migration[5.2]
   def change
     reversible do |dir|
       dir.up   { change_column :committee_decisions, :decision_no, :string, limit: 255 }
-      dir.down { true }
+      dir.down { change_column :committee_decisions, :decision_no, :string }
     end
   end
 end
