5628565b5d8f978a21784fc19a1dcd4ef358e079
isubas <irfan.subas@uzem.omu.edu.tr>
2018-11-28 11:48:06 +0300

    Ders türleri için model oluştur

---
+++ app/models/course_type.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CourseType < ApplicationRecord
+  # search
+  include PgSearch
+  pg_search_scope(
+    :search,
+    against: %i[name code],
+    using: { tsearch: { prefix: true } }
+  )
+
+  # relations
+  has_many :courses # , dependent: :nullify
+
+  # validations
+  validates :name, presence: true, uniqueness: true
+  validates :code, presence: true, uniqueness: true
+  validates :min_credit, presence: true, numericality: { greater_than_or_equal_to: 0 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_090506) do
+ActiveRecord::Schema.define(version: 2018_11_27_113700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -270,6 +270,14 @@ ActiveRecord::Schema.define(version: 2018_11_17_090506) do
 
   create_table "course_group_types", force: :cascade do |t|
     t.string "name", limit: 255, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "course_types", force: :cascade do |t|
+    t.string "name", limit: 255
+    t.string "code", limit: 50
+    t.decimal "min_credit", precision: 5, scale: 2, default: "0.0", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
