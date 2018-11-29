484d294b2c3d4f3f59493924f41ffba62fba7302
ecmelkytz <ecmel.kytz@gmail.com>
2018-11-29 11:00:17 +0300

    Change limit to identifier field

--- db/migrate/20181128080731_add_identifier_to_calendar_title.rb
+++ db/migrate/20181128080731_add_identifier_to_calendar_title.rb
@@ -2,6 +2,6 @@
 
 class AddIdentifierToCalendarTitle < ActiveRecord::Migration[5.2]
   def change
-    add_column :calendar_titles, :identifier, :string, null: false, limit: 255
+    add_column :calendar_titles, :identifier, :string, null: false, limit: 50
   end
 end
