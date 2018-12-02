25093e6efb21435d0515106facd203238f1137d8
msdundar <msdundars@gmail.com>
2018-12-03 02:22:37 +0300

    Fix rubocop offenses

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,15 +3,15 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [
-  :date_of_birth,
-  :email,
-  :fathers_name,
-  :id_number,
-  :mothers_name,
-  :password,
-  :place_of_birth,
-  :registered_to,
-  :registration_city,
-  :registration_district
+Rails.application.config.filter_parameters += %i[
+  date_of_birth
+  email
+  fathers_name
+  id_number
+  mothers_name
+  password
+  place_of_birth
+  registered_to
+  registration_city
+  registration_district
 ]
