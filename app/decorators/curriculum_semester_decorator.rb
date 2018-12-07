c416b4b63a5cd26708ab977b61b3626cfbe0aed7
isubas <irfan.subas@uzem.omu.edu.tr>
2018-12-07 16:49:59 +0300

    Müfredata ders grubu ekleme sayfasını iyileştir

--- app/decorators/curriculum_semester_decorator.rb
+++ app/decorators/curriculum_semester_decorator.rb
@@ -12,6 +12,13 @@ class CurriculumSemesterDecorator < SimpleDelegator
     merge(course_groups, appends)
   end
 
+  def build_curriculum_course_groups
+    available_course_groups.map do |course_group|
+      curriculum_course_groups.new(course_group_id: course_group.id)
+                              .build_curriculum_courses
+    end
+  end
+
   private
 
   def merge(collection, appends)
