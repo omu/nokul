08661d095b7e6f16bafe67fbef92c479a468e4d8
isubas <irfan.subas@uzem.omu.edu.tr>
2018-12-11 16:31:22 +0300

    Dönemler için seed datası oluştur

--- db/beta_seed/academic_term.rb
+++ db/beta_seed/academic_term.rb
@@ -1,37 +1,45 @@
 # frozen_string_literal: true
 
-AcademicTerm.create(
-  year: '2018 - 2019',
-  term: 'fall',
-  start_of_term: '2018-09-01 00:00:00',
-  end_of_term: '2019-01-15 00:00:00',
-  active: true
+Term.create(
+  YAML.load_file(Rails.root.join('db', 'static_data', 'terms.yml'))
 )
-AcademicTerm.create(
-  year: '2018 - 2019',
-  term: 'spring',
-  start_of_term: '2019-02-02 00:00:00',
-  end_of_term: '2019-06-21 00:00:00',
-  active: false
-)
-AcademicTerm.create(
-  year: '2017 - 2018',
-  term: 'fall',
-  start_of_term: '2017-09-017 00:00:00',
-  end_of_term: '2018-01-31 00:00:00',
-  active: false
-)
-AcademicTerm.create(
-  year: '2017 - 2018',
-  term: 'spring',
-  start_of_term: '2018-01-27 00:00:00',
-  end_of_term: '2018-06-28 00:00:00',
-  active: false
-)
-AcademicTerm.create(
-  year: '2017 - 2018',
-  term: 'summer',
-  start_of_term: '2018-06-11 00:00:00',
-  end_of_term: '2018-07-27 00:00:00',
-  active: false
-)
+
+academic_terms = [
+  {
+    year: '2018 - 2019',
+    term: 'fall',
+    start_of_term: '2018-09-01 00:00:00',
+    end_of_term: '2019-01-15 00:00:00',
+    active: true
+  },
+  {
+    year: '2018 - 2019',
+    term: 'spring',
+    start_of_term: '2019-02-02 00:00:00',
+    end_of_term: '2019-06-21 00:00:00',
+    active: false
+  },
+  {
+    year: '2017 - 2018',
+    term: 'fall',
+    start_of_term: '2017-09-017 00:00:00',
+    end_of_term: '2018-01-31 00:00:00',
+    active: false
+  },
+  {
+    year: '2017 - 2018',
+    term: 'spring',
+    start_of_term: '2018-01-27 00:00:00',
+    end_of_term: '2018-06-28 00:00:00',
+    active: false
+  },
+  {
+    year: '2017 - 2018',
+    term: 'summer',
+    start_of_term: '2018-06-11 00:00:00',
+    end_of_term: '2018-07-27 00:00:00',
+    active: false
+  }
+]
+
+AcademicTerm.create(academic_terms)
