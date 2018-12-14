a2c1f25e5c36288b2a57b7ef78e67dd532cc1e1e
M. Serhat Dündar <msdundars@gmail.com>
2018-12-14 19:50:55 +0300

    Fix some typos and issues

--- app/models/calendar_type.rb
+++ app/models/calendar_type.rb
@@ -11,7 +11,6 @@ class CalendarType < ApplicationRecord
 
   # validations
   validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
-  validates :identifier, presence: true, uniqueness: true, length: { maximum: 255 }
 
   # callbacks
   before_save { self.name = name.capitalize_all }
