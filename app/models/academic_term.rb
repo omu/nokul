08a0ee8da4ab27c301622c6dbe52a81de1152c28
M. Serhat Dündar <msdundars@gmail.com>
2018-12-14 17:22:34 +0300

    Add missing validations

--- app/models/academic_term.rb
+++ app/models/academic_term.rb
@@ -6,10 +6,11 @@ class AcademicTerm < ApplicationRecord
   has_many :calendar_events, through: :academic_calendars
 
   # validations
-  validates :term, presence: true
-  validates :year, presence: true, uniqueness: { scope: :term }
+  validates :year, presence: true, uniqueness: { scope: :term }, length: { maximum: 255 }
+  validates :term, presence: true, inclusion: { in: self.terms.keys }
   validates :start_of_term, presence: true
   validates :end_of_term, presence: true
+  validates :active, inclusion: { in: [true, false] }
 
   # enums
   enum term: { fall: 0, spring: 1, summer: 2 }
