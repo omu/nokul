3d1ac6beb50fe6aa03b11a9f6637d35b69d74b92
M. Serhat Dündar <msdundars@gmail.com>
2018-12-14 18:30:22 +0300

    Fix typo and remove redundant index

--- db/migrate/20180506124416_create_friendly_id_slugs.rb
+++ db/migrate/20180506124416_create_friendly_id_slugs.rb
@@ -18,7 +18,6 @@ class CreateFriendlyIdSlugs < ActiveRecord::Migration[5.2]
     add_length_constraint :friendly_id_slugs, :scope, less_than_or_equal_to: 255
 
     add_index :friendly_id_slugs, :sluggable_id
-    add_index :friendly_id_slugs, [:slug, :sluggable_type]
     add_index :friendly_id_slugs, [:slug, :sluggable_type, :scope], unique: true
     add_index :friendly_id_slugs, :sluggable_type
   end
