0d8a1477db265f9b22c7207ed280b190ef70e8ae
msdundar <msdundars@gmail.com>
2018-12-17 18:13:21 +0300

    Upgrade dependencies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,18 +75,18 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address        => 'smtp.gmail.com',
-    :domain         => 'mail.google.com',
-    :port           => 587,
-    :user_name      => Rails.application.credentials.gmail[:username],
-    :password       => Rails.application.credentials.gmail[:password],
-    :authentication => :plain,
-    :enable_starttls_auto => true
+    address: 'smtp.gmail.com',
+    domain: 'mail.google.com',
+    port: 587,
+    user_name: Rails.application.credentials.gmail[:username],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = [I18n.default_locale]
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
