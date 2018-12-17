e3a2cd052d29c9e4888dee57ed0aa7c9487d60b9
msdundar <msdundars@gmail.com>
2018-12-17 19:44:37 +0300

    Fix #604

--- db/migrate/20181217163618_add_created_at_fields_to_address_and_identity.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddCreatedAtFieldsToAddressAndIdentity < ActiveRecord::Migration[5.2]
-  def change
-    add_column :addresses, :created_at, :datetime, null: false
-    add_column :identitites, :created_at, :datetime, null: false
-  end
-end
