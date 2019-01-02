22312231d11b3a8e1027a0d7f7efbfcb744a9a54
Recai Oktaş <roktas@gmail.com>
2019-01-02 19:18:28 +0300

    Remove support before moving to support plugin

--- lib/support/ext/progress_bar.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class ProgressBar
-  def self.spawn(title, total, output = $stdout)
-    ProgressBar.create(title: title, total: total, format: '%t %B %c/%C %a', output: output) if Rails.env.development?
-  end
-end
--- lib/support/utils.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'utils/file_utils'
-require_relative 'utils/rake_utils'
