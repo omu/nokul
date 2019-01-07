d1d1c31c69002c8a6c914c9bde82856b94dc0a17
Recai Oktaş <roktas@gmail.com>
2019-01-07 10:07:16 +0300

    Simplify Tenant interface
    - Load tenant without using an initializer hook (this makes the whole
      initialization process more reliable)
    
    - Make Tenant a shortcut for Nokul::Tenant
    
    - Unify all tenant API under Tenant (Nokul::Tenant)
    
      + Tenant.configuration (instead of Rails.configuration.tenant)
      + Tenant.root
      + Tenant.deep_config_for
      + Tenant.engine (active Tenant instance)

--- test/config/tenant_config_test.rb
+++ test/config/tenant_config_test.rb
@@ -4,12 +4,12 @@ require 'test_helper'
 
 class TenantConfigTest < ActiveSupport::TestCase
   test 'Configuration can read tenant settings' do
-    assert_not_nil Rails.configuration.tenant.abbreviation
-    assert_not_nil Rails.configuration.tenant.host
+    assert_not_nil Tenant.configuration.abbreviation
+    assert_not_nil Tenant.configuration.host
   end
 
   test 'Tenant main configuration includes keys for various environments' do
-    config = YAML.load_file(Nokul::Tenant.engine.config_file_for(:tenant))
+    config = YAML.load_file(Tenant.engine.config_file_for(:tenant))
     assert config.key?('production')
     assert config.key?('beta')
     assert config.key?('test')
