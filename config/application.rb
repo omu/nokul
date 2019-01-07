22645cb8107d9b868bbb5d4fcd1b8b10e1e93803
Recai Oktaş <roktas@gmail.com>
2019-01-07 15:06:23 +0300

    Group plugins to be able to bundle install without plugins
    Will be exploited by Dockerfile

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+Bundler.require(*Rails.groups, :plugins)
 
 module Nokul
   class Application < Rails::Application
