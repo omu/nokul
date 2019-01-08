f0052b4e7157d3fb9889d63c4d98a9336c247f4b
Emre Can Yılmaz <emrecan@ecylmz.com>
2019-01-08 13:09:57 +0300

    Gemfile'da plugins grubunu kaldır

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups, :plugins)
+Bundler.require(*Rails.groups)
 
 module Nokul
   class Application < Rails::Application
