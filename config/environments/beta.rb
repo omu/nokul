d54f390d1da308e9cc18fa42c45df599657ba720
Recai Oktaş <roktas@gmail.com>
2019-01-09 09:22:20 +0300

    Uygulamaya özgü ortam değişkenlerini NOKUL_ ile ön ekle

--- config/environments/beta.rb
+++ config/environments/beta.rb
@@ -4,5 +4,5 @@
 require Rails.root.join('config', 'environments', 'production.rb')
 
 Rails.application.configure do
-  config.force_ssl = false if ENV['DISABLE_SSL']
+  config.force_ssl = false if ENV['NOKUL_DISABLE_SSL']
 end
