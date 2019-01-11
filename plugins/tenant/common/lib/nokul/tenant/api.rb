802345686da8280bb4a5bb4101e5f7a352a53f66
Recai Oktaş <roktas@gmail.com>
2019-01-11 09:51:17 +0300

    [Tenant API] Add optional argument to Tenant.units

--- plugins/tenant/common/lib/nokul/tenant/api.rb
+++ plugins/tenant/common/lib/nokul/tenant/api.rb
@@ -8,11 +8,9 @@ module Nokul
     mattr_accessor :engine
     mattr_accessor :configuration
 
-    module_function
+    alias instance engine
 
-    def deep_config_for(*args)
-      engine.deep_config_for(*args)
-    end
+    module_function
 
     # rubocop:disable Rails/Delegate
     def root
@@ -20,16 +18,27 @@ module Nokul
     end
     # rubocop:enable Rails/Delegate
 
-    alias instance engine
-
-    def units
-      Nokul::Tenant::Units.load_source 'src/all'
+    def deep_config_for(*args)
+      engine.deep_config_for(*args)
     end
 
-    def unit_types(type = nil)
-      return units.map(&:unit_type_id).uniq.sort unless type
+    def units(predication = nil)
+      units = Nokul::Tenant::Units.load_source 'src/all'
+      return units unless predication
 
-      units.select(&:"#{type}?").map(&:unit_type_id).uniq.sort
+      unless Nokul::Tenant::Units::Concerns::Predicable.method_defined?(predicator = "#{predication}?".to_sym)
+        raise "Unsupported unit predication: #{predication}"
+      end
+
+      units.select(&predicator)
+    end
+
+    def unit_types(category = nil)
+      return units.map(&:unit_type_id).uniq.sort unless category
+
+      raise "Unsupported unit category: #{category}" unless %i[administrative academic].include? category.to_sym
+
+      units.select(&:"#{category}?").map(&:unit_type_id).uniq.sort
     end
   end
 end
