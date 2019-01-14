b9d5c746c08c3172866470a53a432b9f733600e9
msdundar <msdundars@gmail.com>
2019-01-15 01:15:59 +0300

    Add missing tests and refactor the existing ones

--- test/controllers/admin/countries_controller_test.rb
+++ test/controllers/admin/countries_controller_test.rb
@@ -7,34 +7,46 @@ module Admin
     setup do
       sign_in users(:john)
       @country = countries(:turkey)
+      @form_params = %w[name alpha_2_code alpha_3_code numeric_code mernis_code yoksis_code]
     end
 
     test 'should get index' do
       get admin_countries_path
+      assert_equal 'index', @controller.action_name
       assert_response :success
       assert_select '#add-button', translate('.index.new_country_link')
     end
 
     test 'should get show' do
       get admin_country_path(@country)
+      assert_equal 'show', @controller.action_name
       assert_response :success
       assert_select '#add-button', translate('.show.new_city_link')
     end
 
     test 'should get new' do
       get new_admin_country_path
+
+      assert_equal 'new', @controller.action_name
       assert_response :success
+      assert_select '.simple_form' do
+        @form_params.each do |param|
+          assert_select "#country_#{param}"
+        end
+      end
     end
 
     test 'should create country' do
       assert_difference('Country.count') do
-        post admin_countries_path params: {
+        post admin_countries_path, params: {
           country: {
             name: 'Test Country', alpha_2_code: 'TC', alpha_3_code: 'TCC',
             numeric_code: '999', mernis_code: '9999', yoksis_code: '30001'
           }
         }
       end
+
+      assert_equal 'create', @controller.action_name
 
       country = Country.last
 
@@ -50,14 +62,12 @@ module Admin
 
     test 'should get edit' do
       get edit_admin_country_path(@country)
+      assert_equal 'edit', @controller.action_name
       assert_response :success
       assert_select '.simple_form' do
-        assert_select '#country_name'
-        assert_select '#country_alpha_2_code'
-        assert_select '#country_alpha_3_code'
-        assert_select '#country_numeric_code'
-        assert_select '#country_mernis_code'
-        assert_select '#country_yoksis_code'
+        @form_params.each do |param|
+          assert_select "#country_#{param}"
+        end
       end
     end
 
@@ -69,6 +79,8 @@ module Admin
           numeric_code: '998', mernis_code: '9998', yoksis_code: '30002'
         }
       }
+
+      assert_equal 'update', @controller.action_name
 
       country.reload
 
@@ -87,6 +99,7 @@ module Admin
         delete admin_country_path(countries(:norway))
       end
 
+      assert_equal 'destroy', @controller.action_name
       assert_redirected_to admin_countries_path
       assert_equal translate('.destroy.success'), flash[:notice]
     end
