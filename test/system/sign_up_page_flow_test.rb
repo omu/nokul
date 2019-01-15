32119d5a4c73539c01f271a7ed21bd8ae3bd09a4
msdundar <msdundars@gmail.com>
2019-01-15 04:29:20 +0300

    Fix rubocop offense

--- test/system/sign_up_page_flow_test.rb
+++ test/system/sign_up_page_flow_test.rb
@@ -10,10 +10,7 @@ class SignUpPageFlowTest < ApplicationSystemTestCase
       page.driver.browser.manage.window.resize_to(*resolution)
       visit(new_user_registration_path)
       {
-        id_number: '70336212330', # valid test id (see documents to pick a test id)
-        email: 'new_user@gmail.com',
-        password: '123456',
-        password_confirmation: '123456'
+        id_number: '70336212330', email: 'new_user@gmail.com', password: '123456', password_confirmation: '123456'
       }.each do |key, value|
         fill_in("user[#{key}]", with: value)
       end
