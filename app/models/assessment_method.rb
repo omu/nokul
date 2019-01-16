ebc182d238d74356010bf7d8b9015da756078831
ecmelkytz <ecmel.kytz@gmail.com>
2019-01-16 14:26:47 +0300

    Create assessment method model

--- app/models/assessment_method.rb
+++ app/models/assessment_method.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
-class EvaluationType < ApplicationRecord
+class AssessmentMethod < ApplicationRecord
   # search
   include ReferenceSearch
   include ReferenceCallbacks
-
-  # relations
-  has_many :course_evaluation_types, dependent: :destroy
-  has_many :available_courses, through: :course_evaluation_types
 
   # validations
   validates :name, presence: true, uniqueness: true, length: { maximum: 255 }
