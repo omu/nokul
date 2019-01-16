4108deab59a1d7eb8eed4494908143934d915978
msdundar <msdundars@gmail.com>
2019-01-16 22:55:53 +0300

    Use different hosts in different environments

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: Tenant.configuration.host }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
 
