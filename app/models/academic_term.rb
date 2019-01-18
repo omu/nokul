bb98c91c2bb9ca13b4b627db12ecf32e4d872963
isubas <irfan.subas@uzem.omu.edu.tr>
2019-01-18 19:27:42 +0300

    Yalnızca bir akademik dönemin aktif olmasını sağla

--- app/models/academic_term.rb
+++ app/models/academic_term.rb
@@ -7,11 +7,16 @@ class AcademicTerm < ApplicationRecord
   has_many :calendars, dependent: :nullify
   has_many :registration_documents, dependent: :nullify
 
+  # callbacks
+
+  after_save -> { AcademicTerm.where.not(id: id).update_all(active: false) }, if: :active?
+
   # validations
   validates :year, presence: true, uniqueness: { scope: :term }, length: { maximum: 255 }
   validates :start_of_term, presence: true
   validates :end_of_term, presence: true
   validates :active, inclusion: { in: [true, false] }
+  validates_with AcademicTermValidator
 
   # scopes
   scope :active, -> { where(active: true) }
