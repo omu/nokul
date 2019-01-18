c0933b9a0f3c5a7490b7b0f2d4e9ec5f50ec6566
isubas <irfan.subas@uzem.omu.edu.tr>
2019-01-18 17:05:00 +0300

    Link helper'ını iyileştir
    link_to_* metodlarının kısmen link_to gibi de kullanılabilmesini sağla #669

--- test/helpers/link_helper_test.rb
+++ test/helpers/link_helper_test.rb
@@ -5,44 +5,44 @@ require 'test_helper'
 class LinkHelperTest < ActionView::TestCase
   include FontAwesome::Rails::IconHelper
 
-  test '#link_to_back' do
+  test 'link_to_back method' do
     link = <<-HTML.squish
       <a class="btn btn-secondary btn-sm" href="#"><i class="fa fa-arrow-left"></i> Back</a>
     HTML
-    assert_equal link_to_back('#', 'Back'), link
+    assert_equal link_to_back('Back', '#'), link
   end
 
-  test '#link_to_destroy' do
+  test 'link_to_destroy method' do
     link = <<-HTML.squish
-      <a data-confirm="#{t('are_you_sure')}"
-         class="btn btn-outline-danger btn-sm"
+      <a class="btn btn-outline-danger btn-sm"
+         data-confirm="#{t('are_you_sure')}"
          rel="nofollow" data-method="delete" href="#"><i class="fa fa-trash"></i> Test Destroy</a>
     HTML
-    assert_equal link_to_destroy('#', 'Test Destroy'), link
+    assert_equal link_to_destroy('Test Destroy', '#'), link
   end
 
-  test '#link_to_edit' do
+  test 'link_to_edit method' do
     link = <<-HTML.squish
       <a class="btn btn-outline-success btn-sm"
          href="#"><i class="fa fa-pencil"></i> Test Edit</a>
     HTML
-    assert_equal link_to_edit('#', 'Test Edit'), link
+    assert_equal link_to_edit('Test Edit', '#'), link
   end
 
-  test '#link_to_new' do
+  test 'link_to_new method' do
     link = <<-HTML.squish
       <a class="btn btn-outline-primary btn-sm" id="add-button"
          href="#"><i class="fa fa-plus"></i> Test New</a>
     HTML
-    assert_equal link_to_new('#', 'Test New'), link
+    assert_equal link_to_new('Test New', '#'), link
   end
 
-  test '#link_to_show' do
+  test 'link_to_show' do
     link = <<-HTML.squish
       <a class="btn btn-outline-info btn-sm"
          href="#"><i class="fa fa-eye"></i> Test Show</a>
     HTML
-    assert_equal link_to_show('#', 'Test Show'), link
+    assert_equal link_to_show('Test Show', '#'), link
   end
 
   test '#link_to_update' do
@@ -50,6 +50,6 @@ class LinkHelperTest < ActionView::TestCase
       <a class="btn btn-outline-info btn-sm"
         href="#"><i class="fa fa-pencil-square-o"></i> Test Update</a>
     HTML
-    assert_equal link_to_update('#', 'Test Update'), link
+    assert_equal link_to_update('Test Update', '#'), link
   end
 end
