c166a888cc0940816b53058c83ff3f6db6029983
Recai Oktaş <roktas@gmail.com>
2019-01-21 15:00:57 +0300

    Remove (now) unneeded Vagrant provision step

--- lib/scripts/preinst.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail; [[ -z ${TRACE:-} ]] || set -x
-
-export DEBIAN_FRONTEND=noninteractive
-
-if [[ -d /vagrant ]] && [[ ! -e /app ]]; then
-	ln -sf /vagrant /app
-fi
