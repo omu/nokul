a3fb2beea2efaf8396928abfb2823b77a5bbaaaf
Recai Oktaş <roktas@gmail.com>
2019-01-25 00:58:00 +0300

    Remove examples as they are already documented

--- plugins/tenant/omu/app/lib/students.rb
+++ plugins/tenant/omu/app/lib/students.rb
@@ -11,40 +11,10 @@ module Students
     Date.current.strftime '%y' # TODO: placeholder
   end
 
-  # Example:
-  #
-  #   numerator = numerator('203', nil)
-  #
-  #   numerator.number #=> 20319001
-  #   numerator.number #=> 20319002
-  #
-  #   numerator.next_sequence  #=> 003 (without actually incrementing number)
-  #   numerator.first_sequence #=> 001
-  #
-  #   numerator = numerator('203', '078')
-  #
-  #   numerator.number #=> 20319078
-  #   numerator.number #=> 20319079
-  #
   def numerator(unit_code, starting_seqeuence)
     Numerator.new starting_seqeuence, leading_prefix: unit_code, trailing_prefix: start_of_active_academic_year
   end
 
-  # Example:
-  #
-  #   long_numerator = long_numerator('203', nil)
-  #
-  #   long_numerator.number #=> 20300001
-  #   long_numerator.number #=> 20300002
-  #
-  #   long_numerator.next_sequence  #=> 00003 (without actually incrementing number)
-  #   long_numerator.first_sequence #=> 00001
-  #
-  #   long_numerator = long_numerator('203', '078')
-  #
-  #   long_numerator.number #=> 20300078
-  #   long_numerator.number #=> 20300079
-  #
   def long_numerator(unit_code, starting_seqeuence)
     Numerator.new starting_seqeuence, leading_prefix: unit_code, trailing_prefix: ''
   end
