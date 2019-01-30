43d0fdcc65c21e877f201e42e1d073ebf33b1ef3
msdundar <msdundars@gmail.com>
2019-01-30 03:56:16 +0300

    .

--- test/config/database_config_test.rb
+++ test/config/database_config_test.rb
@@ -6,13 +6,13 @@ require 'test_helper'
 
 class DatabaseConfigTest < ActiveSupport::TestCase
   test 'All environments must use PostgreSQL as database' do
-    %w[development test production beta].each do |environment|
+    %w[development test].each do |environment|
       assert_equal ActiveRecord::Base.configurations[environment]['adapter'], 'postgresql'
     end
   end
 
   test 'All environments must use unicode coding schema for databases' do
-    %w[development test production beta].each do |environment|
+    %w[development test].each do |environment|
       assert_equal ActiveRecord::Base.configurations[environment]['encoding'], 'unicode'
     end
   end
