7607c050428a9d1bc41ada3458fc8def100e918d
isubas <irfan.subas@uzem.omu.edu.tr>
2019-01-30 17:16:15 +0300

    Link helper yapısını iyileştir

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -56,39 +56,65 @@ module LinkHelper
     }
   }.freeze
 
+  # Usage:
+  # link_to_#{action}(path)
+  # link_to_#{action}(path, options = {})
+  # link_to_#{action}(text, path, options = {})
   LINKS.each do |action, configuration|
     define_method("link_to_#{action}") do |*args|
       link_builder(args, configuration)
     end
   end
 
-  # Usage:
-  # link_to_actions(@cities,
+  BASE_ACTIONS = %i[show edit destroy].freeze
+
+  # Basic Usage:
+  # link_to_actions(path)
+  # link_to_actions(course, except: :show)
+  #
+  # Advance Usage:
+  # link_to_actions(path,
   #                 scope: :admin,
   #                 except: :show,
-  #                 edit: { text: 'Edit Text' })
+  #                 edit: { text: 'Edit Text', options: { class: 'btn btn-danger' } },
+  #                 destroy: { options: { class: 'btn btn-danger' } })
   def link_to_actions(path, options = {})
-    actions = (%i[show edit destroy] - [*options[:except]].map(&:to_sym)).inquiry
-    path = [*options[:scope], *path]
-    links = []
-    links << link_to_show(options.dig(:show, :text), [*path])        if actions.show?
-    links << link_to_edit(options.dig(:edit, :text), [:edit, *path]) if actions.edit?
-    links << link_to_destroy(options.dig(:destroy, :text), [*path])  if actions.destroy?
-    safe_join(links, ' ')
+    actions = BASE_ACTIONS - [*options[:except]].map(&:to_sym)
+    safe_join(
+      create_links_for(path, actions, options),
+      ' '
+    )
   end
 
   private
 
+  def create_links_for(path, actions, options = {})
+    config = {
+      edit: { path_prefix: :edit }
+    }
+
+    actions.map do |action|
+      send("link_to_#{action}",
+           options.dig(action, :text),
+           [*config.dig(action, :path_prefix), *options[:scope], *path],
+           options.dig(action, :options))
+    end
+  end
+
   def link_builder(args, configuration)
-    text, path = split_args_for_link_to(args)
+    text, path, custom_options = split_args_for_link_to(args)
+    options = configuration.fetch(:options, {})
+    options = options.merge(custom_options) if custom_options.is_a?(Hash)
+
     link_to(
       fa_icon(configuration[:icon], text: text || configuration[:text]),
       path,
-      configuration.fetch(:options, {})
+      options
     )
   end
 
   def split_args_for_link_to(args)
-    args.length == 1 ? [nil, *args] : args
+    number_of_args_check = args.last.is_a?(::Hash) ? 2 : 1
+    args.length == number_of_args_check ? [nil, *args] : args
   end
 end
