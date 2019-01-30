b75c54aec968172350ef1210fff6cd44a7277ae8
msdundar <msdundars@gmail.com>
2019-01-30 03:04:42 +0300

    Set paralellize to 1 for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,6 +3,8 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  parallelize(workers: 1)
+
   driven_by :selenium, using: :headless_chrome, screen_size: [1920, 1080], options: {
     desired_capabilities: {
       chromeOptions: {
