9921bb05330d32722f32e87d4cdfb3404dfe983a
Dilara Koca <adilarakoca@gmail.com>
2019-01-31 16:08:53 +0300

    Takvimi kurul kararları ile ilişkilendir

--- app/controllers/calendar_management/calendars_controller.rb
+++ app/controllers/calendar_management/calendars_controller.rb
@@ -71,7 +71,7 @@ module CalendarManagement
       params.require(:calendar)
             .permit(
               :name, :senate_decision_date, :senate_decision_no, :description, :timezone, :academic_term_id,
-              unit_ids: [], calendar_events_attributes: %i[
+              unit_ids: [], committee_decision_ids: [], calendar_events_attributes: %i[
                 id calendar_event_type_id start_time end_time location timezone visible _destroy
               ]
             )
