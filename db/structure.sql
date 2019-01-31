8006489439ce6506141b653cd4f67807eddd4b21
Dilara Koca <adilarakoca@gmail.com>
2019-01-31 16:06:24 +0300

    Takvim modelinde tutulan karar bilgilerini kaldır

--- db/structure.sql
+++ db/structure.sql
@@ -8,6 +8,20 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
 SET default_tablespace = '';
 
 SET default_with_oids = false;
@@ -640,8 +654,6 @@ ALTER SEQUENCE public.calendar_events_id_seq OWNED BY public.calendar_events.id;
 CREATE TABLE public.calendars (
     id bigint NOT NULL,
     name character varying,
-    senate_decision_date date,
-    senate_decision_no character varying,
     description character varying,
     timezone character varying DEFAULT 'Istanbul'::character varying,
     academic_term_id bigint NOT NULL,
@@ -650,8 +662,6 @@ CREATE TABLE public.calendars (
     CONSTRAINT calendars_description_length CHECK ((length((description)::text) <= 65535)),
     CONSTRAINT calendars_name_length CHECK ((length((name)::text) <= 255)),
     CONSTRAINT calendars_name_presence CHECK (((name IS NOT NULL) AND ((name)::text !~ '^\s*$'::text))),
-    CONSTRAINT calendars_senate_decision_no_length CHECK ((length((senate_decision_no)::text) <= 255)),
-    CONSTRAINT calendars_senate_decision_no_presence CHECK (((senate_decision_no IS NOT NULL) AND ((senate_decision_no)::text !~ '^\s*$'::text))),
     CONSTRAINT calendars_timezone_length CHECK ((length((timezone)::text) <= 255)),
     CONSTRAINT calendars_timezone_presence CHECK (((timezone IS NOT NULL) AND ((timezone)::text !~ '^\s*$'::text)))
 );
@@ -5171,6 +5181,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190115100844'),
 ('20190116104001'),
 ('20190116115745'),
-('20190122203727');
+('20190122203727'),
+('20190130105509');
 
 
