4694af71da3e6696169383f3da114d389b7eb8ff
ecmelkytz <ecmel.kytz@gmail.com>
2019-02-05 14:55:13 +0300

    Add least one coordinator to available course group

--- app/validators/available_course_lecturer_validator.rb
+++ app/validators/available_course_lecturer_validator.rb
@@ -2,9 +2,25 @@
 
 class AvailableCourseLecturerValidator < ActiveModel::Validator
   def validate(record)
-    lecturers = record.group.lecturers.reject { |lecturer| lecturer.equal?(record) }
-    return unless record.coordinator && lecturers.map(&:coordinator).any?
+    lecturers(record.group.lecturers.reject { |lecturer| lecturer.equal?(record) })
+
+    return if not_exist_coordinator?(record, @lecturers) || any_coordinator?(record, @lecturers)
 
     record.errors[:coordinator] << I18n.t('coordinator', scope: %i[validators available_course_lecturer])
+  end
+
+  def lecturers(course_lecturers)
+    @lecturers =
+      course_lecturers.map { |lecturer| lecturer unless lecturer.marked_for_destruction? }.compact
+  end
+
+  private
+
+  def any_coordinator?(record, lecturers)
+    !record.coordinator && lecturers.map(&:coordinator).any?
+  end
+
+  def not_exist_coordinator?(record, lecturers)
+    record.coordinator && lecturers.map(&:coordinator).none?
   end
 end
