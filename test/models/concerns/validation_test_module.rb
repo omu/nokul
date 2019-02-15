a992d5078def815b0db3a17483b0337785d34342
msdundar <msdundars@gmail.com>
2019-02-15 12:28:39 +0300

    Add validates_uniqueness_of method

--- test/models/concerns/validation_test_module.rb
+++ test/models/concerns/validation_test_module.rb
@@ -16,5 +16,17 @@ module ValidationTestModule
         end
       end
     end
+
+    def validates_uniqueness_of(attributes, object = nil)
+      object ||= to_s.delete_suffix('Test')
+
+      [attributes].compact.flatten.each do |attribute|
+        test "#{attribute} must be unique (uniqueness: true) for #{object}" do
+          duplicate_object = object.constantize.take.dup
+          assert_not duplicate_object.valid?
+          assert_not_empty duplicate_object.errors[attribute]
+        end
+      end
+    end
   end
 end
