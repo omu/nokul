08faa42f550de8c5494c6520a5015901d773f29f
Recai Oktaş <roktas@gmail.com>
2019-03-03 11:29:34 +0300

    s/NumberCoder/NumberGenerator/

--- plugins/tenant/omu/app/lib/students.rb
+++ plugins/tenant/omu/app/lib/students.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Students
-  NumberCoder = Support::Codifications::PrefixedCoder # Length is 8 by default
+  NumberGenerator = Support::Codifications::PrefixedCoder # Length is 8 by default
 
   module_function
 
@@ -14,10 +14,10 @@ module Students
   end
 
   def short_number_generator(unit_code, starting_sequence)
-    NumberCoder.new starting_sequence, prefix: [unit_code, start_of_active_academic_year]
+    NumberGenerator.new starting_sequence, prefix: [unit_code, start_of_active_academic_year]
   end
 
   def long_number_generator(unit_code, starting_sequence)
-    NumberCoder.new starting_sequence, prefix: unit_code
+    NumberGenerator.new starting_sequence, prefix: unit_code
   end
 end
