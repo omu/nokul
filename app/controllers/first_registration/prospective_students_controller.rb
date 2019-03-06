56c26f76d355428606fc9fecef430178cc1a59ca
ecmelkytz <ecmel.kytz@gmail.com>
2019-03-06 12:39:11 +0300

    Archive the student when its registered

--- app/controllers/first_registration/prospective_students_controller.rb
+++ app/controllers/first_registration/prospective_students_controller.rb
@@ -40,7 +40,7 @@ module FirstRegistration
       prospective_student = FirstRegistration::ProspectiveStudentService.new(@prospective_student)
 
       if prospective_student.register
-        @prospective_student.update(registered: true)
+        @prospective_student.update(registered: true, archived: true)
         redirect_to(index_path, notice: t('.success'))
       else
         redirect_to(index_path, alert: t('.warning'))
