c05835c73b9999db82f887ee11669a700e32f335
msdundar <msdundars@gmail.com>
2019-03-15 23:23:41 +0300

    Read application version from app.json Refactor Nokul::VERSION

--- app/lib/nokul/version.rb
+++ app/lib/nokul/version.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
 module Nokul
-  VERSION = '0.5.8'
+  class Version
+    attr_reader :major, :minor, :patch
+
+    def initialize(*version)
+      @major, @minor, @patch = version
+    end
+
+    def self.current
+      if File.exist?(manifest = Rails.root.join('app.json'))
+        current_version = JSON.parse(File.read(manifest)).fetch('version')
+        Version.new(*current_version.split('.')).to_s
+      else
+        '0.0.0'
+      end
+    end
+
+    def to_s
+      "%d.%d.%d" % [@major, @minor, @patch]
+    end
+  end
 end
