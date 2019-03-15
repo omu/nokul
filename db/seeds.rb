319b4f55eb479d7517451a08ef5a8ef3b9f45e8d
msdundar <msdundars@gmail.com>
2019-03-15 19:20:09 +0300

    Refactor db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,58 @@
 # frozen_string_literal: true
 
-config = ActiveRecord::Base.configurations[Rails.env]
+def load_seed_data
+  Dir[Rails.root.join('db', 'beta_seed', '*.rb')].sort.each do |seed|
+    load seed
+  end
+end
 
-if ENV['SYNC'].eql?('true')
-  Rake::Task['fetch:references'].invoke
-  Rake::Task['import:all'].invoke
-else
+def extract_file(file_name)
+  extraction_path = Rails.root.join('db', 'encrypted_data', "#{file_name}.tar.gz").to_s
+  `tar xvzf "#{extraction_path}" -C tmp/`
+end
+
+def decrypt_and_write_to_file(file_name)
+  file_to_decrypt = "tmp/#{file_name}.sql.enc"
+  file_to_write = "tmp/#{file_name}.sql"
+  File.write(
+    file_to_write,
+    Support::Sensitive.read(file_to_decrypt)
+  )
+end
+
+def restore_sql_dump(file_name)
+  config = ActiveRecord::Base.configurations[Rails.env]
+  dump_file = Rails.root.join('tmp', "#{file_name}.sql")
+
   connection = "PGPASSWORD=#{config['password']} psql \
                -h #{config['host']} \
                -d #{config['database']} \
                -U #{config['username']} "
 
-  `tar xvzf #{Rails.root.join('db', 'encrypted_data', 'static_data.tar.gz').to_s} -C tmp/`
-  File.write('tmp/static_data.sql', Support::Sensitive.read('tmp/static_data.sql.enc'))
-  `#{connection + "-f #{Rails.root.join('tmp', 'static_data.sql')}"}`
+  `#{connection + "-f #{dump_file}"}`
 end
 
-if ENV['SAMPLE_DATA'].eql?('true')
-  if ENV['SYNC'].eql?('true')
+def restore_from_backup(file_name)
+  extract_file(file_name)
+  decrypt_and_write_to_file(file_name)
+  restore_sql_dump(file_name)
+end
+
+# Seed logic goes below
+
+if ENV['SYNC'].eql?('true')
+  Rake::Task['fetch:references'].invoke
+  Rake::Task['import:all'].invoke
+
+  if ENV['SAMPLE_DATA'].eql?('true')
     Rake::Task['fetch:academic_staff'].invoke
     Osym::ImportProspectiveStudentsJob.perform_later('db/encrypted_data/prospective_students.csv')
-  else
-    `tar xvzf #{Rails.root.join('db', 'encrypted_data', 'sample_data.tar.gz').to_s} -C tmp/`
-    File.write('tmp/sample_data.sql', Support::Sensitive.read('tmp/sample_data.sql.enc'))
+  end
+else
+  restore_from_backup('static_data')
 
-    `#{connection + "-f #{Rails.root.join('tmp', 'sample_data.sql')}"}`
-
-    Dir[Rails.root.join('db', 'beta_seed', '*.rb')].sort.each do |seed|
-      load seed
-    end
+  if ENV['SAMPLE_DATA'].eql?('true')
+    restore_from_backup('sample_data') if ENV['SAMPLE_DATA'].eql?('true')
+    load_seed_data
   end
 end
