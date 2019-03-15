8749deca261a9d1cd09d5e5cef76683323f2be62
msdundar <msdundars@gmail.com>
2019-03-16 02:31:00 +0300

    Improve filter_parameter_logging

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,9 +1,12 @@
-# filter sensitive parameters from logs
+# frozen_string_literal: true
 
-Rails.application.config.filter_parameters += %i(
+Rails.application.config.filter_parameters += %i[
+  access_token
+  bearer_token
   client_id
   credit_card
   password
   password_confirmation
+  secret_key
   store_key
-)
+]
