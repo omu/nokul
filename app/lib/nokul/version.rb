3f4c63aca9c2ce0bf38735b68217c487316cdd50
msdundar <msdundars@gmail.com>
2019-03-16 05:38:09 +0300

    Improve Nokul::Version

--- app/lib/nokul/version.rb
+++ app/lib/nokul/version.rb
@@ -1,24 +1,16 @@
 # frozen_string_literal: true
 
 module Nokul
-  class Version
-    attr_reader :major, :minor, :patch
-
-    def initialize(*version)
-      @major, @minor, @patch = version
+  class Version < Gem::Version
+    def self.load_from_manifest
+      version = if File.exist?(manifest = Rails.root.join('app.json'))
+                  JSON.parse(File.read(manifest)).fetch('version')
+                end
+      new(version || '0.0.0')
     end
 
     def self.current
-      @current ||= if File.exist?(manifest = Rails.root.join('app.json'))
-                     current_version = JSON.parse(File.read(manifest)).fetch('version')
-                     Version.new(*current_version.split('.')).to_s
-                   else
-                     '0.0.0'
-                   end
-    end
-
-    def to_s
-      format('%{major}.%{minor}.%{patch}', major: @major, minor: @minor, patch: @patch)
+      @current ||= load_from_manifest.to_s
     end
   end
 end
