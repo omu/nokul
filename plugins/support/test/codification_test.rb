009b4a2276ec0f52610a55f0f569041b09666409
Recai Oktaş <roktas@gmail.com>
2019-03-19 00:38:27 +0300

    Add API test for random_numeric_codes

--- plugins/support/test/codification_test.rb
+++ plugins/support/test/codification_test.rb
@@ -8,9 +8,10 @@ module Nokul
       class CodificationTest < ActiveSupport::TestCase
         test 'API works' do
           %i[
+            alternative_user_names
+            random_numeric_codes
             sequential_numeric_codes
             suffixed_user_names
-            alternative_user_names
           ].all? { |method| assert Codification.respond_to?(method) }
         end
       end
