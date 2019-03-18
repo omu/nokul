e5def149e33c2f55855d352044125b4d8b7150f1
Recai Oktaş <roktas@gmail.com>
2019-03-18 15:16:44 +0300

    Remove old implementation

--- plugins/support/lib/nokul/support.rb
+++ plugins/support/lib/nokul/support.rb
@@ -10,7 +10,6 @@ require_relative 'support/utils'
 require_relative 'support/structure'
 require_relative 'support/collection'
 require_relative 'support/uniq_collection'
-require_relative 'support/codifications'
 require_relative 'support/sensitive'
 require_relative 'support/rest_client'
 
