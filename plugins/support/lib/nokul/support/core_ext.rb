f2c2b0c0bedd11c3a9c9fc5127bea27a9337c696
Recai Oktaş <roktas@gmail.com>
2019-03-19 00:34:54 +0300

    Remove kernel core_ext

--- plugins/support/lib/nokul/support/core_ext.rb
+++ plugins/support/lib/nokul/support/core_ext.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'core_ext/object'
-require_relative 'core_ext/kernel'
 require_relative 'core_ext/array'
-require_relative 'core_ext/hash'
 require_relative 'core_ext/string'
 require_relative 'core_ext/integer'
