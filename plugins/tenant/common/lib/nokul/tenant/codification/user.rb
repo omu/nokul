06915dc0ff3b1180277e5792e095c43dabee190b
Recai Oktaş <roktas@gmail.com>
2019-03-28 12:22:09 +0300

    Update Tenant codification tests against latest changes

--- plugins/tenant/common/lib/nokul/tenant/codification/user.rb
+++ plugins/tenant/common/lib/nokul/tenant/codification/user.rb
@@ -9,15 +9,17 @@ module Nokul
 
         module_function
 
-        def name_generate(first_name:, last_name:, memory:)
+        def name_generate(first_name:, last_name:, memory:, **options)
           Support::Codification.suffixed_user_name [first_name, last_name],
+                                                   **options,
                                                    memory:                  memory,
                                                    random_suffix_separator: const.random_suffix_separator,
                                                    random_suffix_length:    const.random_suffix_length
         end
 
-        def name_suggest(first_name:, last_name:, memory:)
+        def name_suggest(first_name:, last_name:, memory:, **options)
           Support::Codification.unsuffixed_user_names([first_name, last_name],
+                                                      **options,
                                                       memory: memory).available
         end
       end
--- plugins/tenant/common/test/codification/user_test.rb
+++ plugins/tenant/common/test/codification/user_test.rb
@@ -6,7 +6,7 @@ module Nokul
   module Tenant
     module Codification
       class UserTest < ActiveSupport::TestCase
-        test 'name_generate works' do
+        test 'name_generate works by default' do
           user_name = User.name_generate(first_name: 'Mustafa Kemal',
                                          last_name:  'Atatürk',
                                          memory:     Support::Codification::SimpleMemory.new)
@@ -16,10 +16,37 @@ module Nokul
           assert_match(/^mkataturk#{sep}\d{#{len}}$/, user_name)
         end
 
-        test 'name_suggest works' do
+        test 'name_generate works with options' do
+          user_name = User.name_generate(first_name: 'Mustafa Kemal',
+                                         last_name:  'Atatürk',
+                                         memory:     Support::Codification::SimpleMemory.new,
+                                         interfix:   '-')
+
+          sep = Regexp.escape(Codification::User.const.random_suffix_separator)
+          len = Codification::User.const.random_suffix_length
+          assert_match(/^m-k-ataturk#{sep}\d{#{len}}$/, user_name)
+        end
+
+        test 'name_suggest works by default' do
           actual = User.name_suggest(first_name: 'Mustafa Kemal',
                                      last_name:  'Atatürk',
                                      memory:     Support::Codification::SimpleMemory.new)
+
+          expected = %w[
+            mustafa.ataturk
+            kemal.ataturk
+            mustafa.kemal.ataturk
+          ]
+
+          assert_equal expected, actual
+        end
+
+        test 'name_suggest works with options' do
+          actual = User.name_suggest(first_name:  'Mustafa Kemal',
+                                     last_name:   'Atatürk',
+                                     memory:      Support::Codification::SimpleMemory.new,
+                                     alternative: :abbreviated,
+                                     interfix:    nil)
 
           expected = %w[
             mkataturk
