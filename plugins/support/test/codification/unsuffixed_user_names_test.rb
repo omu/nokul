61fccf67f4efcd56c0e0a85bb20dd8579626b11a
Recai Oktaş <roktas@gmail.com>
2019-03-28 12:21:18 +0300

    Update codification library tests against latest changes

--- plugins/support/test/codification/unsuffixed_user_names_test.rb
+++ plugins/support/test/codification/unsuffixed_user_names_test.rb
@@ -6,22 +6,44 @@ module Nokul
   module Support
     module  Codification
       class UnsuffixedUserNamesTest < ActiveSupport::TestCase
-        test 'simple use case works' do
+        test 'simple use case with default works' do
           coder = Codification.unsuffixed_user_names %w[gabriel garcia marquez]
 
-          assert_equal'ggmarquez', coder.run
-          assert_equal'ggarciam', coder.run
-          assert_equal'gabrielgm', coder.run
-          assert_equal'ggarciamarquez', coder.run
-          assert_equal'gabrielgmarquez', coder.run
-          assert_equal'gabrielgarciam', coder.run
-          assert_equal'gabrielgarciamarquez', coder.run
+          assert_equal 'gabriel.marquez', coder.run
+          assert_equal 'garcia.marquez', coder.run
+          assert_equal 'gabriel.garcia.marquez', coder.run
+
+          assert_raise(StopIteration) { coder.run }
+        end
+
+        test 'simple use case with non abbreviated options works' do
+          options = { alternative: :non_abbreviated, interfix: '-' }
+          coder = Codification.unsuffixed_user_names(%w[gabriel garcia marquez], **options)
+
+          assert_equal 'gabriel-marquez', coder.run
+          assert_equal 'garcia-marquez', coder.run
+          assert_equal 'gabriel-garcia-marquez', coder.run
+
+          assert_raise(StopIteration) { coder.run }
+        end
+
+        test 'simple use case with abbreviated options works' do
+          options = { alternative: :abbreviated, interfix: nil }
+          coder = Codification.unsuffixed_user_names(%w[gabriel garcia marquez], **options)
+
+          assert_equal 'ggmarquez', coder.run
+          assert_equal 'ggarciam', coder.run
+          assert_equal 'gabrielgm', coder.run
+          assert_equal 'ggarciamarquez', coder.run
+          assert_equal 'gabrielgmarquez', coder.run
+          assert_equal 'gabrielgarciam', coder.run
+          assert_equal 'gabrielgarciamarquez', coder.run
 
           assert_raise(StopIteration) { coder.run }
         end
 
         test 'API with singular name works' do
-          assert_equal 'ggmarquez', Codification.unsuffixed_user_name(%w[gabriel garcia marquez])
+          assert_equal 'gabriel.marquez', Codification.unsuffixed_user_name(%w[gabriel garcia marquez])
         end
 
         test 'simple use case with memory works' do
@@ -29,26 +51,25 @@ module Nokul
 
           coder = Codification.unsuffixed_user_names %w[gabriel garcia marquez], memory: memory
 
-          memory.remember 'ggarciam'
+          memory.remember 'garcia.marquez'
 
-          assert_equal'ggmarquez', coder.run
-          assert_equal'gabrielgm', coder.run
+          assert_equal 'gabriel.marquez', coder.run
+          assert_equal 'gabriel.garcia.marquez', coder.run
         end
 
         test 'prefix option works' do
           coder = Codification.unsuffixed_user_names %w[gabriel garcia marquez], prefix: 'user.'
 
-          assert_equal'user.ggmarquez', coder.run
-          assert_equal'user.ggarciam', coder.run
+          assert_equal 'user.gabriel.marquez', coder.run
+          assert_equal 'user.garcia.marquez', coder.run
+          assert_equal 'user.gabriel.garcia.marquez', coder.run
         end
 
         test 'regex post process option works' do
-          coder = Codification.unsuffixed_user_names %w[gabriel garcia marquez], post_process: /.+garcia.+/
+          coder = Codification.unsuffixed_user_names %w[gabriel garcia marquez], post_process: /.*garcia/
 
-          assert_equal'ggarciam', coder.run
-          assert_equal'ggarciamarquez', coder.run
-          assert_equal'gabrielgarciam', coder.run
-          assert_equal'gabrielgarciamarquez', coder.run
+          assert_equal 'garcia.marquez', coder.run
+          assert_equal 'gabriel.garcia.marquez', coder.run
 
           assert_raise(StopIteration) { coder.run }
         end
@@ -56,12 +77,14 @@ module Nokul
         test 'proc post process option works' do
           coder = Codification.unsuffixed_user_names %w[gabriel garcia marquez], post_process: proc { |s| s.upcase }
 
-          assert_equal'GGMARQUEZ', coder.run
-          assert_equal'GGARCIAM', coder.run
+          assert_equal 'GABRIEL.MARQUEZ', coder.run
+          assert_equal 'GARCIA.MARQUEZ', coder.run
+          assert_equal 'GABRIEL.GARCIA.MARQUEZ', coder.run
         end
 
         test 'offensive words should not be seen' do
-          coder = Codification.unsuffixed_user_names %w[yilmaz ali rak]
+          options = { alternative: :abbreviated, interfix: nil }
+          coder = Codification.unsuffixed_user_names(%w[yilmaz ali rak], **options)
 
           assert_equal 'yalir', coder.run
           assert_equal 'yilmazar', coder.run
@@ -74,7 +97,8 @@ module Nokul
         end
 
         test 'reserved words should not be seen' do
-          coder = Codification.unsuffixed_user_names %w[william hile]
+          options = { alternative: :abbreviated, interfix: nil }
+          coder = Codification.unsuffixed_user_names(%w[william hile], **options)
 
           assert_equal 'williamh', coder.run
           assert_equal 'williamhile', coder.run
@@ -82,8 +106,14 @@ module Nokul
           assert_raise(StopIteration) { coder.run }
         end
 
-        test 'can produce available names' do
+        test 'can produce (non abbreviated) available names' do
           available = Codification.unsuffixed_user_names(%w[suat alak]).available(3)
+          assert_equal %w[suat.alak], available
+        end
+
+        test 'can produce abbreviated available names' do
+          options = { alternative: :abbreviated, interfix: nil }
+          available = Codification.unsuffixed_user_names(%w[suat alak], **options).available(3)
           assert_equal %w[suata suatalak], available
         end
       end
