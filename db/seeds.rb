3f685b41e20b0ad64f540c74033c839e96d33c1f
isubas <irfan.subas@uzem.omu.edu.tr>
2019-04-19 16:58:26 +0300

    Seed ve dump dosyalarında ufak iyileştirmeler yap

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,36 +6,61 @@ def load_seed_data
   end
 end
 
-def extract_file(file_name)
-  extraction_path = Rails.root.join('db', 'encrypted_data', "#{file_name}.tar.gz").to_s
-  `tar xvzf "#{extraction_path}" -C tmp/`
-end
+class RestoreEncryptedSql
+  ENCRYTED_DATA_DIR = 'db/encrypted_data'
+  COMPRESS_EXT      = '.tar.gz'
 
-def decrypt_and_write_to_file(file_name)
-  file_to_decrypt = "tmp/#{file_name}.sql.enc"
-  file_to_write = "tmp/#{file_name}.sql"
-  File.write(
-    file_to_write,
-    Support::Sensitive.read(file_to_decrypt)
-  )
-end
+  def self.call(file_name)
+    new(file_name).restore
+  end
 
-def restore_sql_dump(file_name)
-  config = ActiveRecord::Base.configurations[Rails.env]
-  dump_file = Rails.root.join('tmp', "#{file_name}.sql")
+  attr_reader :file_name
 
-  connection = "PGPASSWORD=#{config['password']} psql \
-               -h #{config['host']} \
-               -d #{config['database']} \
-               -U #{config['username']} "
+  def initialize(file_name)
+    @file_name = file_name
+    @dir       = Dir.tmpdir
+  end
 
-  `#{connection + "-f #{dump_file}"}`
-end
+  def restore
+    file = prepare_file_for_restored
 
-def restore_from_backup(file_name)
-  extract_file(file_name)
-  decrypt_and_write_to_file(file_name)
-  restore_sql_dump(file_name)
+    `#{db_connection + " -f #{file.path}"}`
+
+    file.close
+  end
+
+  private
+
+  attr_reader :dir
+
+  def db_connection
+    config     = ActiveRecord::Base.configurations[Rails.env]
+
+    "PGPASSWORD=#{config['password']} psql \
+    -h #{config['host']} \
+    -U #{config['username']} \
+    -d #{config['database']}"
+  end
+
+  def extract
+    path = Rails.root.join(ENCRYTED_DATA_DIR, "#{file_name}#{COMPRESS_EXT}")
+
+    `tar xvzf "#{path}" -C #{dir}`.strip
+  end
+
+  def prepare_file_for_restored
+    extracted_file = extract.delete_suffix('.enc')
+
+    tmp = Tempfile.new("#{file_name}-#{Time.now.to_i}.sql")
+
+    # https://www.endpoint.com/blog/2015/01/28/postgres-sessionreplication-role
+    tmp << <<-SQL
+      SET session_replication_role = replica;
+      #{Support::Sensitive.read("#{dir}/#{extracted_file}")}
+      SET session_replication_role = origin;
+    SQL
+    tmp
+  end
 end
 
 # Seed logic goes below
@@ -49,10 +74,10 @@ if ENV['SYNC'].eql?('true')
     Osym::ImportProspectiveStudentsJob.perform_later('db/encrypted_data/prospective_students.csv')
   end
 else
-  restore_from_backup('static_data')
+  RestoreEncryptedSql.call('static_data')
 
   if ENV['SAMPLE_DATA'].eql?('true')
-    restore_from_backup('sample_data') if ENV['SAMPLE_DATA'].eql?('true')
+    RestoreEncryptedSql.call('sample_data')
     load_seed_data
   end
 end
