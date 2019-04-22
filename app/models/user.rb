c6cf82bc7cb1e0279d849054da340b60e7d62689
isubas <irfan.subas@uzem.omu.edu.tr>
2019-04-22 20:33:29 +0300

    User modeline yetkilendirme ile ilgili modülleri ekle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  # authorizations
+  include Patron::Rolable
+  include Patron::Scopable
+
   # virtual attributes
   attr_accessor :country
 
