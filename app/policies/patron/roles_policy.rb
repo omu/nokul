d9ae9b1afb4167d4790dc7b2f247a3f8d160a73f
isubas <irfan.subas@uzem.omu.edu.tr>
2019-04-26 16:50:32 +0300

    Add role policy

---
+++ app/policies/patron/roles_policy.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+module Patron
+  class RolesPolicy < ApplicationPolicy
+    def index?
+      false
+    end
+
+    def show?
+      false
+    end
+
+    def create?
+      false
+    end
+
+    def new?
+      create?
+    end
+
+    def update?
+      false
+    end
+
+    def edit?
+      update?
+    end
+
+    def destroy?
+      false
+    end
+  end
+end
---
+++ test/policies/patron/roles_policy_test.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Patron::RolesPolicyTest < ActiveSupport::TestCase
+end
