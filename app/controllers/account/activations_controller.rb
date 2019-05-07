ba7f2d9e377bec6f1aa4c55d0d1ae23427cb88c2
ecmelkytz <ecmel.kytz@gmail.com>
2019-05-07 15:29:40 +0300

    Refactor activation service for prospective employee

--- app/controllers/account/activations_controller.rb
+++ app/controllers/account/activations_controller.rb
@@ -7,11 +7,11 @@ module Account
     layout 'guest'
 
     def new
-      @activation = Activation::StudentActivationService.new
+      @activation = Activation::ActivationService.new
     end
 
     def create
-      @activation = Activation::StudentActivationService.new(params[:activation])
+      @activation = Activation::ActivationService.new(params[:activation])
 
       if @activation.active
         redirect_to login_path, notice: t('.success')
