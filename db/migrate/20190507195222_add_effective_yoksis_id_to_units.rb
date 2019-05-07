ba78064bb642db7b8b01b06249d44eaecd90b04a
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-05-08 00:52:12 +0300

    Fix indentation

--- db/migrate/20190507195222_add_effective_yoksis_id_to_units.rb
+++ db/migrate/20190507195222_add_effective_yoksis_id_to_units.rb
@@ -2,6 +2,6 @@
 
 class AddEffectiveYoksisIdToUnits < ActiveRecord::Migration[6.0]
   def change
-  	add_column :units, :effective_yoksis_id, :string, comment: 'Bir birimin birlikte yonetildigi ust birimi ifade eder'
+    add_column :units, :effective_yoksis_id, :string, comment: 'Bir birimin birlikte yonetildigi ust birimi ifade eder'
   end
 end
