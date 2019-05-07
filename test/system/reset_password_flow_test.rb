c8a0dda2f83f8477fcd7700c975956d1d7c8188e
msdundar <msdundars@gmail.com>
2019-05-07 23:22:24 +0200

    Fix broken tests

--- test/system/reset_password_flow_test.rb
+++ test/system/reset_password_flow_test.rb
@@ -8,11 +8,10 @@ class ResetPasswordFlowTest < ApplicationSystemTestCase
     test "can login with correct credentials with a #{resolution} screen" do
       page.driver.browser.manage.window.resize_to(resolution[0], resolution[1])
       visit(recover_path)
-      assert find_button(t('account.passwords.new.send_instructions'), visible: true).visible?
+      assert find_button(t('account.passwords.new.reset_password'), visible: true).visible?
       assert find_link(t('account.passwords.new.login'), visible: true).visible?
-      assert find_link(t('account.passwords.new.register'), visible: true).visible?
       fill_in('user[email]', with: users(:serhat).email)
-      click_button(t('account.passwords.new.send_instructions'))
+      click_button(t('account.passwords.new.reset_password'))
       assert_equal t('devise.passwords.send_instructions'), page.find('div', class: 'toast-message').text
     end
   end
