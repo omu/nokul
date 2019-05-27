21a56ff2a4c86e17e44fe7e51da9791e1d5cc63f
isubas <irfan.subas@uzem.omu.edu.tr>
2019-05-27 17:30:03 +0300

    Ldap senkronizasyonu LdapSyncJob ile sağla

--- app/jobs/ldap_sync_job.rb
+++ app/jobs/ldap_sync_job.rb
@@ -3,7 +3,13 @@
 class LdapSyncJob < ApplicationJob
   queue_as :high
 
-  def perform(*args)
-    # Do something later
+  def perform(entity)
+    if Ldap::Client.create(entity)
+      entity.update(synchronized_at: Time.current, status: :success)
+      entity.ldap_sync_errors.update(resolved: true)
+    else
+      entity.update(status: :fail)
+      entity.ldap_sync_errors.create(description: Ldap::Client.results.message)
+    end
   end
 end
