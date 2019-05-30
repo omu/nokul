6b0b2a7df064c8709c980c791b07494afbc03aa3
ecmelkytz <ecmel.kytz@gmail.com>
2019-05-30 14:13:02 +0300

    Fix rubocop method length offence

--- app/controllers/account/activations_controller.rb
+++ app/controllers/account/activations_controller.rb
@@ -18,24 +18,21 @@ module Account
       end
     end
 
-    # rubocop:disable Metrics/MethodLength
     # rubocop:disable Metrics/AbcSize
     def check_phone_verification
       verify = params[:phone_verification]
       response = Twilio::Verify.check_verification_code(verify[:mobile_phone], verify[:verification_code])
 
       if response == 'ok'
-        if update_process(params[:students], params[:employees], params[:user], verify[:mobile_phone])
-          redirect_to login_path, notice: t('.success')
-        else
-          redirect_to activation_path, alert: t('account.activations.system_error')
-        end
+        return redirect_to login_path, notice: t('.success') if update_process(params[:students], params[:employees],
+                                                                               params[:user], verify[:mobile_phone])
+
+        redirect_to activation_path, alert: t('account.activations.system_error')
       else
         redirect_to activation_path,
                     alert: t("twilio.errors.#{response.error_code}", default: t('account.activations.system_error'))
       end
     end
-    # rubocop:enable Metrics/MethodLength
     # rubocop:enable Metrics/AbcSize
 
     private
@@ -44,8 +41,11 @@ module Account
       ActiveRecord::Base.transaction do
         ProspectiveStudent.archive(prospective_student_ids)
         ProspectiveEmployee.archive(prospective_employee_ids)
-        User.find(user_id).update(mobile_phone: phone, activated: true, activated_at: Time.zone.now)
+        User.find(user_id).update!(mobile_phone: phone, activated: true, activated_at: Time.zone.now)
       end
+    rescue StandardError => e
+      Rails.logger.error e.message
+      false
     end
 
     def check_user_login
