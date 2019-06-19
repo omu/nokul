07afb246fe5aad1a5e914564ef66acf5cf55faef
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-06-19 12:51:01 +0300

    Add client code for Meksis

---
+++ app/services/xokul/meksis.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Xokul
+  module Meksis
+    module_function
+
+    def buildings
+      Connection.request('/meksis/buildings')
+    end
+
+    def classrooms(building_id)
+      Connection.request('/meksis/classrooms', params: { building_id: building_id })
+    end
+
+    def syllabuses_by_classroom(classroom_id, year, term)
+      Connection.request(
+        '/meksis/classroom_syllabuses',
+        params: { classroom_id: classroom_id, year: year, term: term }
+      )
+    end
+
+    def syllabuses_by_department(unit_id, year, term)
+      Connection.request(
+        '/meksis/department_syllabuses',
+        params: { unit_id: unit_id, year: year, term: term }
+      )
+    end
+
+    def syllabuses_by_unit(unit_id, year, term)
+      Connection.request(
+        '/meksis/unit_syllabuses',
+        params: { unit_id: unit_id, year: year, term: term }
+      )
+    end
+  end
+end
