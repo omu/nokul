59543de9caa45d53fe308d9883c2cee97b302811
ecmelkytz <ecmel.kytz@gmail.com>
2019-06-24 12:32:41 +0300

    Remove redundant path

--- config/routes/account.rb
+++ config/routes/account.rb
@@ -19,7 +19,6 @@ end
 scope module: :account do
   get 'activation', to: 'activations#new'
   post 'activation', to: 'activations#create'
-  get 'phone_verification', to: 'activations#phone_verification'
   post 'activation_phone_verification', to: 'activations#check_phone_verification'
   get '/profile', to: 'profile_settings#edit'
   patch '/profile', to: 'profile_settings#update'
