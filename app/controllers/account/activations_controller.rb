18b534ba347147641a7c1708282f8522cc2343bf
ecmelkytz <ecmel.kytz@gmail.com>
2019-06-27 10:52:39 +0300

    Minor changes

--- app/controllers/account/activations_controller.rb
+++ app/controllers/account/activations_controller.rb
@@ -21,22 +21,22 @@ module Account
 
     # rubocop:disable Metrics/AbcSize
     def check_phone_verification
-      verify = params[:phone_verification]
       response = check_verification_code
 
       return redirect_to_with_twilio_error(response, activation_path) unless response.ok?
-      return redirect_to login_path, notice: t('.success') if update_process(session[:user_id], verify[:mobile_phone])
+      return redirect_to login_path, notice: t('.success') if update_process(session[:user_id],
+                                                                             params[:phone_verification][:mobile_phone])
 
       redirect_to error_path, alert: t('errors.system_error')
     end
     # rubocop:enable Metrics/AbcSize
 
-    def update_process(user_id, phone)
+    def update_process(user_id, mobile_phone)
       user = User.find(user_id)
       user.transaction do
         user.prospective_students.registered.update(archived: true)
         user.prospective_employees.update(archived: true)
-        user.update!(mobile_phone: phone, activated: true, activated_at: Time.zone.now)
+        user.update!(mobile_phone: mobile_phone, activated: true, activated_at: Time.zone.now)
       end
     rescue ActiveRecord::RecordNotFound, ActiveRecord::RecordInvalid => e
       Rollbar.error(e, e.message)
