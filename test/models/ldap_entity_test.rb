17f62ace32b8f9bdadf005f854edf16c3b4d4080
isubas <irfan.subas@uzem.omu.edu.tr>
2019-07-01 16:12:49 +0300

    Eksik model testini ekle

--- test/models/ldap_entity_test.rb
+++ test/models/ldap_entity_test.rb
@@ -4,16 +4,22 @@ require 'test_helper'
 
 class LdapEntityTest < ActiveSupport::TestCase
   extend Support::Minitest::AssociationHelper
+  extend Support::Minitest::CallbackHelper
   extend Support::Minitest::EnumerationHelper
   extend Support::Minitest::ValidationHelper
 
   # enums
-  enum status: { start: 0, pending: 1, success: 2, fail: 3 }
+  enum status: { pending: 0, synchronized: 1, failed: 2 }
 
   # relations
   belongs_to :user
+  has_many :ldap_sync_errors, dependent: :destroy
 
   # validations: presence
   validates_presence_of :values
   validates_presence_of :dn
+
+  # callbacks
+  after_commit :start_sync
+  before_validation :set_synchronized_at
 end
