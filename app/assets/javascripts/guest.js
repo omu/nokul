7953204e7938c84b025508eb883ba6a1805836d0
ecmelkytz <ecmel.kytz@gmail.com>
2019-07-02 15:27:45 +0300

    Add mask to mobile phone input fields

--- app/assets/javascripts/guest.js
+++ app/assets/javascripts/guest.js
@@ -5,5 +5,6 @@
 //= require toastr/build/toastr.min
 //= require shared/toastr_config
 //= require select2/dist/js/select2.min
+//= require jquery.maskedinput/src/jquery.maskedinput
 //= require flatpickr/dist/flatpickr.min
 //= require flatpickr/dist/l10n/tr
