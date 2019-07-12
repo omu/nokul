6b611f739351a5b370d6c7aec47dcb02f8d8943b
Recai Oktaş <roktas@gmail.com>
2019-07-12 11:59:54 +0300

    Fix Eslint offenses

--- app/assets/javascripts/shared/intl_tel_input_validation.js
+++ app/assets/javascripts/shared/intl_tel_input_validation.js
@@ -1,14 +1,14 @@
 // validate the telephone number on the blur event
 
-var TelInputValidation = function(input_id, error_msg_id, error_messages) {
-  var input = document.querySelector(input_id),
-    error_msg = document.querySelector(error_msg_id);
+var TelInputValidation = function (inputId, errorMsgId, errorMessages) { // eslint-disable-line no-unused-vars
+  var input = document.querySelector(inputId)
+  var errorMsg = document.querySelector(errorMsgId)
 
   var reset = function () {
-    input.classList.remove('is-invalid');
-    input.classList.remove('is-valid');
-    error_msg.innerHTML = '';
-  };
+    input.classList.remove('is-invalid')
+    input.classList.remove('is-valid')
+    errorMsg.innerHTML = ''
+  }
 
   var iti = window.intlTelInput(input, {
     utilsScript: 'assets/shared/intl_tel_input_utils.js',
@@ -17,19 +17,19 @@ var TelInputValidation = function(input_id, error_msg_id, error_messages) {
     preferredCountries: ['tr']
   })
 
-  input.addEventListener('blur', function() {
-    reset();
+  input.addEventListener('blur', function () {
+    reset()
     if (input.value.trim()) {
       if (iti.isValidNumber()) {
-        input.classList.add('is-valid');
+        input.classList.add('is-valid')
       } else {
-        input.classList.add('is-invalid');
-        var error_code = iti.getValidationError();
-        error_msg.innerHTML = error_messages[error_code] || error_messages[0]
+        input.classList.add('is-invalid')
+        var errorCode = iti.getValidationError()
+        errorMsg.innerHTML = errorMessages[errorCode] || errorMessages[0]
       }
     }
-  });
+  })
 
-  input.addEventListener('change', reset);
-  input.addEventListener('keyup', reset);
+  input.addEventListener('change', reset)
+  input.addEventListener('keyup', reset)
 }
