9be83ec9388e4c820252196e2986c51168da099f
ecmelkytz <ecmel.kytz@gmail.com>
2019-09-12 08:53:21 +0300

    Use instance variable

--- app/controllers/account/settings/phones_controller.rb
+++ app/controllers/account/settings/phones_controller.rb
@@ -9,11 +9,9 @@ module Account
 
       def edit; end
 
-      # rubocop:disable Metrics/AbcSize
       def verification
-        phone = current_user[:mobile_phone] = TelephoneNumber.parse(params.dig(:user, :mobile_phone)).e164_number
-
-        return respond_to :js unless current_user.valid? && current_user.mobile_phone_changed?
+        phone = @user.mobile_phone = TelephoneNumber.parse(params.dig(:user, :mobile_phone)).e164_number
+        return respond_to :js unless @user.valid? && @user.mobile_phone_changed?
 
         flash[:alert] = t('errors.system_error') unless Twilio::Verify.send_phone_verification_code(phone).ok?
 
@@ -25,11 +23,10 @@ module Account
         response = check_verification_code
 
         return redirect_to_with_twilio_error(response, settings_path) unless response.ok?
-        return redirect_to settings_path, notice: t('.success') if current_user.update(mobile_phone: phone)
+        return redirect_to settings_path, notice: t('.success') if @user.update(mobile_phone: phone)
 
         redirect_to settings_path, alert: t('errors.system_error')
       end
-      # rubocop:enable Metrics/AbcSize
 
       private
 
