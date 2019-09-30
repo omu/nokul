ffbe1769bc908c4f3d063bf3aa21c9f18b4e704d
ecmelkytz <ecmel.kytz@gmail.com>
2019-09-30 16:27:26 +0300

    Change parameter

--- app/jobs/yoksis/academic_credentials_save_job.rb
+++ app/jobs/yoksis/academic_credentials_save_job.rb
@@ -18,7 +18,7 @@ module Yoksis
         academic_credential = user.academic_credentials.find_or_initialize_by(yoksis_id: credential[:id])
         academic_credential.assign_attributes(
           activity:         credential[:activity_id],
-          country_id:       Country.find_by(yoksis_code: credential[:country_id]),
+          country:          Country.find_by(yoksis_code: credential[:country_id]),
           end_year:         credential[:end_date],
           location:         credential[:location_id],
           scientific_field: credential[:scientific_field_name],
