3fa3311fc46428230f3da64b4a2ac219375ca2f2
dilara <adilarakoca@gmail.com>
2019-10-09 15:43:06 +0300

    Show course catalog for each student record

--- app/controllers/course_management/enrolled_courses_controller.rb
+++ app/controllers/course_management/enrolled_courses_controller.rb
@@ -2,11 +2,38 @@
 
 module CourseManagement
   class EnrolledCoursesController < ApplicationController
+    before_action :set_student
+    before_action :set_curriculum
+    before_action :set_term
+
     def index
-      student = current_user.students.last
-      term = AcademicTerm.active.last.try(:term)
-      @curriculum = student.unit.curriculums.active.last
-      @semesters = @curriculum.semesters.where(term: term).order(:year, :sequence)
+      @students = current_user.students.includes(:unit)
+      @semesters = @curriculum.semesters.where(term: @term.term).order(:sequence)
+    end
+
+    private
+
+    def redirect_with(message)
+      redirect_to(:root, alert: t(".#{message}"))
+    end
+
+    def set_student
+      @student =
+        if (student_id = params[:student_id].presence)
+          current_user.students.find(student_id)
+        else
+          current_user.students.first
+        end
+    end
+
+    def set_curriculum
+      @curriculum = @student.unit.curriculums.active.last
+      redirect_with('active_curriculum_not_found') unless @curriculum
+    end
+
+    def set_term
+      @term = AcademicTerm.active.last
+      redirect_with('active_term_not_found') unless @term
     end
   end
 end
