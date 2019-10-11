37eeebba595e0e92c488b268c548140fcd1959ae
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-10-11 08:33:24 +0300

    Refactor connection layer

--- app/services/xokul/connection.rb
+++ app/services/xokul/connection.rb
@@ -2,40 +2,39 @@
 
 module Xokul
   module Connection
-    BASE_URL     = Tenant.configuration.api_host
-    BEARER_TOKEN = Tenant.credentials.dig(:xokul, :bearer_token)
+    BASE_URL        = Tenant.configuration.api_host
+    BEARER_TOKEN    = Tenant.credentials.dig(:xokul, :bearer_token)
+    DEFAULT_OPTIONS = {
+      headers:     {
+        'Authorization' => "Bearer #{BEARER_TOKEN}",
+        'Content-Type'  => 'application/json'
+      },
+      use_ssl:     true,
+      verify_mode: OpenSSL::SSL::VERIFY_PEER
+    }.freeze
 
-    private_constant :BASE_URL, :BEARER_TOKEN
+    private_constant :BASE_URL, :BEARER_TOKEN, :DEFAULT_OPTIONS
 
-    # rubocop:disable Metrics/MethodLength
-    def self.request(path, params: {}, **http_options)
-      http_options[:open_timeout] ||= http_options[:read_timeout] ||= 10 if Rails.env.test?
+    def self.request(path, params: {}, **options)
+      options[:open_timeout] ||= options[:read_timeout] ||= 10 if Rails.env.test?
 
       response = Support::RestClient.get(
-        URI.join(BASE_URL, path).to_s,
-        headers:     {
-          'Authorization' => "Bearer #{BEARER_TOKEN}",
-          'Content-Type'  => 'application/json'
-        },
-        payload:     params.to_json,
-        use_ssl:     true,
-        verify_mode: OpenSSL::SSL::VERIFY_PEER,
-        **http_options
+        URI.join(BASE_URL, path).to_s, payload: params.to_json, **DEFAULT_OPTIONS.merge(options)
       )
 
-      response.error!
-
-      unmarshal = response.unmarshal_json
-      case unmarshal
-      when Array
-        unmarshal.map(&:deep_symbolize_keys)
-      when Hash
-        unmarshal.deep_symbolize_keys
-      else
-        unmarshal
-      end
+      response.error! unless response.success?
+      decode(response.body)
+    rescue Net::HTTPError => e
+      warn e
     end
-    # rubocop:enable Metrics/MethodLength
+
+    def self.decode(data)
+      return unless data
+
+      JSON.parse(data)
+    rescue JSON::ParserError
+      warn "Unparseable object: #{object}"
+    end
   end
 
   private_constant :Connection
