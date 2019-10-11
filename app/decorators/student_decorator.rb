1ba9281e4efa1bc94dacfdbadfb6444cccf8b73b
dilara <adilarakoca@gmail.com>
2019-10-11 14:40:54 +0300

    Add date range check for course registration

---
+++ app/decorators/student_decorator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class StudentDecorator < SimpleDelegator
+  def calendar
+    unit.calendars.find_by(academic_term: term)
+  end
+
+  def registrable_for_online_course?
+    calendar.check_events('online_course_registrations')
+  end
+
+  private
+
+  def term
+    AcademicTerm.active.last
+  end
+end
