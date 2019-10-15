051989dc3d266f2924f5b51aeabcd55d87a02c49
Dilara Koca <adilarakoca@gmail.com>
2019-10-15 15:53:53 +0300

    Remove redundant active term/calendar method

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -2,16 +2,10 @@
 
 class StudentDecorator < SimpleDelegator
   def calendar
-    unit.calendars.find_by(academic_term: term)
+    calendars.last
   end
 
   def registrable_for_online_course?
     calendar&.check_events('online_course_registrations')
-  end
-
-  private
-
-  def term
-    AcademicTerm.active.last
   end
 end
