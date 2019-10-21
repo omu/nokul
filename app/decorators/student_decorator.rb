b613fc0156b59dee24e3c6f9367afa6b978fc1a2
Dilara Koca <adilarakoca@gmail.com>
2019-10-21 10:03:52 +0300

    Fix rubocop offenses

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -32,8 +32,8 @@ class StudentDecorator < SimpleDelegator
 
   def available_courses_for_semester(semester)
     semester.available_courses.includes(curriculum_course: :course)
-                              .where(academic_term: active_term)
-                              .where.not(id: enrolled_courses.pluck(:available_course_id))
-                              .order('courses.name')
+            .where(academic_term: active_term)
+            .where.not(id: enrolled_courses.pluck(:available_course_id))
+            .order('courses.name')
   end
 end
