3113d413019f64bd2e285aed4c9a32d886965ce1
Dilara Koca <adilarakoca@gmail.com>
2019-10-21 15:34:15 +0300

    Refactor and add ects methods

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class StudentDecorator < SimpleDelegator
+  TOTAL_ECTS = 30
+
+  def active_term
+    @active_term ||= AcademicTerm.active.last
+  end
+
   def calendar
     calendars.last
   end
@@ -9,19 +15,22 @@ class StudentDecorator < SimpleDelegator
     calendar&.check_events('online_course_registrations')
   end
 
-  def course_catalog
+  def selected_ects
+    selected_courses.sum(&:ects).to_i
+  end
+
+  def selectable_ects
+    TOTAL_ECTS - selected_ects
+  end
+
+  def selectable_courses
     semesters = curriculum.semesters.where(term: active_term.term).order(:sequence)
     semesters.collect { |semester| [semester, available_courses_for_semester(semester)] }
   end
 
-  # TODO
-  def enrolled_courses
-    course_enrollments.includes(:available_course)
+  def selected_courses
+    course_enrollments.includes(available_course: [curriculum_course: :course])
                       .where(semester: semester)
-  end
-
-  def active_term
-    AcademicTerm.active.last
   end
 
   private
@@ -30,10 +39,11 @@ class StudentDecorator < SimpleDelegator
     curriculums.active.last
   end
 
-  def available_courses_for_semester(semester)
-    semester.available_courses.includes(curriculum_course: :course)
-            .where(academic_term: active_term)
-            .where.not(id: enrolled_courses.pluck(:available_course_id))
-            .order('courses.name')
+  # TODO: fix for selected elective courses
+  def available_courses_for_semester(curriculum_semester)
+    curriculum_semester.available_courses.includes(curriculum_course: :course)
+                       .where(academic_term: active_term)
+                       .where.not(id: selected_courses.pluck(:available_course_id))
+                       .order('courses.name')
   end
 end
