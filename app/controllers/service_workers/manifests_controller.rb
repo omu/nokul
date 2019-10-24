2f47982f33612a12b5e0a635afdce147ce4d2d08
Onur Baran <onur.baran@pivot.us>
2019-10-24 17:05:59 +0300

    Fix Rubocop offenses

--- app/controllers/service_workers/manifests_controller.rb
+++ app/controllers/service_workers/manifests_controller.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 module ServiceWorkers
   class ManifestsController < ApplicationController
     skip_before_action :authenticate_user!
-    
-    def index
-    end
+
+    def index; end
   end
 end
