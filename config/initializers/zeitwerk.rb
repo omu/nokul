3b48c7f5834961d6818f8b334d1989b3506b45fe
isubas <irfan.subas@uzem.omu.edu.tr>
2019-11-06 11:01:19 +0300

    Upgrade dependencies

--- config/initializers/zeitwerk.rb
+++ config/initializers/zeitwerk.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
-class CustomInflector < Zeitwerk::Inflector
-  def camelize(basename, _abspath)
-    case basename
-    when 'ldap' then 'LDAP'
-    else
-      super
-    end
-  end
-end
-
 Rails.autoloaders.each do |autoloader|
-  autoloader.inflector = CustomInflector.new
+  autoloader.inflector.inflect(
+    'ldap' => 'LDAP'
+  )
 end
