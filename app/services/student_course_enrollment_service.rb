6929a9f1efbe7cb4ddf066f3e975848f97c2da97
Dilara Koca <adilarakoca@gmail.com>
2019-11-28 13:11:28 +0300

    Add elective course addition validations

--- app/services/student_course_enrollment_service.rb
+++ app/services/student_course_enrollment_service.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/ClassLength
 class StudentCourseEnrollmentService
   ECTS = 30
 
@@ -22,7 +23,7 @@ class StudentCourseEnrollmentService
   def semester_enrollments
     @semester_enrollments ||=
       @student.course_enrollments.where(semester: @student.semester)
-                                 .includes(available_course: [curriculum_course: %i[course curriculum_semester]])
+              .includes(available_course: [curriculum_course: %i[course curriculum_semester]])
   end
 
   def enrollment_status
@@ -57,17 +58,16 @@ class StudentCourseEnrollmentService
   end
 
   def ensure_addable(available_course)
+    if available_course.type == 'elective'
+      group_selectable_ects = selectable_ects_from_group(available_course.curriculum_course.curriculum_course_group)
+      available_course.errors.add(:base, translate('already_enrolled_at_group')) if group_selectable_ects.zero?
+      available_course.errors.add(:base, translate('not_enough_ects')) if group_selectable_ects < available_course.ects
+    end
+
     available_course.errors.add(:base, translate('not_enough_ects')) if selectable_ects < available_course.ects
     available_course.errors.add(:base, translate('quota_full')) if available_course.quota_full?
 
     available_course
-  end
-
-  def enroll_a_course_from_group?(group)
-    semester_enrollments.includes(available_course: :curriculum_course)
-                        .where(curriculum_courses: { curriculum_course_group_id: group.id })
-                        .sum(:ects)
-                        .eql?(group.ects)
   end
 
   private
@@ -83,7 +83,7 @@ class StudentCourseEnrollmentService
   end
 
   def curriculum
-    @student.curriculums.active.last
+    @curriculum ||= @student.curriculums.active.last
   end
 
   def curriculum_semesters
@@ -100,20 +100,30 @@ class StudentCourseEnrollmentService
                        .each { |available_course| ensure_addable(available_course) }
   end
 
-  def elective_courses_for(curriculum_semester)
-    curriculum_semester.curriculum_course_groups.each_with_object([]) do |group, group_courses|
-      courses = group.available_courses
-                     .where.not(id: semester_enrollments.pluck(:available_course_id))
-                     .where(academic_term_id: active_term.id)
-                     .each { |available_course| ensure_addable(available_course) }
+  def group_courses(group)
+    group.available_courses
+         .where.not(id: semester_enrollments.pluck(:available_course_id))
+         .where(academic_term_id: active_term.id)
+  end
 
-      group_courses << { group: group, courses: courses }
+  def ensure_enrollable(group)
+    group_selectable_ects = selectable_ects_from_group(group)
+
+    group_courses(group).each_with_object([]) do |course, courses|
+      course.errors.add(:base, translate('already_enrolled_at_group')) if group_selectable_ects.zero?
+      if (course.ects > group_selectable_ects) || (course.ects > selectable_ects)
+        course.errors.add(:base, translate('not_enough_ects'))
+      end
+      course.errors.add(:base, translate('quota_full')) if course.quota_full?
+
+      courses << course
     end
   end
 
-  def enrolled_in_required_courses_of?(curriculum_semester)
-    enrolled_in_compulsory_courses?(curriculum_semester) &&
-      enrolled_in_elective_courses?(curriculum_semester)
+  def elective_courses_for(curriculum_semester)
+    curriculum_semester.curriculum_course_groups.each_with_object([]) do |group, group_courses|
+      group_courses << { group: group, courses: ensure_enrollable(group) }
+    end
   end
 
   def enrolled_in_compulsory_courses?(curriculum_semester)
@@ -124,12 +134,23 @@ class StudentCourseEnrollmentService
                        .empty?
   end
 
+  def selectable_ects_from_group(group)
+    group.ects - semester_enrollments.includes(available_course: :curriculum_course)
+                                     .where(curriculum_courses: { curriculum_course_group_id: group.id })
+                                     .sum(:ects)
+  end
+
   def enrolled_in_elective_courses?(curriculum_semester)
     curriculum_semester.curriculum_course_groups.inject(true) do |enrolled, group|
-      break unless enrolled &&= !enroll_a_course_from_group?(group)
+      break unless enrolled &&= selectable_ects_from_group(group).zero?
 
       enrolled
     end
+  end
+
+  def enrolled_in_required_courses_of?(curriculum_semester)
+    enrolled_in_compulsory_courses?(curriculum_semester) &&
+      enrolled_in_elective_courses?(curriculum_semester)
   end
 
   def max_sequence
@@ -140,4 +161,4 @@ class StudentCourseEnrollmentService
     I18n.t("studentship.course_enrollments.errors.#{key}", params)
   end
 end
-  
+# rubocop:enable Metrics/ClassLength
