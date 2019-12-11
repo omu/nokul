60782c3d941b2b4a19ef03e7c3c36aa5d1e4a08f
ecmelkytz <ecmel.kytz@gmail.com>
2019-12-11 11:03:33 +0300

    Refactor prospective student validation

--- app/validators/prospective_student_validator.rb
+++ app/validators/prospective_student_validator.rb
@@ -2,22 +2,13 @@
 
 class ProspectiveStudentValidator < ActiveModel::Validator
   def validate(record)
-    return unless record.user
-    return unless registered?(record)
+    return unless record.registered?
     return unless record.unit_id_changed? || record.academic_term_id_changed?
 
     record.errors[:base] << message('unchangeable_variables')
   end
 
   private
-
-  def registered?(record)
-    record.user.students.find_by(unit_id: unit(record))&.permanently_registered?
-  end
-
-  def unit(record)
-    record.unit_id_changed? ? Unit.find(record.changes[:unit_id]&.first) : record.unit
-  end
 
   def message(key)
     I18n.t(key, scope: %i[validators prospective_student])
