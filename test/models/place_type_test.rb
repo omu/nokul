07127cee894e4b1f2a8406e7c03fab86f4be5e31
ecmelkytz <ecmel.kytz@gmail.com>
2019-12-18 09:40:52 +0300

    Add place type model test

--- test/models/place_type_test.rb
+++ test/models/place_type_test.rb
@@ -3,7 +3,14 @@
 require 'test_helper'
 
 class PlaceTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  extend Support::Minitest::AssociationHelper
+
+  # relations
+  has_one :building, dependent: :destroy
+  has_one :classroom, dependent: :destroy
+
+  # custom methods
+  test 'number_of_children method' do
+    assert_equal place_types(:egitim).children.count, place_types(:egitim).number_of_children
+  end
 end
