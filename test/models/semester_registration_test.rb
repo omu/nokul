65860fb6364440e6437b9425528442e93d624b27
dilara <adilarakoca@gmail.com>
2020-01-02 15:22:22 +0300

    Add tests for semester registration

---
+++ test/models/semester_registration_test.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SemesterRegistrationTest < ActiveSupport::TestCase
+  extend Support::Minitest::AssociationHelper
+  extend Support::Minitest::CallbackHelper
+  extend Support::Minitest::EnumerationHelper
+  extend Support::Minitest::ValidationHelper
+
+  # callbacks
+  before_validation :assign_academic_term
+  before_validation :assign_semester
+
+  # enums
+  enum status: { draft: 0, saved: 1 }
+
+  # relations
+  belongs_to :academic_term
+  belongs_to :student
+  has_many :course_enrollments, dependent: :destroy
+
+  # validations: uniqueness
+  test 'uniqueness validations for semester registration of a semester' do
+    fake = semester_registrations(:serhat_third_semester).dup
+    assert_not fake.valid?
+    assert_not_empty fake.errors[:semester]
+  end
+end
