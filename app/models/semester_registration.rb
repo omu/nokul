f23c9b90d9871b8402e994d7357273bb6fa68d42
dilara <adilarakoca@gmail.com>
2020-01-02 14:59:38 +0300

    Set academic term and semester of registration

--- app/models/semester_registration.rb
+++ app/models/semester_registration.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class SemesterRegistration < ApplicationRecord
+  # callbacks
+  before_validation :assign_academic_term
+  before_validation :assign_semester
+
   # enums
   enum status: { draft: 0, saved: 1 }
 
@@ -12,4 +16,14 @@ class SemesterRegistration < ApplicationRecord
   # validations
   validates :semester, uniqueness: { scope: :student_id }
   validates :semester, numericality: { greater_than: 0 }
+
+  private
+
+  def assign_academic_term
+    self.academic_term = AcademicTerm.current
+  end
+
+  def assign_semester
+    self.semester = student&.semester
+  end
 end
