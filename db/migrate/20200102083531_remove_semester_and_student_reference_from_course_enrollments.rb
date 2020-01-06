8314ed0de0c33471c135e95a1ec5cd512a98c443
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-01-06 14:24:11 +0300

    Remove not null constraint

--- db/migrate/20200102083531_remove_semester_and_student_reference_from_course_enrollments.rb
+++ db/migrate/20200102083531_remove_semester_and_student_reference_from_course_enrollments.rb
@@ -1,6 +1,6 @@
 class RemoveSemesterAndStudentReferenceFromCourseEnrollments < ActiveRecord::Migration[6.0]
   def change
-    remove_reference :course_enrollments, :student, null: false, foreign_key: true
+    remove_reference :course_enrollments, :student, foreign_key: true
     remove_numericality_constraint :course_enrollments, :semester, greater_than: 0
     remove_null_constraint :course_enrollments, :semester
     remove_column :course_enrollments, :semester, :integer, default: 1
