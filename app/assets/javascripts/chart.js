377debb534c88f18ef9d5a26b74815f104b62788
isubas <irfan.subas@uzem.omu.edu.tr>
2020-01-09 16:43:12 +0300

    Highchart için TR haritasının meta datalarını ekle

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -1,4 +1,4 @@
 //= require echarts/dist/echarts.min
 //= require highcharts/highcharts
 //= require highcharts/modules/map
-//= require @highcharts/map-collection/countries/tr/tr-all
+//= require highcharts-maps/tr
