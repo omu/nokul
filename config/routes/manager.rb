c86d5bb07056ba213a0c960f00a6b67f605d6843
isubas <irfan.subas@uzem.omu.edu.tr>
2020-01-15 16:52:59 +0300

    Grafik görünütülenme yapısını değiştir

--- config/routes/manager.rb
+++ config/routes/manager.rb
@@ -2,4 +2,12 @@
 
 namespace :manager do
   get '/', to: 'dashboard#index'
+
+  namespace :charts do
+    get :cities
+    get :double_major_and_minor
+    get :genders
+    get :genders_and_degrees
+    get :non_graduates
+  end
 end
