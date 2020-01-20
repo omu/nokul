42ff1592cbeefd541171445f705166953004a49c
isubas <irfan.subas@uzem.omu.edu.tr>
2020-01-20 16:02:21 +0300

    SSO yapılandırmaları için modül ekle

---
+++ app/lib/nokul/sso.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Nokul
+  module SSO
+    module_function
+
+    def disable?
+      !enable?
+    end
+
+    def enable?
+      ActiveRecord::Type::Boolean.new.cast(
+        ENV.fetch('NOKUL_SSO_ENABLE', false)
+      )
+    end
+  end
+end
