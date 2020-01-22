2c3a08ec4e3ba5761c7ad87f2a1e420c473ac34a
Onur Baran <onurbaran@ymail.com>
2020-01-22 23:54:39 +0300

    model relations

--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class Target < ApplicationRecord
+  belongs_to :course
+  belongs_to :icc
+
+  validates :course_id, presence: true
+  validates :icc_id, presence: true, uniqueness: { scope: :course_id }
 end
