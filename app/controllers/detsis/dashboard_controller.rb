1a00db0bab223b483d6717645b87935a4e418cf8
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-01-23 15:12:01 +0300

    Add dashboard controller for Detsis

---
+++ app/controllers/detsis/dashboard_controller.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Detsis
+  class DashboardController < ApplicationController
+    def index; end
+  end
+end
---
+++ test/controllers/detsis/dashboard_controller_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Detsis
+  class DashboardControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
