a15631eb076d35eb86d866a3199f0731d6647118
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-01-24 11:03:03 +0300

    Add model test for sdp code

--- test/models/sdp_code_test.rb
+++ test/models/sdp_code_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class SdpCodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'full_code must return the full of standardized sdp code' do
+    sdp_code_one = sdp_codes(:one)
+    sdp_code_two = sdp_codes(:two)
+
+    assert_equal sdp_code_one.full_code, '010.01.99.05'
+    assert_equal sdp_code_two.full_code, '045.99.00.10'
+  end
 end
