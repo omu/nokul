53be68a94ecf6cda7dbfa3d5785754d3a86ef93a
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-01-24 13:58:59 +0300

    Remove unrecognized configuration parameter

--- db/structure.sql
+++ db/structure.sql
@@ -11,7 +11,7 @@ SET row_security = off;
 
 SET default_tablespace = '';
 
-SET default_table_access_method = heap;
+SET default_with_oids = false;
 
 --
 -- Name: academic_credentials; Type: TABLE; Schema: public; Owner: -
