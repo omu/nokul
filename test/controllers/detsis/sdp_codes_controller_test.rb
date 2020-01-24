8cccab58e908854028d81f137c7de17db41cc69a
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-01-24 11:03:29 +0300

    Add controller test for sdp code

--- test/controllers/detsis/sdp_codes_controller_test.rb
+++ test/controllers/detsis/sdp_codes_controller_test.rb
@@ -4,8 +4,16 @@ require 'test_helper'
 
 module Detsis
   class SdpCodesControllerTest < ActionDispatch::IntegrationTest
-    # test "the truth" do
-    #   assert true
-    # end
+    setup do
+      sign_in users(:john)
+    end
+
+    test 'should get index' do
+      get detsis_sdp_codes_path
+
+      assert_response :success
+      assert_equal 'index', @controller.action_name
+      assert_select '#collapseSearch', t('smart_search')
+    end
   end
 end
