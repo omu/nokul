ebeede3b7046a61f2a9d02039dacb3c033ccfe7b
ecmelkytz <ecmel.kytz@gmail.com>
2020-02-04 15:56:31 +0300

    Create disability controller

---
+++ app/controllers/account/disability_controller.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Account
+  class DisabilityController < ApplicationController
+    before_action :set_user
+
+    def edit; end
+
+    def update
+      return redirect_to user_path(@user), notice: t('.success') if @user.update(user_params)
+
+      render :edit
+    end
+
+    private
+
+    def set_user
+      @user = User.friendly.find(params[:user_id])
+      not_found unless @user
+    end
+
+    def user_params
+      params.require(:user).permit(:disability_type_id, :disability_rate)
+    end
+  end
+end
