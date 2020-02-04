85ab5805ddf207d52906d9baf059054147d8ff5d
ecmelkytz <ecmel.kytz@gmail.com>
2020-02-04 16:13:19 +0300

    Fix typo

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -53,6 +53,7 @@ class UserTest < ActiveSupport::TestCase
   validates_length_of :website, maximum: 50
 
   # validations: numericality
+  validates_numericality_of :id_number
   validates_numericality_of :disability_rate
   validates_numericality_of :extension_number
   validates_numerical_range :disability_rate, greater_than_or_equal_to: 0, less_than_or_equal_to: 100
