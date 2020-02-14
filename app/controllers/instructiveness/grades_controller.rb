c5f8e0e1543e535e40a6e8dea663c5340fcc4d6d
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-02-14 15:55:56 +0300

    Add grades controller

---
+++ app/controllers/instructiveness/grades_controller.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+module Instructiveness
+  class GradesController < ApplicationController
+    before_action :set_employee
+    before_action :set_course
+    before_action :set_assessment
+
+    def edit
+      @groups = @course.groups
+      @grades = @assessment.grades
+    end
+
+    def update
+      @course.saved_enrollments.each do |enrollment|
+        new_point = grades_params[enrollment.id.to_s]
+        grade = enrollment.grades.find_or_initialize_by(course_assessment_method: @assessment)
+        grade.update(point: new_point) if grade.point != new_point
+      end
+
+      @assessment.update(status: :draft) ? redirect_with('success') : render(:edit)
+    end
+
+    private
+
+    def redirect_with(message)
+      redirect_to(edit_given_course_assessment_grades_path(@course, @assessment), flash: { info: message })
+    end
+
+    def set_employee
+      not_found if (@employee = current_user.current_employee).nil?
+    end
+
+    def set_course
+      @course = @employee.given_courses.find(params[:given_course_id])
+    end
+
+    def set_assessment
+      @assessment = @course.course_assessment_methods.find(params[:assessment_id])
+    end
+
+    def grades_params
+      params.require(:course_enrollment_grades)
+    end
+  end
+end
