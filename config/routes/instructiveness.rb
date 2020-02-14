3bac0e719e6ee57c90eb7c7922e95892e8c1490d
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-02-14 15:50:25 +0300

    Create assessment grades new and update routes

--- config/routes/instructiveness.rb
+++ config/routes/instructiveness.rb
@@ -2,6 +2,13 @@
 
 scope module: :instructiveness do
   resources :given_courses, only: %i[index show] do
+    resources :assessments, only: [] do
+      resources :grades, only: [] do
+        get :edit, on: :collection
+        post :update, on: :collection
+      end
+    end
+
     get :students, on: :member
   end
 end
