59f4337794ecc9db081d85c24c416e2296355552
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-17 13:32:30 +0300

    Configure Xokul parameters in a shy way for now

--- config/initializers/xokul.rb
+++ config/initializers/xokul.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-Xokul.configure do |config|
+Xokul::Configuration.configure do |config|
   config.endpoint = Tenant.configuration.api_host
   config.bearer_token = Tenant.credentials.xokul[:bearer_token]
 end
