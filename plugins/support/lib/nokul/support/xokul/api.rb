f9ff478d6aa9b5f90bb3b58cb166c5e3c8b4eca0
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-17 12:54:16 +0300

    Add an abstracted requester to API interface

--- plugins/support/lib/nokul/support/xokul/api.rb
+++ plugins/support/lib/nokul/support/xokul/api.rb
@@ -4,7 +4,11 @@ require_relative 'api/yoksis'
 
 module Xokul
   class API
-    def initialize
+    include ActiveSupport::Configurable
+
+    config_accessor :namespace, :version, instance_writer: false
+
+    def initialize(*)
       @conn = Connection.new(Configuration.endpoint)
       @conn.bearer_auth Configuration.bearer_token
     end
@@ -12,5 +16,13 @@ module Xokul
     protected
 
     attr_reader :conn
+
+    private
+
+    def request(path, params: {})
+      url  = URI.join(namespace, path).to_s
+      resp = conn.get(url, params: params)
+      resp.decode
+    end
   end
 end
