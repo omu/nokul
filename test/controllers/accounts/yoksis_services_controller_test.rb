93bfd9b6416a9aa0ac79a127f73ac3100dbad913
isubas <irfan.subas@uzem.omu.edu.tr>
2020-02-17 16:54:28 +0300

    Fix test

--- test/controllers/accounts/yoksis_services_controller_test.rb
+++ test/controllers/accounts/yoksis_services_controller_test.rb
@@ -5,10 +5,10 @@ require 'test_helper'
 module Accounts
   class YoksisServicesControllerTest < ActionDispatch::IntegrationTest
     setup do
-      sign_in @user
+      sign_in users(:serhat)
     end
 
-    SERVICES = {
+    {
       academic_credentials:   Yoksis::AcademicCredentialsSaveJob,
       articles:               Yoksis::ArticlesSaveJob,
       books:                  Yoksis::BooksSaveJob,
@@ -16,12 +16,11 @@ module Accounts
       education_informations: Yoksis::EducationInformationsSaveJob,
       papers:                 Yoksis::PapersSaveJob,
       projects:               Yoksis::ProjectsSaveJob
-    }.freeze
-
-    SERVICES.each do |service, _job|
+    }.each do |service, _job|
       test "should get fetch for #{service}" do
-        get yoksis_services_fetch_path(service: service)
-        assert_response :success
+        get yoksis_services_fetch_path(service: service.to_s)
+        assert_response :redirect
+        assert_equal t("account.yoksis_services.fetch.#{service}"), flash[:notice]
       end
     end
   end
