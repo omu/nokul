d86a1483b82f26a3934fab59ad509b640c123233
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-18 10:30:29 +0300

    Add a simple callback to use in extreme cases

--- plugins/support/lib/nokul/support/xokul/api.rb
+++ plugins/support/lib/nokul/support/xokul/api.rb
@@ -6,11 +6,15 @@ module Xokul
 
     config_accessor :synopsis, :version, :namespace, instance_writer: false
 
-    def initialize(*)
+    def initialize
       url   = URI.join(Configuration.endpoint, namespace).to_s
       @conn = Connection.new(url)
       @conn.bearer_auth Configuration.bearer_token
+
+      after_initialize!
     end
+
+    def after_initialize!; end
 
     protected
 
