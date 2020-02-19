d327b41ab0672076e8da6207374be345ca99eadd
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-19 13:43:13 +0300

    Adapt students endpoint to the new client

--- plugins/support/lib/nokul/support/xokul/api/yoksis.rb
+++ plugins/support/lib/nokul/support/xokul/api/yoksis.rb
@@ -3,5 +3,5 @@
 require_relative 'yoksis/graduates'
 require_relative 'yoksis/references'
 require_relative 'yoksis/staff'
+require_relative 'yoksis/students'
 require_relative 'yoksis/units'
-
---
+++ plugins/support/lib/nokul/support/xokul/api/yoksis/students.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Xokul
+  module Yoksis
+    class Students < API
+      configure do |config|
+        config.synopsis  = 'Get student related information from YOKSIS'
+        config.version   = '1'
+        config.namespace = '/yoksis/students'
+      end
+
+      def information(id_number)
+        conn.get '/information', params: { id_number: id_number }
+      end
+    end
+  end
+end
