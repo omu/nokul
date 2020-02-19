00f2ab3bcc57bd02f219a73b3bf6408196896257
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-19 15:36:45 +0300

    Configure necessary variables at base module

--- plugins/support/lib/nokul/support/xokul.rb
+++ plugins/support/lib/nokul/support/xokul.rb
@@ -4,3 +4,13 @@ require_relative 'xokul/configuration'
 require_relative 'xokul/connection'
 require_relative 'xokul/endpoint'
 require_relative 'xokul/api'
+
+module Xokul
+  private_constant :Configuration
+
+  def self.configure(&block)
+    return Configuration.config unless block_given?
+
+    Configuration.configure(&block)
+  end
+end
