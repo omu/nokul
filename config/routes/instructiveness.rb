928597167c678ebe3b1358b55cb574bb6c12b69c
dilara <adilarakoca@gmail.com>
2020-02-20 13:09:00 +0300

    Remove grades routes & controller & views

--- config/routes/instructiveness.rb
+++ config/routes/instructiveness.rb
@@ -2,15 +2,6 @@
 
 scope module: :instructiveness do
   resources :given_courses, only: %i[index show] do
-    resources :assessments, only: [] do
-      resources :grades, only: [] do
-        collection do
-          get :edit
-          post :update
-        end
-      end
-    end
-
     get :students, on: :member
   end
 end
