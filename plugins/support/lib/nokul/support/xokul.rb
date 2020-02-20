82a0acb7ea84407604e5c82299044f1c1071c458
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-20 14:04:51 +0300

    Refactor configure method

--- plugins/support/lib/nokul/support/xokul.rb
+++ plugins/support/lib/nokul/support/xokul.rb
@@ -8,11 +8,7 @@ require_relative 'xokul/api'
 module Xokul
   private_constant :Configuration
 
-  # FIXME: Not sure how to design a configure method. That's why, see the Rails
-  # source code to gain know-how about how they designed it.
-  def self.configure(&block)
-    return Configuration.config unless block_given?
-
-    Configuration.configure(&block)
+  def self.configure
+    yield Configuration.config if block_given?
   end
 end
