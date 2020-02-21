e23080f8a9477996b2878aecb7579b120357fc29
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-21 20:20:26 +0300

    Make a fresh start (renew a right spirit within me, god)

--- plugins/support/lib/nokul/support/xokul/api/yoksis.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'yoksis/graduates'
-require_relative 'yoksis/military'
-require_relative 'yoksis/references'
-require_relative 'yoksis/resumes'
-require_relative 'yoksis/staff'
-require_relative 'yoksis/students'
-require_relative 'yoksis/units'
