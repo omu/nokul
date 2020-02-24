4824547a3bd3bf806f7ace79ce669c7387bd7ff6
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-24 15:12:22 +0300

    Remove module inclusion for basic configuration methods

--- plugins/support/lib/nokul/support/xokul.rb
+++ plugins/support/lib/nokul/support/xokul.rb
@@ -3,12 +3,3 @@
 require_relative 'xokul/configuration'
 require_relative 'xokul/connection'
 require_relative 'xokul/endpoint'
-require_relative 'xokul/api'
-
-module Xokul
-  private_constant :Configuration
-
-  def self.configure
-    yield Configuration.config if block_given?
-  end
-end
