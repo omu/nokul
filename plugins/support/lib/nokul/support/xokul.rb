e17205dc6fd0fb11cf12e487a930f4c49c900322
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-02-27 14:20:42 +0300

    Require module files

---
+++ plugins/support/lib/nokul/support/xokul.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require_relative 'xokul/default'
+require_relative 'xokul/configuration'
+require_relative 'xokul/authentication'
+require_relative 'xokul/connection'
