4cc3cc0906fe91ea98dc0f52e1563b8c5dd2d56e
ecmelkytz <ecmel.kytz@gmail.com>
2020-03-02 14:11:26 +0300

    Both fees cannot be left empty

--- app/models/tuition.rb
+++ app/models/tuition.rb
@@ -15,5 +15,10 @@ class Tuition < ApplicationRecord
   # validations
   validates :fee, numericality: { greater_than_or_equal_to: 0 }
   validates :foreign_student_fee, numericality: { greater_than_or_equal_to: 0 }
+  validate :fee_must_be_specified
   validates_associated :unit_tuitions
+
+  def fee_must_be_specified
+    errors.add(:base, :not_specified_fees) if [fee, foreign_student_fee].map(&:zero?).all?
+  end
 end
