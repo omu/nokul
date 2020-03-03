4d22d0778da4b0eecb85aff099685f80eeff780e
ecmelkytz <ecmel.kytz@gmail.com>
2020-03-03 14:10:53 +0300

    Refactor unit tuition validator

--- app/validators/unit_tuition_validator.rb
+++ app/validators/unit_tuition_validator.rb
@@ -2,18 +2,19 @@
 
 class UnitTuitionValidator < ActiveModel::Validator
   def validate(record)
-    @unit_tuition = record
-    @unit = record.unit
-    @academic_term = record.academic_term
-    return error_message if tuition_exists?
+    @record = record
+    @tuition = record.tuition
+    return unless already_exists?
+
+    @tuition.errors.add(
+      :base,
+      I18n.t('defined', name: @record&.unit&.name, scope: %i[validators unit_tuition])
+    )
   end
 
-  def tuition_exists?
+  def already_exists?
     UnitTuition.includes(tuition: :academic_term)
-               .where.not(id: @unit_tuition).where(unit: @unit).map(&:academic_term).include?(@academic_term)
-  end
-
-  def error_message
-    @unit_tuition.tuition.errors.add(:base, I18n.t('defined', name: @unit.name, scope: %i[validators unit_tuition]))
+               .where.not(id: @record.id)
+               .where(unit: @record.unit_id, tuitions: { academic_term_id: @tuition&.academic_term_id }).exists?
   end
 end
