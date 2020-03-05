ecf3e5f1e4d755a98e5086e28a06653601766e5d
dilara <adilarakoca@gmail.com>
2020-03-05 15:10:33 +0300

    Refactor standard migration

--- db/migrate/20200305103056_create_standards.rb
+++ db/migrate/20200305103056_create_standards.rb
@@ -10,10 +10,6 @@ class CreateStandards < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :standards, :version
-    add_index :standards, :name_tr
-    add_index :standards, :name_en
-
     add_length_constraint :standards, :version, less_than_or_equal_to: 50
     add_length_constraint :standards, :name_tr, less_than_or_equal_to: 255
     add_length_constraint :standards, :name_en, less_than_or_equal_to: 255
