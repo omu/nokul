c8a18939a45c25657e2b7c49d210a9609ae756bc
dilara <adilarakoca@gmail.com>
2020-03-05 15:17:32 +0300

    Add uniqueness validations and remove relation

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class Standard < ApplicationRecord
-  has_many :snccs
-
+  # validations
   validates :version, presence: true, length: { maximum: 50 }
-  validates :name_tr, presence: true, length: { maximum: 255 }
-  validates :name_en, presence: true, length: { maximum: 255 }
+  validates :name_tr, presence: true, uniqueness: { scope: :version }, length: { maximum: 255 }
+  validates :name_en, presence: true, uniqueness: { scope: :version }, length: { maximum: 255 }
 end
