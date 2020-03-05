6928bd47da6bab4b4ece8590114159f4e2424fd6
dilara <adilarakoca@gmail.com>
2020-03-05 09:57:06 +0300

    Remove files & relations about question bank module

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -27,7 +27,6 @@ class Unit < ApplicationRecord
   belongs_to :unit_instruction_type, optional: true
   belongs_to :unit_instruction_language, optional: true
   belongs_to :university_type, optional: true
-
   has_many :duties, dependent: :destroy
   has_many :employees, through: :duties
   has_many :students, dependent: :nullify
@@ -49,9 +48,6 @@ class Unit < ApplicationRecord
   has_many :unit_calendars, dependent: :destroy
   has_many :calendars, through: :unit_calendars
   has_many :snccs, dependent: :destroy
-  has_many :source_books, dependent: :destroy
-  has_many :questions, dependent: :destroy
-  has_many :open_ended_questions, dependent: :destroy
 
   # validations
   validates :name, presence: true,
