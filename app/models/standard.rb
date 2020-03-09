b7ba80e92652f66c7f4c1d48c9a31e5bb781fd41
dilara <adilarakoca@gmail.com>
2020-03-09 13:39:35 +0300

    Add reference search

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class Standard < ApplicationRecord
+  # search
+  include ReferenceSearch
+
   # validations
   validates :version, presence: true, length: { maximum: 50 }
   validates :name, presence: true, uniqueness: { scope: :version }, length: { maximum: 255 }
