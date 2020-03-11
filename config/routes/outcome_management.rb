496d402fc0542f39299b81e70e7a3ffae25b27e3
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-11 14:35:58 +0300

    Add outcomes routes

--- config/routes/outcome_management.rb
+++ config/routes/outcome_management.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 scope module: :outcome_management do
-  resources :unit_standards
+  resources :unit_standards do
+    resources :outcomes, except: :index
+  end
 end
