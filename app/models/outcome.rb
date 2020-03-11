aeb22045b59a431ca923f19721186e6fe9f26baf
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-11 10:03:11 +0300

    Change unit relation to unit_standard

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -2,9 +2,12 @@
 
 class Outcome < ApplicationRecord
   # relations
-  belongs_to :unit
+  belongs_to :unit_standard
 
   # validations
-  validates :code, presence: true, uniqueness: { scope: :unit_id }, length: { maximum: 10 }
+  validates :code, presence: true, uniqueness: { scope: :unit_standard_id }, length: { maximum: 10 }
   validates :name, presence: true, length: { maximum: 255 }
+
+  # delegates
+  delegate :unit, to: :unit_standard
 end
