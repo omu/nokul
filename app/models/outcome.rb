ad722f03f478a4fe39cc8406aef70f100f19b85e
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-17 11:16:55 +0300

    Add parent/child relations to outcome

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -3,6 +3,10 @@
 class Outcome < ApplicationRecord
   # relations
   belongs_to :unit_standard
+  belongs_to :macro_outcome, class_name: 'Outcome', foreign_key: :parent_id,
+                             inverse_of: :micro_outcomes, optional: true
+  has_many :micro_outcomes, class_name: 'Outcome', foreign_key: :parent_id,
+                            inverse_of: :macro_outcome, dependent: :destroy
 
   # validations
   validates :code, presence: true, uniqueness: { scope: :unit_standard_id }, length: { maximum: 10 }
