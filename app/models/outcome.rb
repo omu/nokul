cff4a2beb0af2a94a5b809049f0674bde1361de9
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-17 11:17:34 +0300

    Accept atrributes for child outcome

--- app/models/outcome.rb
+++ app/models/outcome.rb
@@ -7,6 +7,7 @@ class Outcome < ApplicationRecord
                              inverse_of: :micro_outcomes, optional: true
   has_many :micro_outcomes, class_name: 'Outcome', foreign_key: :parent_id,
                             inverse_of: :macro_outcome, dependent: :destroy
+  accepts_nested_attributes_for :micro_outcomes
 
   # validations
   validates :code, presence: true, uniqueness: { scope: :unit_standard_id }, length: { maximum: 10 }
