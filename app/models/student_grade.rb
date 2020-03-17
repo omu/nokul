0485577b91682ec75a6652137897d46940c8c5d5
ecmelkytz <ecmel.kytz@gmail.com>
2020-03-17 14:51:56 +0300

    Add new relations to student and grade models

--- app/models/student_grade.rb
+++ app/models/student_grade.rb
@@ -4,4 +4,14 @@ class StudentGrade < ApplicationRecord
   include ReferenceCallbacks
   include ReferenceSearch
   include ReferenceValidations
+
+  # relations
+  has_many :students, dependent: :nullify
+
+  # scopes
+  scope :course, -> { where(code: 9) }
+  scope :preparatory, -> { where(code: [1, 15]) }
+  scope :phd_qualify, -> { where(code: 11) }
+  scope :project, -> { where(code: 14) }
+  scope :thesis, -> { where(code: 10) }
 end
