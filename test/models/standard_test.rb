3a2a962f424ab8b0b406ad55abf701a737719320
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-18 10:54:39 +0300

    Add standard model test

--- test/models/standard_test.rb
+++ test/models/standard_test.rb
@@ -3,4 +3,20 @@
 require 'test_helper'
 
 class StandardTest < ActiveSupport::TestCase
+  extend Support::Minitest::ValidationHelper
+
+  # validations: presence
+  validates_presence_of :version
+  validates_presence_of :name
+
+  # validations: length
+  validates_length_of :version, maximum: 50
+  validates_length_of :name, maximum: 255
+
+  # validations: uniqueness
+  test 'uniqueness validations for name of a standard' do
+    fake = standards(:fedek).dup
+    assert_not fake.valid?
+    assert_not_empty fake.errors[:name]
+  end
 end
