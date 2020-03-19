c6905a5752054e2d43efe40b3306541ded3fc7cc
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-19 08:44:49 +0300

    Add has many standards relation

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -4,6 +4,9 @@ class Standard < ApplicationRecord
   # search
   include ReferenceSearch
 
+  # relations
+  has_many :unit_standards, dependent: :destroy
+
   # validations
   validates :version, presence: true, length: { maximum: 50 }
   validates :name, presence: true, uniqueness: { scope: :version }, length: { maximum: 255 }
