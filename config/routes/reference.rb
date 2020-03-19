5b591e625170f8d684f42656cab6940a6f4b62a2
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-19 14:31:27 +0300

    Add accreditation standard routes & views

--- config/routes/reference.rb
+++ config/routes/reference.rb
@@ -2,15 +2,15 @@
 
 namespace :reference do
   get '/', to: 'dashboard#index'
-  resources :academic_terms,     except: :show
-  resources :assessment_methods, except: :show
-  resources :course_group_types, except: :show
-  resources :course_types,       except: :show
-  resources :document_types,     except: :show
-  resources :evaluation_types,   except: :show
-  resources :high_school_types,  except: :show
-  resources :languages,          except: :show
-  resources :scholarship_types,  except: :show
-  resources :standards,          except: :show
-  resources :titles,             except: :show
+  resources :academic_terms,          except: :show
+  resources :assessment_methods,      except: :show
+  resources :course_group_types,      except: :show
+  resources :course_types,            except: :show
+  resources :document_types,          except: :show
+  resources :evaluation_types,        except: :show
+  resources :high_school_types,       except: :show
+  resources :languages,               except: :show
+  resources :scholarship_types,       except: :show
+  resources :accreditation_standards, except: :show
+  resources :titles,                  except: :show
 end
