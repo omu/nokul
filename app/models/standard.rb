e3be999e810c06449aeb92dffc8577fe94bf6da4
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-19 15:08:57 +0300

    Add standard & unit_standard models

--- app/models/standard.rb
+++ app/models/standard.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
 class Standard < ApplicationRecord
-  # search
-  include ReferenceSearch
+  # enums
+  enum status: { passive: 0, active: 1 }
 
   # relations
+  belongs_to :accreditation_standard
+  has_many :macro_outcomes, -> { where(parent_id: nil) }, class_name: 'Outcome', inverse_of: :standard
+  has_many :outcomes, dependent: :destroy
   has_many :unit_standards, dependent: :destroy
+  has_many :units, through: :unit_standards
 
   # validations
-  validates :version, presence: true, length: { maximum: 50 }
-  validates :name, presence: true, uniqueness: { scope: :version }, length: { maximum: 255 }
+  validates :status, inclusion: { in: statuses.keys }
+  validates_associated :unit_standards
+
+  # delegates
+  delegate :name, to: :accreditation_standard
 end
--- app/models/unit_standard.rb
+++ app/models/unit_standard.rb
@@ -1,16 +1,11 @@
 # frozen_string_literal: true
 
 class UnitStandard < ApplicationRecord
-  # enums
-  enum status: { passive: 0, active: 1 }
-
   # relations
-  belongs_to :standard
   belongs_to :unit
-  has_many :outcomes, dependent: :destroy
-  has_many :macro_outcomes, -> { where(parent_id: nil) }, class_name: 'Outcome', inverse_of: :unit_standard
+  belongs_to :standard
 
   # validations
-  validates :standard, presence: true, uniqueness: { scope: :unit }
-  validates :status, inclusion: { in: statuses.keys }, uniqueness: { scope: :unit }
+  validates :standard, uniqueness: { scope: :unit }
+  validates_with UnitStandardValidator
 end
