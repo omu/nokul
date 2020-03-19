3b0e62e37516474d13c8427be7de8e119e50f2b2
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-03-19 15:14:40 +0300

    Add standard tests

--- test/models/standard_test.rb
+++ test/models/standard_test.rb
@@ -4,23 +4,21 @@ require 'test_helper'
 
 class StandardTest < ActiveSupport::TestCase
   extend Support::Minitest::AssociationHelper
+  extend Support::Minitest::EnumerationHelper
   extend Support::Minitest::ValidationHelper
 
+  # enums
+  enum status: { passive: 0, active: 1 }
+
   # relations
+  belongs_to :accreditation_standard
+  has_many :macro_outcomes, class_name: 'Outcome', inverse_of: :standard
+  has_many :outcomes, dependent: :destroy
   has_many :unit_standards, dependent: :destroy
+  has_many :units, through: :unit_standards
 
-  # validations: presence
-  validates_presence_of :version
-  validates_presence_of :name
-
-  # validations: length
-  validates_length_of :version, maximum: 50
-  validates_length_of :name, maximum: 255
-
-  # validations: uniqueness
-  test 'uniqueness validations for name of a standard' do
-    fake = standards(:fedek).dup
-    assert_not fake.valid?
-    assert_not_empty fake.errors[:name]
+  # delegates
+  test 'a standard reach accreditation standard name parameter' do
+    assert standards(:one).send(:name)
   end
 end
