413a8df4de88afe1033fa4bbb112f5f53a77553b
ecmelkytz <ecmel.kytz@gmail.com>
2020-03-31 15:18:28 +0300

    Add process for daytime education

---
+++ app/services/debt/tuition/process/daytime_education.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require_relative '../tuition_handler'
+require_relative '../operation/scholarship'
+require_relative '../operation/disability'
+require_relative '../operation/no_discount'
+
+module Debt
+  module Tuition
+    module Process
+      class DaytimeEducation < TuitionHandler
+        def self.chain
+          Operation::Scholarship.new(Operation::Disability.new(Operation::NoDiscount.new))
+        end
+      end
+    end
+  end
+end
