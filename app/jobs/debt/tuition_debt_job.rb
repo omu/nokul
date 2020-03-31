0d4caa75d7a68b379ff86b3eb1178b9f6c4bae49
ecmelkytz <ecmel.kytz@gmail.com>
2020-03-31 16:38:17 +0300

    Refactor tuition debt job

--- app/jobs/debt/tuition_debt_job.rb
+++ app/jobs/debt/tuition_debt_job.rb
@@ -5,19 +5,29 @@ module Debt
     queue_as :high
 
     def perform(unit_ids, term_id)
-      @chain = Debt::Tuition::Process::SecondaryEducation.chain
       @units = Unit.find(unit_ids.reject(&:empty?))
       @term_id = term_id
     end
 
     after_perform do |debt|
       [@units].flatten.compact.each do |unit|
+        chain = set_chain(unit)
         unit.students.active.each do |student|
           tuition = unit.tuitions.find_by(academic_term_id: @term_id)
           debt = Debt::Tuition::TuitionDebt.new(student, tuition.unit_tuitions.first)
-          @chain.call(debt)
+          chain.call(debt)
         end
       end
     end
+
+    # rubocop:disable Naming/AccessorMethodName
+    def set_chain(unit)
+      if unit.evening?
+        Debt::Tuition::Process::EveningEducation.chain
+      else
+        Debt::Tuition::Process::DaytimeEducation.chain
+      end
+    end
+    # rubocop:enable Naming/AccessorMethodName
   end
 end
