8724594b4d99a5baa7915d2ae5f0e3c5ea6b80ce
ecmelkytz <ecmel.kytz@gmail.com>
2020-04-09 09:42:46 +0300

    Don't anything if student have scholarship

--- app/services/debt/tuition/operation/scholarship.rb
+++ app/services/debt/tuition/operation/scholarship.rb
@@ -6,25 +6,12 @@ module Debt
   module Tuition
     module Operation
       class Scholarship < TuitionHandler
-        include Creation
-
-        def operate(params)
-          compute(params)
-          create_debt(params, description)
+        def operate(_params)
+          nil
         end
 
         def fulfill?(params)
           params.student.scholarship?
-        end
-
-        private
-
-        def compute(params)
-          params.amount = 1
-        end
-
-        def description
-          'Burslu olduğu sembolik 1 lira borçlandırılmıştır.'
         end
       end
     end
