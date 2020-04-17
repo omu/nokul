f64b2f0e2ff8e257e4d966a5fed5aea370401a6f
ecmelkytz <ecmel.kytz@gmail.com>
2020-04-17 10:11:34 +0300

    Fix debt service test

--- test/services/debt/tuition_debt_service_test.rb
+++ test/services/debt/tuition_debt_service_test.rb
@@ -4,6 +4,11 @@ require 'test_helper'
 
 module Debt
   class TuitionDebtServiceTest < ActiveSupport::TestCase
+    setup do
+      @academic_term = academic_terms(:active_term)
+      @student = students(:mike)
+    end
+
     test 'must be create tuition debt if exceeded_education_period is true' do
       academic_term = academic_terms(:spring_2018_2019)
 
@@ -22,16 +27,14 @@ module Debt
     end
 
     test 'student of evening education and has disability' do
-      academic_term = academic_terms(:active_term)
-
       Debt::Tuition::Dispatch.perform([units(:buro_yonetimi_ve_yonetici_asistanligi_io)].flatten,
-                                      academic_term,
+                                      @academic_term,
                                       Time.zone.now + 2.months)
 
       tuition_debt = TuitionDebt.last
 
-      assert_equal tuition_debt.student, students(:mike)
-      assert_equal tuition_debt.academic_term, academic_term
+      assert_equal tuition_debt.student, @student
+      assert_equal tuition_debt.academic_term, @academic_term
       assert_equal tuition_debt.amount.to_f, 325
       assert_equal tuition_debt.description, 'disability'
       assert_equal tuition_debt.type, 'bulk'
@@ -39,23 +42,15 @@ module Debt
     end
 
     test "student of evening education and hasn't disability and scholarship" do
-      academic_term = academic_terms(:active_term)
-      student = students(:mike)
-
-      student.user.update(disability_rate: 0)
+      @student.user.update(disability_rate: 0)
 
       Debt::Tuition::Dispatch.perform([units(:buro_yonetimi_ve_yonetici_asistanligi_io)].flatten,
-                                      academic_term,
+                                      @academic_term,
                                       Time.zone.now + 2.months)
 
       tuition_debt = TuitionDebt.last
-
-      assert_equal tuition_debt.student, student
-      assert_equal tuition_debt.academic_term, academic_term
       assert_equal tuition_debt.amount.to_f, 650
       assert_equal tuition_debt.description, 'no_discount'
-      assert_equal tuition_debt.type, 'bulk'
-      assert_not tuition_debt.paid
     end
   end
 end
