7e3ab1e5a1a8e3a355396b7fbcaedb7748e279d9
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-05-07 07:55:58 +0300

    Remove relocated method

--- app/decorators/calendar_event_decorator.rb
+++ app/decorators/calendar_event_decorator.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 class CalendarEventDecorator < SimpleDelegator
-  def active_now?
-    return unless presence
-
-    end_time ? Time.current.between?(start_time, end_time) : start_time.past?
-  end
-
   def date_range
     return I18n.t('calendar_management.calendar_events.undefined_date_range') unless presence
 
--- test/decorators/calendar_event_decorator_test.rb
+++ test/decorators/calendar_event_decorator_test.rb
@@ -7,11 +7,6 @@ class CalendarEventDecoratorTest < ActiveSupport::TestCase
     @calendar_event = CalendarEventDecorator.new(calendar_events(:mid_term_results_announcement))
   end
 
-  test 'active_now? method' do
-    assert_not CalendarEventDecorator.new(calendar_events(:add_drop_fall_2018_grad)).active_now?
-    assert @calendar_event.active_now?
-  end
-
   test 'date_range? method' do
     assert_equal @calendar_event.date_range,
                  "#{@calendar_event.start_time&.strftime('%F %R')} - #{@calendar_event.end_time&.strftime('%F %R')}"
