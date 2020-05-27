3687f0caa8085395a6ded16b36a66ff89ae904aa
Dilara Koca <dilara.koca@baum.omu.edu.tr>
2020-05-27 12:03:40 +0300

    Add tests for cases when event is not active

--- test/controllers/course_management/available_course_groups_controller_test.rb
+++ test/controllers/course_management/available_course_groups_controller_test.rb
@@ -17,25 +17,24 @@ module CourseManagement
 
     test 'should create available course group' do
       assert_difference('AvailableCourseGroup.count') do
-        post available_course_available_course_groups_path(@available_course), params: {
-          available_course_group: {
-            name: 'Group 4', quota: 10,
-            lecturers_attributes: {
-              '0' => {
-                lecturer_id: employees(:serhat_active).id,
-                coordinator: true
-              }
-            }
-          }
-        }
+        post available_course_available_course_groups_path(@available_course), params: create_params
       end
 
       group = AvailableCourseGroup.last
-
       assert_equal 'Group 4', group.name
       assert_equal 10, group.quota
       assert_redirected_to available_course_path(@available_course)
       assert_equal translate('.create.success'), flash[:info]
+    end
+
+    test 'should not create available course group when event is not active' do
+      deactivate_event
+      assert_no_difference('AvailableCourseGroup.count') do
+        post available_course_available_course_groups_path(@available_course), params: create_params
+      end
+
+      assert_redirected_to available_course_path(@available_course)
+      assert_equal t('errors.not_proper_event_range', scope: %i[course_management available_courses]), flash[:info]
     end
 
     test 'should get edit' do
@@ -75,7 +74,33 @@ module CourseManagement
       assert_equal translate('.destroy.success'), flash[:info]
     end
 
+    test 'should not destroy available course group when event is not active' do
+      deactivate_event
+      assert_no_difference('AvailableCourseGroup.count') do
+        delete available_course_available_course_group_path(@available_course, @group)
+      end
+
+      assert_redirected_to available_course_path(@available_course)
+      assert_equal t('errors.not_proper_event_range', scope: %i[course_management available_courses]), flash[:info]
+    end
+
     private
+
+    def deactivate_event
+      event = @available_course.unit.calendars.active.last.event('add_drop_available_courses')
+      event.update(end_time: Time.zone.now - 1.day)
+    end
+
+    def create_params
+      {
+        available_course_group: {
+          name: 'Group 4', quota: 10,
+          lecturers_attributes: {
+            '0' => { lecturer_id: employees(:serhat_active).id, coordinator: true }
+          }
+        }
+      }
+    end
 
     def translate(key)
       t("course_management.available_course_groups#{key}", course: @available_course.name)
--- test/controllers/course_management/course_evaluation_types_controller_test.rb
+++ test/controllers/course_management/course_evaluation_types_controller_test.rb
@@ -16,30 +16,27 @@ module CourseManagement
     end
 
     test 'should create course evaluation type' do
-      evaluation_type = evaluation_types(:undergraduate_final)
-      parameters = {
-        available_course_id:                  @available_course.id,
-        evaluation_type_id:                   evaluation_type.id,
-        percentage:                           60,
-        course_assessment_methods_attributes: {
-          '0' => {
-            assessment_method_id: assessment_methods(:exam).id,
-            percentage:           100
-          }
-        }
-      }
       assert_difference('CourseEvaluationType.count') do
-        post available_course_evaluation_types_path(@available_course),
-             params: { course_evaluation_type: parameters }
+        post available_course_evaluation_types_path(@available_course), params: create_params
       end
 
       course_evaluation_type = CourseEvaluationType.last
 
       assert_equal @available_course, course_evaluation_type.available_course
-      assert_equal evaluation_type, course_evaluation_type.evaluation_type
+      assert_equal evaluation_types(:undergraduate_final), course_evaluation_type.evaluation_type
       assert_equal 60, course_evaluation_type.percentage
       assert_redirected_to available_course_path(@available_course)
       assert_equal translate('.create.success'), flash[:notice]
+    end
+
+    test 'should not create course evaluation type when event is not active' do
+      deactivate_event
+      assert_no_difference('CourseEvaluationType.count') do
+        post available_course_evaluation_types_path(@available_course), params: create_params
+      end
+
+      assert_redirected_to available_course_path(@available_course)
+      assert_equal t('errors.not_proper_event_range', scope: %i[course_management available_courses]), flash[:info]
     end
 
     test 'should get edit' do
@@ -81,7 +78,38 @@ module CourseManagement
       assert_equal translate('.destroy.success'), flash[:notice]
     end
 
+    test 'should not destroy course evaluation type when event is not active' do
+      deactivate_event
+      course_evaluation_type = course_evaluation_types(:course_evaluation_type_to_delete)
+      available_course = course_evaluation_type.available_course
+
+      assert_no_difference('CourseEvaluationType.count') do
+        delete available_course_evaluation_type_path(available_course, course_evaluation_type)
+      end
+
+      assert_redirected_to available_course_path(available_course)
+      assert_equal t('errors.not_proper_event_range', scope: %i[course_management available_courses]), flash[:info]
+    end
+
     private
+
+    def deactivate_event
+      event = @available_course.unit.calendars.active.last.event('add_drop_available_courses')
+      event.update(end_time: Time.zone.now - 1.day)
+    end
+
+    def create_params
+      {
+        course_evaluation_type: {
+          available_course_id:                  @available_course.id,
+          evaluation_type_id:                   evaluation_types(:undergraduate_final).id,
+          percentage:                           60,
+          course_assessment_methods_attributes: {
+            '0' => { assessment_method_id: assessment_methods(:exam).id, percentage: 100 }
+          }
+        }
+      }
+    end
 
     def translate(key)
       t("course_management.course_evaluation_types#{key}", course: @available_course.name)
