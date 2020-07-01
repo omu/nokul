1b249935ef23ee8fb62763e53cb107c0f1dcfbe7
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-07-02 02:01:04 +0300

    Xokul adaylar servisinin entegrasyonuna başla

---
+++ app/services/xokul/yoksis/prospectives.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Xokul
+  module Yoksis
+    module Prospectives
+      module_function
+
+      def all(type, year, page: 1, per_page: 100)
+        path = path_with_page_params '/yoksis/prospectives/students', page, per_page
+        Connection.request path, params: { prospective: { type: type, year: year } }
+      end
+
+      def student(type, year, id_number)
+        Connection.request(
+          '/yoksis/prospectives/students', params: { prospective: { type: type, year: year, id_number: id_number } }
+        )
+      end
+
+      def photo(id_number)
+        Connection.request(
+          '/yoksis/prospectives/students/photo', params: { prospective: { id_number: id_number } }
+        )
+      end
+
+      def path_with_page_params(path, page, per_page)
+        "#{path}?page=#{page}&per_page=#{per_page}"
+      end
+
+      private_class_method :path_with_page_params
+    end
+  end
+end
