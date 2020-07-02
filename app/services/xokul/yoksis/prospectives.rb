da7b98bab4cbbfdf517030e56034493cf2e3b935
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-07-02 13:35:16 +0300

    YÖKSİS adaylar servisinin eksik metotlarını ekle

--- app/services/xokul/yoksis/prospectives.rb
+++ app/services/xokul/yoksis/prospectives.rb
@@ -10,6 +10,16 @@ module Xokul
         Connection.request path, params: { prospective: { type: type, year: year } }
       end
 
+      def through_electronic(type, year, page: 1, per_page: 100)
+        path = path_with_page_params '/yoksis/prospectives/students/through_electronic', page, per_page
+        Connection.request path, params: { prospective: { type: type, year: year } }
+      end
+
+      def without_electronics(type, year, page: 1, per_page: 100)
+        path = path_with_page_params '/yoksis/prospectives/students/without_electronic', page, per_page
+        Connection.request path, params: { prospective: { type: type, year: year } }
+      end
+
       def student(type, year, id_number)
         Connection.request(
           '/yoksis/prospectives/students', params: { prospective: { type: type, year: year, id_number: id_number } }
