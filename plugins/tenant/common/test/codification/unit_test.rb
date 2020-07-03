5f515f1ba36fd17f77a81dd6aa1290c2e4ded242
Recai Oktaş <roktas@gmail.com>
2020-07-03 11:33:28 +0300

    Kodifikasyon testlerini düzelt

--- plugins/tenant/common/test/codification/unit_test.rb
+++ plugins/tenant/common/test/codification/unit_test.rb
@@ -7,45 +7,45 @@ module Nokul
     module Codification
       class UnitTest < ActiveSupport::TestCase
         test 'code_generator works' do
-          coder = Unit.code_generator(starting: '023', ending: '025', memory: nil)
-          assert_equal '023', coder.run
-          assert_equal '024', coder.run
-          assert_equal '025', coder.run
+          coder = Unit.code_generator(starting: '0023', ending: '0025', memory: nil)
+          assert_equal '0023', coder.run
+          assert_equal '0024', coder.run
+          assert_equal '0025', coder.run
 
           assert_raise(StopIteration) { coder.run }
         end
 
         test 'code_generator works as hexadecimal sequences automagically' do
-          coder = Unit.code_generator(starting: '009', ending: '00C', memory: nil)
-          assert_equal '009', coder.run
-          assert_equal '00A', coder.run
-          assert_equal '00B', coder.run
-          assert_equal '00C', coder.run
+          coder = Unit.code_generator(starting: 'A009', ending: 'A00C', memory: nil)
+          assert_equal 'A009', coder.run
+          assert_equal 'A00A', coder.run
+          assert_equal 'A00B', coder.run
+          assert_equal 'A00C', coder.run
 
           assert_raise(StopIteration) { coder.run }
         end
 
         test 'code_generator works as alphabetical sequences automagically' do
-          coder = Unit.code_generator(starting: '009', ending: '00H', memory: nil)
-          assert_equal '009', coder.run
-          assert_equal '00A', coder.run
-          assert_equal '00B', coder.run
-          assert_equal '00C', coder.run
-          assert_equal '00D', coder.run
-          assert_equal '00E', coder.run
-          assert_equal '00F', coder.run
-          assert_equal '00G', coder.run
-          assert_equal '00H', coder.run
+          coder = Unit.code_generator(starting: '3009', ending: '300H', memory: nil)
+          assert_equal '3009', coder.run
+          assert_equal '300A', coder.run
+          assert_equal '300B', coder.run
+          assert_equal '300C', coder.run
+          assert_equal '300D', coder.run
+          assert_equal '300E', coder.run
+          assert_equal '300F', coder.run
+          assert_equal '300G', coder.run
+          assert_equal '300H', coder.run
 
           assert_raise(StopIteration) { coder.run }
         end
 
         test 'code_generator works with patterns' do
-          coder = Unit.code_generator(starting: '001', ending: '009', only: '^[^3-7]+$', memory: nil)
-          assert_equal '001', coder.run
-          assert_equal '002', coder.run
-          assert_equal '008', coder.run
-          assert_equal '009', coder.run
+          coder = Unit.code_generator(starting: '8001', ending: '8009', only: '^[^3-7]+$', memory: nil)
+          assert_equal '8001', coder.run
+          assert_equal '8002', coder.run
+          assert_equal '8008', coder.run
+          assert_equal '8009', coder.run
 
           assert_raise(StopIteration) { coder.run }
         end
