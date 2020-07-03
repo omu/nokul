9f1fb40b4978259664b8de0fcd5eb65d86992e06
isubas <irfan.subas@uzem.omu.edu.tr>
2020-07-03 13:47:04 +0300

    feat: add job to upload prospective students via yoksis

---
+++ app/jobs/yoksis/prospective_students_save_job.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Yoksis
+  class ProspectiveStudentsSaveJob < ApplicationJob
+    queue_as :high
+
+    def perform(type, year)
+      @type = type
+      @year = year
+
+      process
+    end
+
+    private
+
+    def process(page: 0)
+      response = Xokul::Yoksis::Prospectives.all(@type, @year, page: page)
+
+      response[:data].each do |params|
+        Actions::ProspectiveStudent::Upsert.call(params)
+      rescue ActiveRecord::RecordInvalid => e
+        byebug
+        next
+      end
+
+      current_page, total_pages = response.fetch(:meta, {}).values_at(:current_page, :total_pages)
+
+      process(page: current_page + 1) if current_page < total_pages - 1
+    end
+  end
+end
