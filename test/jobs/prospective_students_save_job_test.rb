825e3dc87ab132daa08f19b83f3a6e9cf275411b
isubas <irfan.subas@uzem.omu.edu.tr>
2020-07-06 10:48:04 +0300

    fix: prospective student için dosya üzerinden import özelliğini kaldır

--- test/jobs/prospective_students_save_job_test.rb
+++ test/jobs/prospective_students_save_job_test.rb
@@ -3,7 +3,7 @@
 require 'test_helper'
 
 class ProspectiveStudentsSaveJobTest < ActiveJob::TestCase
-  test 'Yoksis::ImportProspectiveStudentsJob runs in the high priority queue' do
+  test 'Yoksis::ProspectiveStudentsSaveJob runs in the high priority queue' do
     assert_equal Yoksis::ProspectiveStudentsSaveJob.new.queue_name, 'high'
   end
 end
