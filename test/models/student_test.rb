50b1c81b72964193b48ff60bda57086f4a7286b5
ecmelkytz <ecmel.kytz@gmail.com>
2020-07-08 15:10:01 +0300

    Fix tests

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -58,6 +58,10 @@ class StudentTest < ActiveSupport::TestCase
   end
 
   # scopes
+  test 'abroads scope returns students from abroad' do
+    assert_includes(Student.abroads, students(:mike))
+  end
+
   test 'exceeded scope returns students with exceeded education period' do
     assert_includes(Student.exceeded, students(:john))
   end
@@ -75,6 +79,12 @@ class StudentTest < ActiveSupport::TestCase
   end
 
   # custom methods
+
+  test 'abroad?' do
+    assert students(:mike).abroad?
+    assert_not students(:serhat_omu).abroad?
+  end
+
   test 'gpa method' do
     assert_equal students(:serhat).gpa, 1.8
     assert_equal students(:serhat_omu).gpa, 0
