23385de4f0e3589f727f7203e8486c7cad6df4b8
Emre Can Yılmaz <emrecan@ecylmz.com>
2020-07-20 11:39:22 +0300

    Codification yerine Nokul::Tennant::Codification kullan

--- test/lib/codification_test.rb
+++ test/lib/codification_test.rb
@@ -4,17 +4,17 @@ require 'test_helper'
 
 class CodificationTest < ActiveSupport::TestCase
   test 'Codification API exists' do
-    assert defined? Codification
-    assert(%i[Unit User Student].all? { |mod| Codification.const_defined?(mod) })
+    assert defined? Nokul::Tenant::Codification
+    assert(%i[Unit User Student].all? { |mod| Nokul::Tenant::Codification.const_defined?(mod) })
   end
 
   test 'Codification API has proper methods' do
-    assert_respond_to Codification::User, :name_generate
-    assert_respond_to Codification::User, :name_suggest
+    assert_respond_to Nokul::Tenant::Codification::User, :name_generate
+    assert_respond_to Nokul::Tenant::Codification::User, :name_suggest
 
-    assert_respond_to Codification::Student, :short_number_generator
-    assert_respond_to Codification::Student, :long_number_generator
+    assert_respond_to Nokul::Tenant::Codification::Student, :short_number_generator
+    assert_respond_to Nokul::Tenant::Codification::Student, :long_number_generator
 
-    assert_respond_to Codification::Unit, :code_generator
+    assert_respond_to Nokul::Tenant::Codification::Unit, :code_generator
   end
 end
