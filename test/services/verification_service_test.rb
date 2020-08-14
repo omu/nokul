66712edfe3b2d025abc5c5aacad68f663dc6bf8f
Emre Can Yılmaz <emrecan@ecylmz.com>
2020-08-14 11:10:07 +0300

    Remove verifications fixtures

--- test/services/verification_service_test.rb
+++ test/services/verification_service_test.rb
@@ -4,20 +4,20 @@ require 'test_helper'
 
 class VerificationServiceTest < ActiveSupport::TestCase
   setup do
-    @v = verifications(:one)
+    @v = { code: '12345', mobile_phone: '+905412345678', verified: 'false' }
 
     @verification =
       VerificationService.new(
-        mobile_phone: @v.mobile_phone
+        mobile_phone: @v[:mobile_phone]
       )
   end
 
   test 'verification code should be changed on every request' do
-    assert_not_equal @verification.code, @v.code
+    assert_not_equal @verification.code, @v[:code]
   end
 
   test 'should not send if sms limit exceeded' do
-    REDIS.hset("verification_#{@v.mobile_phone}", { sent_count: 3 })
+    REDIS.hset("verification_#{@v[:mobile_phone]}", { sent_count: 3 })
     assert_not @verification.can_be_sent?
   end
 end
