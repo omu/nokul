432355d6a7fb81f05a11cb26a84dcc225908115a
Emre Demir <emmredemmir@gmail.com>
2020-11-21 16:55:16 +0300

    Akademik yayınlar ekranına üst birim seviyesinde yeni grafikler ekle

--- app/controllers/manager/stats/articles_controller.rb
+++ app/controllers/manager/stats/articles_controller.rb
@@ -16,6 +16,19 @@ module Manager
       end
       
       def dashboard
+        unit_types = UnitType.includes(:units)
+                             .where(name: ['Rektörlük', 'Fakülte', 'Meslek Yüksekokulu', 'Yüksekokul', 'Enstitü'])
+        @number_of_articles_by_units = Article.active.joins(:units).group('units.names_depth_cache').count
+        @number_of_articles_by_base_units =
+          unit_types.each_with_object(Hash.new{ |h,k| h[k] = {} }) do |unit_type, hash|
+            unit_type.units.each do |unit|
+              unit_names = unit.subtree.pluck(:names_depth_cache)
+              number_of_articles = @number_of_articles_by_units.values_at(*unit_names).compact.sum
+              next if number_of_articles.zero?
+              
+              hash[unit_type.name][unit.name] = number_of_articles
+            end
+          end
         render layout: false
       end
 
